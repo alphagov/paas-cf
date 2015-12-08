11318b096ebacc30da917d5f36ffcb93245c24c6
Jonty Wareing <jonty@jonty.co.uk>
2015-12-08 11:04:24 +0000

    Add secret merging, fixtures, and a pass Makefile

--- manifests/cf-manifest/build_manifest.sh
+++ manifests/cf-manifest/build_manifest.sh
@@ -4,10 +4,15 @@ set -e
 cd $(dirname $0)
 
 terraform_outputs=${TERRAFORM_OUTPUTS:-"outputs/terraform-outputs.yml"}
+secrets=${SECRETS:-"outputs/cf-secrets.yml"}
+ssl_certs=${SSL_CERTS:-"outputs/cf-ssl-certificates.yml"}
 
 spruce merge \
   --prune meta --prune lamb_meta \
   --prune terraform_outputs \
+  --prune secrets \
   deployments/*.yml \
   deployments/aws/*.yml \
-  ${terraform_outputs}
+  ${terraform_outputs} \
+  ${secrets} \
+  ${ssl_certs}
