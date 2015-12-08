faca955d6aefab910460138648a24ff6a196cfbc
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-12-08 10:13:09 +0000

    Prune meta and lamb_meta from output manifest.
    These aren't needed once all the values have been interpolated, so let's
    remove them to keep the output clean.

--- manifests/cf-manifest/build_manifest.sh
+++ manifests/cf-manifest/build_manifest.sh
@@ -6,6 +6,7 @@ cd $(dirname $0)
 terraform_outputs=${TERRAFORM_OUTPUTS:-"outputs/terraform-outputs.yml"}
 
 spruce merge \
+  --prune meta --prune lamb_meta \
   --prune terraform_outputs \
   deployments/*.yml \
   deployments/aws/*.yml \
