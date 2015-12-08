97daec76ed139c1934d4ba8dcd6de0f5dc1aae97
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-12-08 15:38:02 +0000

    Populate AWS availability zones from terraform

---
+++ manifests/cf-manifest/spec/resource_pools_spec.rb
@@ -0,0 +1,50 @@
+
+RSpec.describe "resource_pools" do
+  let(:resource_pools) { manifest_with_defaults.fetch("resource_pools") }
+
+  POOL_BASE_NAMES = %w(
+    small
+    medium
+    large
+    runner
+    router
+  )
+  ERRAND_POOL_NAMES = %w(
+    small_errand
+    xlarge_errand
+  )
+  ZONE_KEYS = {
+    "z1" => :zone0,
+    "z2" => :zone1,
+  }
+
+  POOL_BASE_NAMES.each do |base_name|
+    ZONE_KEYS.keys.each do |zone_suffix|
+      describe "#{base_name}_#{zone_suffix}" do
+        let(:pool) { resource_pools.find {|p| p["name"] == "#{base_name}_#{zone_suffix}" } }
+
+        it "should specify the correct AWS AZ" do
+          expect(pool["cloud_properties"]["availability_zone"]).to eq(terraform_fixture(ZONE_KEYS[zone_suffix]))
+        end
+      end
+    end
+  end
+
+  ERRAND_POOL_NAMES.each do |pool_name|
+    describe pool_name do
+      let(:pool) { resource_pools.find {|p| p["name"] == pool_name } }
+
+      it "should specify the correct AWS AZ" do
+        expect(pool["cloud_properties"]["availability_zone"]).to eq(terraform_fixture(:zone0))
+      end
+    end
+  end
+
+  describe "the compilation pool" do
+    let(:compilation) { manifest_with_defaults.fetch("compilation") }
+
+    it "should specify the correct AWS AZ" do
+      expect(compilation["cloud_properties"]["availability_zone"]).to eq(terraform_fixture(:zone0))
+    end
+  end
+end
