dd674d8ea105840eca669578100c886454538f43
Colin Saliceti <c.saliceti@gmail.com>
2015-12-09 10:56:42 +0000

    Script to create terraform outputs yaml
    Extracts terraform outputs into a yaml file we can consume with `spruce`.

---
+++ concourse/scripts/extract_terraform_state_to_yaml.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'yaml'
+
+outputs=JSON.load($stdin)
+terraform_outputs = {
+  'terraform_outputs' => outputs['modules'][0]['outputs']
+}
+
+puts YAML.dump(terraform_outputs)
+
