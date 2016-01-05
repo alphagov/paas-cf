a969cd196fcc7a609309155c458652f8f9b68248
Michal Tekel <mtekel@gmail.com>
2016-01-05 12:27:53 +0000

    s3init: support instance profiles
    Try to get credentials from instance profile if they are not explicitly
    specified in the environment variables. Fail if the credentials are
    missing. Update signatures to involve AWS security token, which is
    required when using temporary credentials.

--- concourse/scripts/s3init.sh
+++ concourse/scripts/s3init.sh
@@ -4,12 +4,29 @@ bucket=$1
 file=$2
 init_file=$3
 
+# Attempt to use instance profile if keys not configured
+if [[ -z "${AWS_SECRET_ACCESS_KEY}" ]] && [[ -z ${AWS_ACCESS_KEY_ID} ]] ; then
+  meta_url="http://169.254.169.254/latest/meta-data/iam/security-credentials/"
+  profile_name=$(curl -s ${meta_url})
+  instance_profile=$(curl -s ${meta_url}/${profile_name})
+
+      AWS_ACCESS_KEY_ID=$(echo "${instance_profile}" | awk -F\" '$2 == "AccessKeyId"     { print $4 }')
+     AWS_SECURITY_TOKEN=$(echo "${instance_profile}" | awk -F\" '$2 == "Token"           { print $4 }')
+  AWS_SECRET_ACCESS_KEY=$(echo "${instance_profile}" | awk -F\" '$2 == "SecretAccessKey" { print $4 }')
+
+  [[ -z "${AWS_SECURITY_TOKEN}" ]] && echo "Could not obtain AWS_SECURITY_TOKEN from instance profile" && exit 105
+fi
+
 [[ -z "${bucket}" ]]    && echo "Must provide bucket name"    && exit 100
 [[ -z "${file}" ]]      && echo "Must provide file name"      && exit 101
 [[ -z "${init_file}" ]] && echo "Must provide init file path" && exit 102
+[[ -z "${AWS_ACCESS_KEY_ID}"     ]] && echo "AWS_ACCESS_KEY_ID nor specified or present in instance profile"     && exit 103
+[[ -z "${AWS_SECRET_ACCESS_KEY}" ]] && echo "AWS_SECRET_ACCESS_KEY nor specified or present in instance profile" && exit 104
 
 aws_path=/
 content_type='application/x-compressed-tar'
+acl="x-amz-acl:private"
+security_token="x-amz-security-token:${AWS_SECURITY_TOKEN}"
 region=eu-west-1
 host=${bucket}.s3-${region}.amazonaws.com
 
@@ -20,27 +37,28 @@ sign() {
 
 put() {
   date=$(date +"%a, %d %b %Y %T %z")
-  acl="x-amz-acl:private"
-  string="PUT\n\n$content_type\n$date\n$acl\n/$bucket$aws_path$file"
+  string="PUT\n\n${content_type}\n${date}\n${acl}\n${security_token}\n/${bucket}${aws_path}${file}"
   signature=$(sign "${string}")
   curl -i -s -X PUT -T ${init_file} \
-    -H "Host: $bucket.s3.amazonaws.com" \
-    -H "Date: $date" \
-    -H "Content-Type: $content_type" \
-    -H "$acl" \
-    -H "Authorization: AWS ${AWS_ACCESS_KEY_ID}:$signature" \
-    "https://${host}$aws_path$file"
+    -H "Host: ${bucket}.s3.amazonaws.com" \
+    -H "Date: ${date}" \
+    -H "Content-Type: ${content_type}" \
+    -H "${acl}" \
+    -H "${security_token}" \
+    -H "Authorization: AWS ${AWS_ACCESS_KEY_ID}:${signature}" \
+    "https://${host}${aws_path}${file}"
 }
 
 get() {
   date=$(date +"%a, %d %b %Y %T %z")
-  string="GET\n\n${contentType}\n${date}\n/$bucket$aws_path$file"
+  string="GET\n\n${content_type}\n${date}\n${security_token}\n/${bucket}${aws_path}${file}"
   signature=$(sign "${string}")
   curl -v -i -s -H "Host: ${bucket}.s3.amazonaws.com" \
     -H "Date: ${date}" \
-    -H "Content-Type: ${contentType}" \
+    -H "Content-Type: ${content_type}" \
+    -H "${security_token}" \
     -H "Authorization: AWS ${AWS_ACCESS_KEY_ID}:${signature}" \
-    https://${host}/${file}
+    "https://${host}/${file}"
 }
 
 response=$(get)
