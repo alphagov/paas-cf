a9e26916eb94797b5facdb6b5e5bfe73d61dc903
Jim Conner <jim.conner@digital.cabinet-office.gov.uk>
2016-01-08 15:08:53 +0000

    Configure ELB and security groups for cf ssh proxy
    This commit creates a dedicated ELB and security group for ssh-proxy
    access as sharing one with the web access leads to traffic being
    incorrectly routed. The terraform outputs have been extended to include
    the new sshproxy elb, which is then used in the deployment manifests.

--- terraform/cloudfoundry/outputs.tf
+++ terraform/cloudfoundry/outputs.tf
@@ -6,6 +6,10 @@ output "cf2_subnet_id" {
   value = "${aws_subnet.cf.1.id}"
 }
 
+output "ssh_elb_name" {
+  value = "${aws_elb.ssh-proxy-router.name}"
+}
+
 output "cf_root_domain" {
   value = "${var.env}.${var.dns_zone_name}"
 }
@@ -17,6 +21,7 @@ output "dns_zone_name" {
 output "elb_name" {
   value = "${aws_elb.router.name}"
 }
+
 output "web_access_cidrs" {
   value = "${var.web_access_cidrs}"
 }
