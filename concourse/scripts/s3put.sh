8fc1a33c0f9822597a3eb70d8504e945e771430b
Timothy Mower <timothy.mower@gmail.com>
2016-01-12 17:14:29 +0000

    Remove insecure deployer for bosh-init
    Bosh init needs ssh to create a tunnel for the agent on the stemcell
    bosh init create to connect to the director running on bosh init. To do
    this it requires a keypair. However, it only requires this keypair
    during the initial deploy, as a redeploy will recreate the instance. So
    it is safe to generate a new keypair each time we run a deployment. It
    is also safe to run terraform without saving the state file between runs
    - due to the dummy state file, terraform will see the resource as
    requiring changes and destroy and recreate it.
    
    For the case that we need to ssh to the bosh init director without
    redeploying, we upload the key to s3 so it can be retrieved if needed.
    The role given to the concourse vm running bosh init should only allow
    write access to the bucket.

---
+++ concourse/scripts/s3put.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+bucket=$1
+file=$2
+
+# Attempt to use instance profile if keys not configured
+if [ -z "${AWS_SECRET_ACCESS_KEY}" ] && [ -z ${AWS_ACCESS_KEY_ID} ] ; then
+  meta_url="http://169.254.169.254/latest/meta-data/iam/security-credentials/"
+  profile_name=$(curl -s ${meta_url})
+  instance_profile=$(curl -s ${meta_url}/${profile_name})
+
+      AWS_ACCESS_KEY_ID=$(echo "${instance_profile}" | awk -F\" '$2 == "AccessKeyId"     { print $4 }')
+     AWS_SECURITY_TOKEN=$(echo "${instance_profile}" | awk -F\" '$2 == "Token"           { print $4 }')
+  AWS_SECRET_ACCESS_KEY=$(echo "${instance_profile}" | awk -F\" '$2 == "SecretAccessKey" { print $4 }')
+
+  [ -z "${AWS_SECURITY_TOKEN}" ] && echo "Could not obtain AWS_SECURITY_TOKEN from instance profile" && exit 105
+fi
+
+[ -z "${bucket}" ]    && echo "Must provide bucket name"    && exit 100
+[ -z "${file}" ]      && echo "Must provide file name"      && exit 101
+[ -z "${AWS_ACCESS_KEY_ID}"     ] && echo "AWS_ACCESS_KEY_ID nor specified or present in instance profile"     && exit 103
+[ -z "${AWS_SECRET_ACCESS_KEY}" ] && echo "AWS_SECRET_ACCESS_KEY nor specified or present in instance profile" && exit 104
+
+aws_path=/
+content_type='application/x-compressed-tar'
+acl="x-amz-acl:private"
+region=eu-west-1
+host=${bucket}.s3-${region}.amazonaws.com
+
+sign() {
+  string=$1
+  printf "${string}" | openssl sha1 -hmac "${AWS_SECRET_ACCESS_KEY}" -binary | base64
+}
+
+put() {
+  date=$(date +"%a, %d %b %Y %T %z")
+  string="PUT\n\n${content_type}\n${date}\n${acl}\n${AWS_SECURITY_TOKEN:+x-amz-security-token:$AWS_SECURITY_TOKEN\n}/${bucket}${aws_path}${file}"
+  signature=$(sign "${string}")
+  curl -i -s -X PUT -T ${file} \
+    -H "Host: ${bucket}.s3.amazonaws.com" \
+    -H "Date: ${date}" \
+    -H "Content-Type: ${content_type}" \
+    -H "${acl}" \
+    ${AWS_SECURITY_TOKEN:+-H "x-amz-security-token: ${AWS_SECURITY_TOKEN}"} \
+    -H "Authorization: AWS ${AWS_ACCESS_KEY_ID}:${signature}" \
+    "https://${host}${aws_path}${file}"
+}
+
+put
