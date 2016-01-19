913eeed1a990aaa696eaf19cba60274e71ce44b7
Colin Saliceti <c.saliceti@gmail.com>
2016-01-19 15:44:56 +0000

    Script to generate CF secrets

---
+++ concourse/scripts/generate-cf-secrets.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -e
+
+SCRIPTS_DIR=`dirname $0`
+
+simplepass() {
+  pwgen -1s 12
+}
+
+vcap_password_orig=$(simplepass)
+vcap_password=$(echo ${vcap_password_orig} | mkpasswd -m sha-512)
+
+cat <<EOF
+---
+  secrets:
+    vcap_password_orig: ${vcap_password_orig}
+    vcap_password: ${vcap_password}
+    postgres_password: $(simplepass)
+    ccadmin_password: $(simplepass)
+    uuadmin_password: $(simplepass)
+    staging_upload_password: $(simplepass)
+    bulk_api_password: $(simplepass)
+    nats_password: $(simplepass)
+    router_password: $(simplepass)
+    uaa_batch_password: $(simplepass)
+    uaa_clients_firehose_password: $(simplepass)
+    uaa_admin_password: $(simplepass)
+    cc_db_encryption_key: $(simplepass)
+    uaa_admin_client_secret: $(simplepass)
+    uaa_batch_password: $(simplepass)
+    uaa_cc_client_secret: $(simplepass)
+    uaa_cc_routing_secret: $(simplepass)
+    uaa_clients_app_direct_secret: $(simplepass)
+    uaa_clients_developer_console_secret: $(simplepass)
+    uaa_clients_login_secret: $(simplepass)
+    uaa_clients_notifications_secret: $(simplepass)
+    uaa_clients_doppler_secret: $(simplepass)
+    uaa_clients_cloud_controller_username_lookup_secret: $(simplepass)
+    uaa_clients_gorouter_secret: $(simplepass)
+    uaa_clients_firehose_password: $(simplepass)
+    uaa_admin_password: $(simplepass)
+    loggregator_endpoint_shared_secret: $(simplepass)
+    consul_encrypt_keys:
+    - $(simplepass)
+EOF
