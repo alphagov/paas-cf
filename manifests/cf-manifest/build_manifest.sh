00785b406a0e99926303d3d1667c6fca8c2c96b2
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2016-01-22 14:33:10 +0000

    Fix shellcheck in cf-manifest/build_manifest.sh
    SC2086: Double quote to prevent globbing and word splitting.
    
    This has required refactoring the cf-manifest/build_manifest.sh to take
    a terraform outputs directory instead of a single file. This proved
    problematic because we needed to support globbing of the variable
    contents which is something shellcheck complains about.

--- manifests/cf-manifest/build_manifest.sh
+++ manifests/cf-manifest/build_manifest.sh
@@ -3,7 +3,7 @@
 set -e
 
 manifests_dir=${MANIFESTS_DIR:-"./"}
-terraform_outputs=${TERRAFORM_OUTPUTS:-"${manifests_dir}/outputs/terraform-outputs.yml"}
+terraform_output_dir=${TERRAFORM_OUTPUT_DIR:-"${manifests_dir}/outputs/terraform"}
 secrets=${SECRETS:-"${manifests_dir}/outputs/cf-secrets.yml"}
 ssl_certs=${SSL_CERTS:-"${manifests_dir}/outputs/cf-ssl-certificates.yml"}
 
@@ -11,8 +11,8 @@ spruce merge \
   --prune meta --prune lamb_meta \
   --prune terraform_outputs \
   --prune secrets \
-  ${manifests_dir}/deployments/*.yml \
-  ${manifests_dir}/deployments/aws/*.yml \
-  ${terraform_outputs} \
-  ${secrets} \
-  ${ssl_certs}
+  "${manifests_dir}"/deployments/*.yml \
+  "${manifests_dir}"/deployments/aws/*.yml \
+  "${terraform_output_dir}"/*.yml \
+  "${secrets}" \
+  "${ssl_certs}"
