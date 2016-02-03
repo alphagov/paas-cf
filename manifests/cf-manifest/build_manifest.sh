e0361723d3184a918b6d373de7ab0b75cecbc406
Hector Rivas Gandara <keymon@gmail.com>
2016-02-03 16:00:31 +0000

    deploy-cf: Generate uaa_jwt_signing cert
    Generate the uaa_jwt_signing key. In this case we only need the key, not
    the certificate, but we generate it with the same logic.
    
    In an additional task the public part of the key is extracted, so it can be
    used as vertification key for the UAA tokens.
    
    More info: https://github.com/cloudfoundry/uaa/blob/master/docs/Sysadmin-Guide.rst

--- manifests/cf-manifest/build_manifest.sh
+++ manifests/cf-manifest/build_manifest.sh
@@ -7,6 +7,7 @@ terraform_output_dir=${TERRAFORM_OUTPUT_DIR:-"${manifests_dir}/outputs/terraform
 secrets=${SECRETS:-"${manifests_dir}/outputs/cf-secrets.yml"}
 ssl_certs=${SSL_CERTS:-"${manifests_dir}/outputs/cf-ssl-certificates.yml"}
 ssl_certs_dir=${SSL_CERTS_DIR:-}
+uaa_jwt_verification_key=${UAA_JWT_VERIFICATION_KEY:-}
 
 spruce merge \
   --prune meta --prune lamb_meta \
@@ -17,4 +18,5 @@ spruce merge \
   "${terraform_output_dir}"/*.yml \
   "${secrets}" \
   "${ssl_certs}" \
-  ${ssl_certs_dir:+"${ssl_certs_dir}"/*.yml}
+  ${ssl_certs_dir:+"${ssl_certs_dir}"/*.yml} \
+  ${uaa_jwt_verification_key:+"${uaa_jwt_verification_key}"}
