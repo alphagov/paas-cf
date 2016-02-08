8d4b499c164a4faec3aa46b9db848279cce0a219
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2016-02-08 10:50:55 +0000

    Create acceptance-test suite setup
    Add the initial ginkgo setup.

---
+++ acceptance-tests/src/acceptance/init_test.go
@@ -0,0 +1,13 @@
+package acceptance_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Acceptance tests")
+}
