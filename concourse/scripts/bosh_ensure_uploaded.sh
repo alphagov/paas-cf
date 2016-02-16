fa2a65e5f5407b63a5834db66399232887c663f1
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2016-02-16 12:15:42 +0000

    Use new bosh-cli container everywhere.
    Now that we have this container available, it makes sense to use it
    everywhere we need the bosh CLI. This avoids using the bosh resouece
    container, which isn't intended to be used in this way.
    
    This required a fix to the bosh_ensure_uploaded.sh script as it was
    using a bashism, but had /bin/sh in the #! line. In the new container,
    /bin/sh is no longer bash, so this was erroring.

--- concourse/scripts/bosh_ensure_uploaded.sh
+++ concourse/scripts/bosh_ensure_uploaded.sh
@@ -8,7 +8,7 @@ bosh login admin "${BOSH_PASSWORD}" >/dev/null
 
 existing_version=$("${script_dir}/bosh_list_${TYPE}s.rb" | awk -v name="${NAME}" -F"/" '$1 ~ name {print $2}')
 
-if [[ "${existing_version}" != "${VERSION}" ]]; then
+if [ "${existing_version}" != "${VERSION}" ]; then
   eval bosh upload "${TYPE}" "${URL}"
 else
   echo "${NAME}/${VERSION} is already uploaded"
