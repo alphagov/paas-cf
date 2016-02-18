8db0cc2678beb9f2e8dc1ca6bf8c6a28f1e45939
Martin Jackson <mjackson@equalexperts.com>
2016-02-18 14:54:14 +0000

    Added ACL for router
    Added ACL for router and fixed white space, also added tagging to make
    things more obvious to see in the AWS console.

--- terraform/cloudfoundry/network_acls.tf
+++ terraform/cloudfoundry/network_acls.tf
@@ -1,9 +1,23 @@
 resource "aws_network_acl" "cf" {
-    vpc_id = "${var.vpc_id}"
-    subnet_ids = ["${aws_subnet.cf.*.id}"]
+  vpc_id = "${var.vpc_id}"
+  subnet_ids = ["${aws_subnet.cf.*.id}"]
+  tags {
+    Name = "${var.env}-cf-acl"
+  }
 }
 
 resource "aws_network_acl" "cell" {
-    vpc_id = "${var.vpc_id}"
-    subnet_ids = ["${aws_subnet.cell.*.id}"]
+  vpc_id = "${var.vpc_id}"
+  subnet_ids = ["${aws_subnet.cell.*.id}"]
+  tags {
+    Name = "${var.env}-cell-acl"
+  }
+}
+
+resource "aws_network_acl" "router" {
+  vpc_id = "${var.vpc_id}"
+  subnet_ids = ["${aws_subnet.router.*.id}"]
+  tags {
+    Name = "${var.env}-router-acl"
+  }
 }
