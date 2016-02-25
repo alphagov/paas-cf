44c4f2be14c70dc22f048abb615cc6b634abdab7
Hector Rivas Gandara <keymon@gmail.com>
2016-02-25 11:45:40 +0000

    Bosh: create RDS instance in terraform
    Use terraform to setup an RDS instance for the bosh state.

--- terraform/bosh/outputs.tf
+++ terraform/bosh/outputs.tf
@@ -45,3 +45,23 @@ output "compiled_cache_bucket_name" {
 output "bosh_blobstore_bucket_name" {
   value = "${aws_s3_bucket.bosh-blobstore.id}"
 }
+
+output "bosh_db_address" {
+  value = "${aws_db_instance.bosh.address}"
+}
+
+output "bosh_db_port" {
+  value = "${aws_db_instance.bosh.port}"
+}
+
+output "bosh_db_username" {
+  value = "${aws_db_instance.bosh.username}"
+}
+
+output "bosh_db_password" {
+  value = "${aws_db_instance.bosh.password}"
+}
+
+output "bosh_db_dbname" {
+  value = "${aws_db_instance.bosh.name}"
+}
--- terraform/bosh/variables.tf
+++ terraform/bosh/variables.tf
@@ -2,3 +2,18 @@ variable "concourse_security_group_id" {
   description = "id of the security group for the concourse VM which deploys bosh with bosh-init"
 }
 
+variable "secrets_bosh_db_master_password" {
+  description = "Master password for bosh database"
+  # FIXME
+  default = "changeme"
+}
+
+variable "bosh_db_multi_az" {
+  description = "BOSH database multi availabiliy zones"
+  default = "false"
+}
+
+variable "bosh_db_backup_retention_period" {
+  description = "BOSH database backup retention period"
+  default = 0
+}
