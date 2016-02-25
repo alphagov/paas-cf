f4e2f26e9b2d2cb09968bef2c14bf505143fcc13
Colin Saliceti <c.saliceti@gmail.com>
2016-02-25 15:42:45 +0000

    Init database pipeline tasks
    The previous postgres job was doing some work to initialise
    the databases: create roles, create databases, enable extensions.
    
    Most of this work should have been done with the postgres terraform
    provider, but it has bugs and can't destroy the resources properly:
    https://github.com/hashicorp/terraform/issues/5340
    
    When this is fixed, we can move the create role and db tasks to
    terraform, but we still need to enable extensions.

---
+++ manifests/cf-manifest/scripts/create-cf-dbs.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+# Check environment variables
+export PGPASSWORD=${TF_VAR_secrets_cf_db_master_password:?}
+api_pass=${TF_VAR_secrets_cf_db_api_password:?}
+uaa_pass=${TF_VAR_secrets_cf_db_uaa_password:?}
+db_address=${TF_VAR_cf_db_address:?}
+
+# See: https://github.com/koalaman/shellcheck/wiki/SC2086#exceptions
+psql_adm() { psql -h "${db_address}" -U dbadmin "$@"; }
+
+# Create roles
+psql_adm -d postgres -c "SELECT rolname FROM pg_roles WHERE rolname = 'api'" \
+  | grep -q 'api' || psql_adm -d postgres \
+  -c "CREATE USER api WITH PASSWORD '${api_pass}' ROLE dbadmin"
+
+psql_adm -d postgres -c "SELECT rolname FROM pg_roles WHERE rolname = 'uaa'" \
+  | grep -q 'uaa' || psql_adm -d postgres \
+  -c "CREATE USER uaa WITH PASSWORD '${uaa_pass}' ROLE dbadmin"
+
+for db in api uaa; do
+
+  # Create database
+  psql_adm -d postgres -l | grep -q " ${db} " || \
+    psql_adm -d postgres -c "CREATE DATABASE ${db} OWNER ${db}"
+
+  # Enable extensions
+  for ext in citext pgcrypto pg_stat_statements; do
+    psql_adm -d "${db}" -c "CREATE EXTENSION IF NOT EXISTS ${ext}"
+  done
+
+done
