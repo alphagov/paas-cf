e18e80b5b56797dd98f6c20764b81027e1fc60a3
Michal Tekel <mtekel@gmail.com>
2016-03-22 16:25:14 +0000

    Test for closed http
    Add a test that verifies that http is closed for both API and apps.
    We have picked 11 second timeout as not too long to delay tests for too
    much and also not too short to be caused by random events like concourse
    being too slow (when it launches 5 parallel tests), internet interfering
    with TCP packets (between councourse and ELB) or even ELB having some
    random hiccup.

--- tests/acceptance-tests/generate_test_config.rb
+++ tests/acceptance-tests/generate_test_config.rb
@@ -9,12 +9,14 @@ manifest = YAML.load_file(manifest_file)
 admin_password = manifest.fetch("properties").fetch("acceptance_tests").fetch("admin_password")
 api_url = manifest.fetch("properties").fetch("cc").fetch("srv_api_uri")
 apps_domain = manifest.fetch("properties").fetch("app_domains").first
+system_domain = manifest.fetch("properties").fetch("system_domain")
 
 config = {
   "api" => api_url,
   "admin_user" => "admin",
   "admin_password" => admin_password,
   "apps_domain" => apps_domain,
+  "system_domain" => system_domain,
   "use_http" => false,
 }
 puts config.to_json
---
+++ tests/acceptance-tests/src/acceptance/http_closed_test.go
@@ -0,0 +1,44 @@
+package acceptance_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
+
+	"net"
+	"time"
+)
+
+var _ = Describe("Http is closed", func() {
+
+	var (
+		CONNECTION_TIMEOUT = 11 * time.Second
+	)
+
+	It("for apps", func() {
+		appName := generator.PrefixedRandomName("CATS-APP-")
+		Expect(cf.Cf(
+			"push", appName,
+			"--no-start",
+			"-b", config.StaticFileBuildpackName,
+			"-p", "../../example_apps/static_app",
+			"-d", config.AppsDomain,
+		).Wait(CF_PUSH_TIMEOUT)).To(Exit(0))
+		Expect(cf.Cf("start", appName).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+
+		uri := appName + "." + config.AppsDomain + ":80"
+		_, err := net.DialTimeout("tcp", uri, CONNECTION_TIMEOUT)
+		Expect(err.(net.Error).Timeout()).To(BeTrue())
+	})
+
+	It("for api", func() {
+		uri := "api." + config.SystemDomain + ":80"
+		_, err := net.DialTimeout("tcp", uri, CONNECTION_TIMEOUT)
+		Expect(err.(net.Error).Timeout()).To(BeTrue())
+
+	})
+
+})
