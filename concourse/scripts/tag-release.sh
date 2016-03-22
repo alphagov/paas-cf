11a50ba9a9b211a4a5ece658e3f52d827c1180d8
Piotr Komborski <piotr.komborski@gmail.com>
2016-03-22 17:19:44 +0000

    Use functions for tag versioning operations
    For code clarity, this has been rewritten using functions for
    get_tag, promote_tag and create_tag.

--- concourse/scripts/tag-release.sh
+++ concourse/scripts/tag-release.sh
@@ -2,9 +2,10 @@
 
 set -eu -o pipefail
 
-TAG_PREFIX=$1
-AWS_ACCOUNT=$2
-DEPLOY_ENV=$3
+TAG_PREFIX="${1}"
+AWS_ACCOUNT="${2}"
+DEPLOY_ENV="${3}"
+TAG_FILTER="${4:-""}"
 
 GIT_EMAIL="the-multi-cloud-paas-team+deployer-ci@digital.cabinet-office.gov.uk"
 GIT_USER="gov-paas-${AWS_ACCOUNT}"
@@ -20,21 +21,37 @@ Host github.com
   IdentityFile $(pwd)/git-key
 EOF
 
-cd paas-cf
-if [ -n "${PAAS_CF_TAG_FILTER}" ]
-then 
-  tag="$(git tag -l --contains HEAD | grep "${PAAS_CF_TAG_FILTER}" | sed s/"${PAAS_CF_TAG_FILTER%?}"/"${TAG_PREFIX}"/)"
-else
-  version=$(cat ../release-version/number)
-  tag="${TAG_PREFIX}${version}"
-fi
+get_tag(){
+  tag_filter=${1}
+  git tag -l --contains HEAD | grep "${tag_filter}"
+}
 
+promote_existing_tag(){
+  existing_tag=${1}
+  echo "${existing_tag}" | sed s/"${TAG_FILTER%?}"/"${TAG_PREFIX}"/
+}
+
+create_new_tag(){
+  version=$(cat ../release-version/number)
+  echo "${TAG_PREFIX}${version}"
+}
+
+cd paas-cf
 echo Configure Git
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_USER}"
 git remote add ssh "${GIT_REPO_URL}"
 
-echo "Create tag ${tag}"
+if [ -n "${TAG_FILTER}" ]
+then
+  latest_tag=$(get_tag "${TAG_FILTER%?}")
+  tag=$(promote_existing_tag "${latest_tag}")
+  echo "Promote ${latest_tag} to ${tag}"
+else
+  tag=$(create_new_tag)
+  echo "Create new tag ${tag}"
+fi
+
 git tag -a "${tag}" -m "Tag ${tag} passed ${AWS_ACCOUNT} \
 in environment ${DEPLOY_ENV}"
 
