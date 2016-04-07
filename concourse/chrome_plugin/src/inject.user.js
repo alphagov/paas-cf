52cdce24d918afc287b6b6970a211563733925af
Jonty Wareing <jonty@jonty.co.uk>
2016-04-07 12:16:02 +0100

    Make the chrome extension userscript compatible

---
+++ concourse/chrome_plugin/src/inject.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Remove concourse elements
+// @namespace   cloudpipeline.digital
+// @description Strips away some of the cruft from the concourse pipeline view when showing it on monitoring screens.
+// @include     https://deployer.*.cloudpipeline.digital/*
+// @include     https://deployer.cloud.service.gov.uk/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+
+        console.log("Monitor mode is go");
+        var element = document.getElementsByClassName("legend")[0];
+        element.parentNode.removeChild(element);
+
+        var element = document.getElementById("cli-downloads");
+        element.parentNode.removeChild(element);
+
+        var hostname = location.hostname.replace("deployer.", "").replace(".cloudpipeline.digital","")
+
+        var element = document.getElementsByTagName("nav")[0];
+        element.innerHTML = "&nbsp;<font size=5>" + hostname + "</font>";
+    }
+}, 10);
