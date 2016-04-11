03e7dc7f912b8fb0709792c38a457cb39d41d3ef
Hector Rivas Gandara <keymon@gmail.com>
2016-04-11 15:39:36 +0100

    Add API SG group and dedicated ELB for cc
    Add a dedicated ELB for the Cloud Foundry CloudController and a
    CF API security group to allow access to the API endpoint to only
    the office IPs, the NAT nodes and concourse.
    
    The new ELB will use the same configuration than the go-router ELB, but
    use the API nodes as backend. Cloud Controller listens in port 9022,
    without SSL. Health check can be done in the endpoint /info.
    
    CC does not require WebSockets, so we can enable the HTTP protocol in
    the ELB configuration.

---
+++ terraform/cloudfoundry/cf_api_elb.tf
@@ -0,0 +1,24 @@
+resource "aws_elb" "cf_cc" {
+  name = "${var.env}-cf-cc"
+  subnets = ["${split(",", var.infra_subnet_ids)}"]
+  idle_timeout = "${var.elb_idle_timeout}"
+  cross_zone_load_balancing = "true"
+  security_groups = [
+    "${aws_security_group.cf_api_elb.id}",
+  ]
+
+  health_check {
+    target = "HTTP:9022/info"
+    interval = "${var.health_check_interval}"
+    timeout = "${var.health_check_timeout}"
+    healthy_threshold = "${var.health_check_healthy}"
+    unhealthy_threshold = "${var.health_check_unhealthy}"
+  }
+  listener {
+    instance_port = 9022
+    instance_protocol = "http"
+    lb_port = 443
+    lb_protocol = "https"
+    ssl_certificate_id = "${var.router_external_cert_arn}"
+  }
+}
