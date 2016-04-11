0cd07ddb94dee49b449c6ed32084bc18d64b67fd
Hector Rivas Gandara <keymon@gmail.com>
2016-04-11 16:07:50 +0100

    Remove wildcard DNS for CF API services
    Now each service uses its own ELB and we explicily declare each DNS
    entry for these services. Now we don't need a wildcard name for
    all the services.

--- terraform/cloudfoundry/dns-records.tf
+++ terraform/cloudfoundry/dns-records.tf
@@ -1,11 +1,3 @@
-resource "aws_route53_record" "system_wildcard" {
-  zone_id = "${var.system_dns_zone_id}"
-  name = "*.${var.system_dns_zone_name}."
-  type = "CNAME"
-  ttl = "60"
-  records = ["${aws_elb.router.dns_name}"]
-}
-
 resource "aws_route53_record" "cf_cc" {
   zone_id = "${var.system_dns_zone_id}"
   name = "api.${var.system_dns_zone_name}."
