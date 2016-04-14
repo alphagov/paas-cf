2933579e0d5481743dfc0cc5596b5fb9d7f21178
Hector Rivas Gandara <keymon@gmail.com>
2016-04-14 11:27:34 +0100

    Add test for tag-release.sh using rspec/aruba
    Add a test around tag-release.sh, using the helpers and library for
    aruba on rspec.
    
    This test prepares a fake origin repository with tags, for which it uses
    the helper bash_c to execute a batch of shell commands using aruba, and
    mocks the state the script will find in concourse.
    
    The script had to be changed to adapt it to the test and to be able to
    run test it locally and in travis, like add the option of defining an
    alternate remote url, or use bash rather than sh.

--- concourse/scripts/tag-release.sh
+++ concourse/scripts/tag-release.sh
@@ -1,17 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-set -eu -o pipefail
+set -eu
+set -o pipefail
 
 TAG_PREFIX="${1}"
 AWS_ACCOUNT="${2}"
 DEPLOY_ENV="${3}"
 TAG_FILTER="${4:-""}"
 
-GIT_PUSH="${GIT_PUSH:-true}" # Push the repo at the end or not
-
 GIT_EMAIL="the-multi-cloud-paas-team+deployer-ci@digital.cabinet-office.gov.uk"
 GIT_USER="gov-paas-${AWS_ACCOUNT}"
-GIT_REPO_URL="git@github.com:alphagov/paas-cf.git"
+GIT_REPO_URL="${GIT_REPO_URL:-git@github.com:alphagov/paas-cf.git}"
 
 echo Configure SSH
 tar xzf git-keys/git-keys.tar.gz
@@ -54,7 +53,7 @@ check_already_tagged "${TAG_PREFIX}"
 echo Configure Git
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_USER}"
-git remote add ssh "${GIT_REPO_URL}"
+git remote add tag_origin "${GIT_REPO_URL}"
 
 if [ -n "${TAG_FILTER}" ]
 then
@@ -69,7 +68,5 @@ fi
 git tag -a "${tag}" -m "Tag ${tag} passed ${AWS_ACCOUNT} \
 in environment ${DEPLOY_ENV}"
 
-if [ "${GIT_PUSH}" = "true" ]; then
-  echo "Push tag ${tag}"
-  git push ssh "${tag}"
-fi
+echo "Push tag ${tag}"
+git push tag_origin "${tag}"
