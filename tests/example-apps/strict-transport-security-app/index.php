d3d74de7b4ceeeb12368ad4a3fb55b2590c63196
Hector Rivas Gandara <keymon@gmail.com>
2016-04-22 14:30:25 +0100

    Added test for Strict-Transport-Security headers
    Add two cases:
     * Default header must be added if the application does not return
       a Strict-Transport-Security header. We use the example static
       application.
    
       We had to add the Staticfile to this application so it can be
       deployed propertly.
    
     * Respect the existing header if the application returns it, for
       which we use the new application strict-transport-security-app.
    
       This application adds a Strict-Transport-Security
       header with a value which is different to what we set in
       HAProxy.
    
    We use the cf-test-helpers CurlApp function, but asking to get the
    headers by passing the `-I` options, which are then parse witha
    net.textproto.Reader.ReadMIMEHeader()

---
+++ tests/example-apps/strict-transport-security-app/index.php
@@ -0,0 +1,17 @@
+<?php
+header('Cache-Control: max-age=0,no-store,no-cache');
+header('Strict-Transport-Security: max-age=24');
+?>
+<!DOCTYPE html PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<HTML>
+  <HEAD>
+    <meta http-equiv="Cache-Control" content="no-store" />
+    <TITLE>
+      Strict Transport Security
+    </TITLE>
+  </HEAD>
+  <BODY>
+    <H1>Hi</H1>
+    <P>This app exists to test whether we are allowing apps to set their own Strict-Transport-Security header</P>
+  </BODY>
+</HTML>
