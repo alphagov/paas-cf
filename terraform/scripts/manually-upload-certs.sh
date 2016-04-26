02623227ad84e1e67ad5774375db302346e9b833
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2016-04-26 12:56:48 +0100

    Extract manually-upload-certs script from Makefile
    This makes it easier to maintain (avoids some of the make oddities), and
    it can be shellchecked etc.

---
+++ terraform/scripts/manually-upload-certs.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -eu
+
+export PASSWORD_STORE_DIR=${CERT_PASSWORD_STORE_DIR}
+
+pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/system_domain.crt" > /dev/null
+pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/system_domain.key" > /dev/null
+pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain.crt" > /dev/null
+pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain.key" > /dev/null
+
+if ! aws s3 ls "s3://${DEPLOY_ENV}-state/cf-certs.tfstate" --summarize | grep -q "Total Objects: 0"; then
+  aws s3 cp "s3://${DEPLOY_ENV}-state/cf-certs.tfstate" cf-certs.tfstate
+else
+  echo "No previous cf-certs.tfstate file found in s3://${DEPLOY_ENV}-state/. Assuming first run."
+fi
+
+terraform apply -var env="${DEPLOY_ENV}" \
+  -var-file="terraform/${AWS_ACCOUNT}.tfvars" \
+  -state=cf-certs.tfstate \
+  -var system_domain_crt="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/system_domain.crt")" \
+  -var system_domain_key="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/system_domain.key")" \
+  -var apps_domain_crt="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain.crt")" \
+  -var apps_domain_key="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain.key")" \
+  terraform/cf-certs
+
+aws s3 cp cf-certs.tfstate "s3://${DEPLOY_ENV}-state/cf-certs.tfstate"
+rm -f cf-certs.tfstate
