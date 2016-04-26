d62b88d97a0b242887a6dfbffb7a9adc536f5dff
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2016-04-26 14:26:55 +0100

    Use temp dir for manually-upload-certs state files
    Makes the cleanup easier (avoids leaving the .backup file behind etc).

--- terraform/scripts/manually-upload-certs.sh
+++ terraform/scripts/manually-upload-certs.sh
@@ -9,20 +9,22 @@ pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/system_domain.key" > /dev/null
 pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain.crt" > /dev/null
 pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain.key" > /dev/null
 
+WORKING_DIR=$(mktemp -d cf-certs.XXXXXX)
+
 if ! aws s3 ls "s3://${DEPLOY_ENV}-state/cf-certs.tfstate" --summarize | grep -q "Total Objects: 0"; then
-  aws s3 cp "s3://${DEPLOY_ENV}-state/cf-certs.tfstate" cf-certs.tfstate
+  aws s3 cp "s3://${DEPLOY_ENV}-state/cf-certs.tfstate" "${WORKING_DIR}/cf-certs.tfstate"
 else
   echo "No previous cf-certs.tfstate file found in s3://${DEPLOY_ENV}-state/. Assuming first run."
 fi
 
 terraform apply -var env="${DEPLOY_ENV}" \
   -var-file="terraform/${AWS_ACCOUNT}.tfvars" \
-  -state=cf-certs.tfstate \
+  -state="${WORKING_DIR}/cf-certs.tfstate" \
   -var system_domain_crt="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/system_domain.crt")" \
   -var system_domain_key="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/system_domain.key")" \
   -var apps_domain_crt="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain.crt")" \
   -var apps_domain_key="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain.key")" \
   terraform/cf-certs
 
-aws s3 cp cf-certs.tfstate "s3://${DEPLOY_ENV}-state/cf-certs.tfstate"
-rm -f cf-certs.tfstate
+aws s3 cp "${WORKING_DIR}/cf-certs.tfstate" "s3://${DEPLOY_ENV}-state/cf-certs.tfstate"
+rm -r "${WORKING_DIR}"
