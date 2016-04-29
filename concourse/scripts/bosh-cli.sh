536c9ec35a1941dea15f6b8e073a50b7310cee28
Piotr Komborski <piotr.komborski@gmail.com>
2016-04-29 16:50:22 +0100

    Fix bosh-cli task and bosh-login
    As bosh_cli relies on bosh-login (and we use it in Concourse pipelines)
    it needs to be fixed to use SYSTEM_DNS_ZONE_NAME variable to access
    bosh instance.

--- concourse/scripts/bosh-cli.sh
+++ concourse/scripts/bosh-cli.sh
@@ -13,10 +13,37 @@ $("${SCRIPT_DIR}/environment.sh")
 OUTPUT_FILE=$(mktemp -t bosh-cli.XXXXXX)
 trap 'rm -f "${OUTPUT_FILE}"' EXIT
 
+generate_config(){
+  cat <<EOF
+---
+platform: linux
+
+image: docker:///governmentpaas/bosh-cli
+
+inputs:
+  - name: paas-cf
+  - name: cf-manifest
+  - name: bosh-secrets
+run:
+  path: sh
+  args:
+  - -c
+  - -e
+  - |
+    ./paas-cf/concourse/scripts/bosh_login.sh bosh.${SYSTEM_DNS_ZONE_NAME} bosh-secrets/bosh-secrets.yml
+
+    uuid=\$(bosh status --uuid)
+    sed -e "s/^director_uuid:.*$/director_uuid: \${uuid}/" cf-manifest/cf-manifest.yml > cf-manifest-with-uuid.yml
+    bosh deployment ./cf-manifest-with-uuid.yml
+EOF
+}
+
+generate_config > /dev/null
+
 $FLY_CMD -t "${FLY_TARGET}" \
   execute \
   --inputs-from=create-bosh-cloudfoundry/cf-deploy \
-  --config="${SCRIPT_DIR}/../pipelines/bosh-cli/bosh-cli.yml" \
+  --config=<(generate_config) \
   | tee "${OUTPUT_FILE}"
 
 BUILD_NUMBER=$(awk '/executing build/ { print $3 }' "${OUTPUT_FILE}")
--- concourse/scripts/bosh_login.sh
+++ concourse/scripts/bosh_login.sh
@@ -2,9 +2,9 @@
 
 set -eu
 
-bosh_secrets_file=$1
-bosh_ip=${BOSH_IP-"10.0.0.6"}
+bosh_host=$1
+bosh_secrets_file=$2
 
 bosh_password=$(awk '/bosh_admin_password/ { print $2 }' "${bosh_secrets_file}")
-bosh -t "${bosh_ip}" login admin "${bosh_password}"
-bosh target "${bosh_ip}"
+bosh -t "${bosh_host}" login admin "${bosh_password}"
+bosh target "${bosh_host}"
