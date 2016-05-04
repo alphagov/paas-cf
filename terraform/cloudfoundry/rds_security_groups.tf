181a01bb1784ed233771fdb9390d719ee8e80495
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2016-05-04 16:24:01 +0100

    Reorganise CF RDS terraform files.
    We're about to add more RDS related config, so we rename these to
    disambiguate.

--- terraform/cloudfoundry/rds_security_groups.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_security_group" "cf_rds_client" {
-  name = "${var.env}-cf-rds-client"
-  description = "Security group of the CF RDS clients"
-  vpc_id = "${var.vpc_id}"
-
-  tags {
-    Name = "${var.env}-cf-rds-client"
-  }
-}
