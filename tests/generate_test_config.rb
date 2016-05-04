86932e4904725d70cacc366c32df8bfdf5ae0c2e
Michal Tekel <mtekel@gmail.com>
2016-05-04 14:55:14 +0100

    Use tmp admin for custom acceptance and perf tests
    Create and destroy temporary user before and after tests.
    Use this user to run custom acceptance and performance tests.
    
    Use temporary admin credentials instead of getting real admin
    credentials from manifest.
    
    We do this because sometimes it was possible some tests leaked admin
    password on a specific failure. Hence switch to temporary user.

--- tests/generate_test_config.rb
+++ tests/generate_test_config.rb
@@ -6,14 +6,16 @@ require 'yaml'
 manifest_file = ENV.fetch("CF_MANIFEST")
 manifest = YAML.load_file(manifest_file)
 
-admin_password = manifest.fetch("properties").fetch("acceptance_tests").fetch("admin_password")
+admin_user = File.read('admin-creds/username').strip()
+admin_password = File.read('admin-creds/password').strip()
+
 api_url = manifest.fetch("properties").fetch("cc").fetch("srv_api_uri")
 apps_domain = manifest.fetch("properties").fetch("app_domains").first
 system_domain = manifest.fetch("properties").fetch("system_domain")
 
 config = {
   "api" => api_url,
-  "admin_user" => "admin",
+  "admin_user" => admin_user,
   "admin_password" => admin_password,
   "apps_domain" => apps_domain,
   "system_domain" => system_domain,
