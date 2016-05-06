ece03737920d507e0d45ce517c37b7849b99856e
Michal Tekel <mtekel@gmail.com>
2016-05-06 17:36:36 +0100

    Add -z option to tar in import_bosh_ca
    We are running now in various containers that have different tars.
    Add explicit -z option to tar so that it does not rely on compression
    format auto detection, which fails e.g. in bosh-cli container with
    `tar: invalid tar magic`

--- concourse/scripts/import_bosh_ca.sh
+++ concourse/scripts/import_bosh_ca.sh
@@ -2,5 +2,5 @@
 set -eu
 
 echo "Adding bosh-CA to root certificates"
-tar -xf bosh-CA/bosh-CA.tar.gz -C /usr/local/share/ca-certificates bosh-CA.crt
+tar -xzf bosh-CA/bosh-CA.tar.gz -C /usr/local/share/ca-certificates bosh-CA.crt
 update-ca-certificates
