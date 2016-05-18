cdcab37f2fe21d4b6e5229fda89de321c39c9b04
Hector Rivas Gandara <keymon@gmail.com>
2016-05-18 12:13:19 +0100

    Fix bosh_login.sh when password starts with '-'
    If the random generated password for the bosh admin starts with '-', the
    `bosh_login.sh` won't work because it recognises the password with an
    option.
    
    To avoid that we add '--' to tell bosh that the following parameters
    are not options.
    
    Additionally, if the string starts with '-', ruby yaml will quote the
    string in the cf-secrets.yml with `"`. We need to remote these quotes
    before passing the password as argument.

--- concourse/scripts/bosh_login.sh
+++ concourse/scripts/bosh_login.sh
@@ -5,6 +5,6 @@ set -eu
 bosh_host=$1
 bosh_secrets_file=$2
 
-bosh_password=$(awk '/bosh_admin_password/ { print $2 }' "${bosh_secrets_file}")
-bosh -t "${bosh_host}" login admin "${bosh_password}"
+bosh_password=$(awk '/bosh_admin_password/ { print $2 }' "${bosh_secrets_file}" | tr -d '"')
+bosh -t "${bosh_host}" login admin -- "${bosh_password}"
 bosh target "${bosh_host}"
