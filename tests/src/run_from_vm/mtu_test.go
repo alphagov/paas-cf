88b5d1a327c067705902e213ae74b11ba0cbae4a
Dan Carley <dan.carley@gmail.com>
2016-05-19 17:00:18 +0100

    tests/run_from_vm: Check MTU of host
    Test that the suite is being run from a host with a network interface
    configured with an MTU of 9000~, because that is needed to exercise the bug
    found by the other test in this suite.
    
    I didn't want to hardcode the interface name/index because it wouldn't be
    portable between operating system types (OSX and Linux), versions (udev
    random naming), or containers. This required quite a bit more code though,
    because it needs to connect to the endpoint in order to find out which local
    interface the connection is routed through. It also requires two steps
    because we get an IP address and then have to find the interface it belongs
    to.
    
    I used `http.Client` to make the connection and hijack the `net.Dial`
    functions because `net/http` has existing logic for converting the
    `http`/`https` schemes to the right port number whilst respecting any `:n`
    port at the end of the hostname.
    
    I'm allowing a variance of 10 when checking the MTU value because it's quite
    common to use a value of `9000` but AWS uses `9001`.

--- tests/src/run_from_vm/mtu_test.go
+++ tests/src/run_from_vm/mtu_test.go
@@ -2,8 +2,11 @@ package run_from_vm_test
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
+	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"time"
@@ -17,6 +20,7 @@ var _ = Describe("MTU", func() {
 		DefaultTimeout = time.Second
 		HeaderName     = "Authorization"
 		HeaderBase     = "bearer ="
+		JumboMTU       = 9000
 		ReqSizeMin     = 1300
 		ReqSizeMax     = 1500
 	)
@@ -28,6 +32,19 @@ var _ = Describe("MTU", func() {
 	BeforeEach(func() {
 		EndpointURL = os.Getenv("API_ENDPOINT")
 		Expect(EndpointURL).ToNot(BeEmpty(), "API_ENDPOINT environment variable must be set")
+	})
+
+	It("is running from a host configured to use jumbo frames", func() {
+		egressAddr, err := EgressAddr(EndpointURL, DefaultTimeout)
+		Expect(err).To(BeNil())
+
+		egressIP, _, err := net.SplitHostPort(egressAddr.String())
+		Expect(err).To(BeNil())
+
+		iface, err := InterfaceByAddr(egressIP)
+		Expect(err).To(BeNil())
+
+		Expect(iface.MTU).To(BeNumerically("~", JumboMTU, 10))
 	})
 
 	It("can access the endpoint with a variety of request sizes", func() {
@@ -55,3 +72,71 @@ var _ = Describe("MTU", func() {
 		}
 	})
 })
+
+// EgressAddr returns the address (host:port) of the network interface used
+// to connect to `endpointURL`.
+func EgressAddr(endpointURL string, timeout time.Duration) (net.Addr, error) {
+	var (
+		egressAddr   net.Addr
+		errConnAbort error = errors.New("connection aborted because we only wanted the local addr")
+	)
+
+	dialHijack := func(network, addr string) (net.Conn, error) {
+		conn, err := net.DialTimeout(network, addr, timeout)
+		if err != nil {
+			return conn, err
+		}
+
+		defer conn.Close()
+		egressAddr = conn.LocalAddr()
+
+		return nil, errConnAbort
+	}
+
+	client := &http.Client{
+		Timeout: timeout,
+		Transport: &http.Transport{
+			Dial:    dialHijack,
+			DialTLS: dialHijack,
+		},
+	}
+
+	req, err := http.NewRequest("GET", endpointURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = client.Do(req)
+	if err != nil {
+		if urlErr, ok := err.(*url.Error); !(ok && urlErr.Err == errConnAbort) {
+			return nil, err
+		}
+	}
+
+	return egressAddr, nil
+}
+
+// InterfaceByAddr returns the network interface that has the IP address
+// `ipAddr`
+func InterfaceByAddr(ipAddr string) (net.Interface, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return net.Interface{}, err
+	}
+
+	for _, iface := range ifaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			continue
+		}
+
+		for _, addr := range addrs {
+			ip, _, err := net.ParseCIDR(addr.String())
+			if err == nil && ip.String() == ipAddr {
+				return iface, nil
+			}
+		}
+	}
+
+	return net.Interface{}, fmt.Errorf("unable to find interface for IP %s", ipAddr)
+}
