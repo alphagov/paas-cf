c77d6e0088530d9cb88a5e45d48bfbee512ad905
Dan Carley <dan.carley@gmail.com>
2016-05-19 17:00:15 +0100

    tests/run_from_vm: Requests of different sizes
    We experienced a bug in CF on AWS whereby TPS was unable to query Doppler
    with an `Authorization` header that contained a username of a specific
    length.
    
    This test reproduces the same behaviour by making HTTP or HTTPS requests
    with increasingly large packet sizes near to the standard network MTU of
    1500 bytes. It should prevent the same from happening again in the future
    without us needing to debug the internal CF calls.
    
    It needs to be run from a VM that is configured with an MTU of 9000~ (aka.
    Jumbo Frames) and against an endpoint that is accessed via a NAT Gateway and
    ELB. It cannot be run from a garden-linux container (eg. Concourse or Diego)
    because the virtual interface presented to the container has an MTU of 1500
    regardless of whether the host is configured to use 9000.
    
    When run against CF's API endpoint the test currently fails at a request
    size of 1397 bytes. It doesn't continue on failure, but if it did, it would
    also fail with 1398~1400 and then succeed at 1401 or more.
    
    The request sizes described above and specified in the test aren't the same
    as packet size seen by tcpdump. This is because of the overhead added by
    IP+TCP and TLS (if in use). We can't compute the exact size without fiddling
    with raw sockets, so we try a range of sizes instead.
    
    The range of sizes needs to be tested from small to large and halted on the
    very first failure because we've seen the large requests can fix the problem
    for a temporary period of time.
    
    Hector wrote a TCP echo server which could also test this behaviour without
    fiddling with HTTP. However it would have been harder to integrate as a test
    in our pipeline because it would have required running the server code
    somewhere and configuring an ELB, whereas this test uses our existing
    endpoints.
    
    - https://gist.github.com/keymon/907ef1f942ee532a894c3fabe0aa03de

--- tests/src/run_from_vm/mtu_test.go
+++ tests/src/run_from_vm/mtu_test.go
@@ -1,13 +1,26 @@
 package run_from_vm_test
 
 import (
+	"bytes"
+	"fmt"
+	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("MTU", func() {
+	const (
+		DefaultTimeout = time.Second
+		HeaderName     = "Authorization"
+		HeaderBase     = "bearer ="
+		ReqSizeMin     = 1300
+		ReqSizeMax     = 1500
+	)
+
 	var (
 		EndpointURL string
 	)
@@ -15,5 +28,30 @@ var _ = Describe("MTU", func() {
 	BeforeEach(func() {
 		EndpointURL = os.Getenv("API_ENDPOINT")
 		Expect(EndpointURL).ToNot(BeEmpty(), "API_ENDPOINT environment variable must be set")
+	})
+
+	It("can access the endpoint with a variety of request sizes", func() {
+		req, err := http.NewRequest("GET", EndpointURL, nil)
+		Expect(err).To(BeNil())
+
+		var buf bytes.Buffer
+		req.Header.Set(HeaderName, HeaderBase)
+		req.Write(&buf)
+		baseSize := buf.Len()
+
+		client := &http.Client{Timeout: DefaultTimeout}
+		var padding string
+		for reqSize := ReqSizeMin; reqSize <= ReqSizeMax; reqSize++ {
+			By(fmt.Sprintf("using request size of %d bytes", reqSize))
+			padding = strings.Repeat("=", reqSize-baseSize)
+			req.Header.Set(HeaderName, HeaderBase+padding)
+
+			buf.Reset()
+			req.Write(&buf)
+			Expect(buf.Len()).To(Equal(reqSize))
+
+			_, err := client.Do(req)
+			Expect(err).To(BeNil(), "request failed with size of %d bytes", reqSize)
+		}
 	})
 })
