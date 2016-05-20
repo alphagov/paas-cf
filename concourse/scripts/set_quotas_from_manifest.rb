e055715cb0e01ed84d10228d9d337a57a005cde9
Alex Tomlins <alex.tomlins@digital.cabient-office.gov.uk>
2016-05-20 15:12:37 +0100

    Refactor quota setting task
    This was implemented in 2 separate places - a ruby script to extract
    details from the manifest, and an inline shell script in the pipeline to
    apply these details. There was also a bug whereby it couldn't add a
    quota that didn't already exist (cf update-quota errors if the quota
    doesn't exist).
    
    This refactors it into a single ruby script that reads the manifest and
    applies the quotas. This now follows the same pattern as the task to set
    the security groups.

---
+++ concourse/scripts/set_quotas_from_manifest.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'yaml'
+
+class QuotasSetter
+  def initialize(manifest)
+    @manifest = manifest
+  end
+
+  def apply!
+    @manifest.fetch("properties").fetch("cc").fetch("quota_definitions").each do |name, definition|
+      create_update_quota(name, definition)
+    end
+  end
+
+  private
+
+  def create_update_quota(name, definition)
+    args = []
+    definition.each do |param, value|
+      case param
+      when "memory_limit"
+        args << '-m' << "#{value}M"
+      when "total_services"
+        args << '-s' << value.to_s
+      when "total_routes"
+        args << '-r' << value.to_s
+      when "non_basic_services_allowed"
+        args << (value ? '--allow-paid-service-plans' : '--disallow-paid-service-plans')
+      end
+    end
+    if existing_quotas.include?(name)
+      cf("update-quota", name, *args)
+    else
+      cf("create-quota", name, *args)
+    end
+  end
+
+  def existing_quotas
+    @existing_quotas ||= fetch_quotas
+  end
+
+  def fetch_quotas
+    quotas = []
+    headers_done = false
+    `cf quotas`.each_line do |line|
+      unless headers_done
+        headers_done = true if line =~ /\Aname\s+/
+        next
+      end
+      if line =~ /\A(\S+)\s+/
+        quotas << $1
+      end
+    end
+    quotas
+  end
+
+  def cf(*args)
+    unless system("cf", *args)
+      raise "Error: 'cf #{args.join(' ')}' exited #{$?.exitstatus}"
+    end
+  end
+end
+
+if __FILE__ == $0
+  abort "Usage: #{$0} /path/to/manifest.yml" unless ARGV.size == 1
+  manifest = YAML.load_file(ARGV[0])
+  QuotasSetter.new(manifest).apply!
+end
---
+++ concourse/scripts/spec/set_quotas_from_manifest_spec.rb
@@ -0,0 +1,100 @@
+require_relative "../set_quotas_from_manifest"
+
+RSpec.describe QuotasSetter do
+  let(:quota_definitions) { {} }
+  let(:manifest) {{
+    "properties" => {
+      "cc" => {
+        "quota_definitions" => quota_definitions,
+      },
+    },
+  }}
+  subject { described_class.new(manifest) }
+
+  before :each do
+    allow(subject).to receive(:`).with('cf quotas') do
+      system("exit 0") # setup $?
+      ""
+    end
+  end
+
+  describe "creating/updating quotas" do
+    before :each do
+      allow(subject).to receive(:system).with("cf", /^(create|update)-quota$/, any_args) do
+        system("exit 0")
+      end
+
+      quota_definitions["default"] = {
+        "memory_limit" => 2048,
+        "total_services" => 10,
+        "total_routes" => 1000,
+        "non_basic_services_allowed" => false,
+      }
+      quota_definitions["large"] = {
+        "memory_limit" => 10240,
+        "total_services" => 100,
+        "total_routes" => 10000,
+        "non_basic_services_allowed" => true,
+      }
+    end
+
+    context "with no extant quotas" do
+      before :each do
+        allow(subject).to receive(:`).with('cf quotas') do
+          system("exit 0") # setup $?
+          <<-EOT
+Getting quotas as admin...
+OK
+
+name                                    total memory   instance memory   routes   service instances   paid plans   app instances   route ports
+          EOT
+        end
+      end
+
+      it "creates the quotas" do
+        expect_cf_quota_create("default", *%w(-m 2048M -s 10 -r 1000 --disallow-paid-service-plans))
+        expect_cf_quota_create("large", *%w(-m 10240M -s 100 -r 10000 --allow-paid-service-plans))
+
+        subject.apply!
+      end
+
+    end
+
+    context "when some quotas exist" do
+      before :each do
+        allow(subject).to receive(:`).with('cf quotas') do
+          system("exit 0") # setup $?
+          <<-EOT
+Getting quotas as admin...
+OK
+
+name                                    total memory   instance memory   routes   service instances   paid plans   app instances   route ports
+default                                 2G             unlimited         1000     10                  disallowed   unlimited       0
+          EOT
+        end
+      end
+
+      it "updates an existing quota" do
+        expect_cf_quota_update("default", *%w(-m 2048M -s 10 -r 1000 --disallow-paid-service-plans))
+        subject.apply!
+      end
+
+      it "creates a quota that doesn't already exist" do
+        expect_cf_quota_create("large", *%w(-m 10240M -s 100 -r 10000 --allow-paid-service-plans))
+        subject.apply!
+      end
+    end
+  end
+
+  def expect_cf_quota_write(name, action, *args)
+    expect(subject).to receive(:system).with("cf", action, name, *args) do
+      system("exit 0") # setup $?
+    end
+  end
+  def expect_cf_quota_create(name, *args)
+    expect_cf_quota_write(name, 'create-quota', *args)
+  end
+  def expect_cf_quota_update(name, *args)
+    expect_cf_quota_write(name, 'update-quota', *args)
+  end
+end
