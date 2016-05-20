9b732a66980010a8e4ee468d09d93af6514259e6
Dan Carley <dan.carley@gmail.com>
2016-05-20 10:49:49 +0100

    bosh_login: Use val_from_yaml to get BOSh password
    So that we parse the YAML instead of trying to scrape strings from it. This
    will be more reliable and help with the quoting problems described in
    cdcab37.

--- concourse/scripts/bosh_login.sh
+++ concourse/scripts/bosh_login.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-
 set -eu
 
 bosh_host=$1
 bosh_secrets_file=$2
 
-bosh_password=$(awk '/bosh_admin_password/ { print $2 }' "${bosh_secrets_file}" | tr -d '"')
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+bosh_password=$("${SCRIPT_DIR}"/val_from_yaml.rb secrets.bosh_admin_password "${bosh_secrets_file}")
+
 bosh -t "${bosh_host}" login admin -- "${bosh_password}"
 bosh target "${bosh_host}"
