96ae848135e1446f034c8e50425b9bc4f087e091
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2016-05-26 10:09:57 +0100

    Add unit tests for rds-broker plan uniqueness.
    If there are id or name collisions we will end up with errors after
    deployment that aren't the easiest to track down. Validating the
    uniqueness of these properties at this stage will help prevent this.

--- manifests/cf-manifest/spec/rds_broker_spec.rb
+++ manifests/cf-manifest/spec/rds_broker_spec.rb
@@ -30,4 +30,44 @@ RSpec.describe "RDS broker properties" do
       expect(sgs.last).to eq("rds_broker_instances")
     end
   end
+
+  describe "service plans" do
+    let(:rds_broker_job) {
+      manifest.fetch("jobs").find {|j| j["name"] == "rds_broker_z1" }
+    }
+    let(:services) {
+      rds_broker_job.fetch("properties").fetch("rds-broker").fetch("catalog").fetch("services")
+    }
+    let(:all_plans) {
+      services.map {|s| s["plans"]}.flatten(1)
+    }
+
+    specify "all services have a unique id" do
+      all_ids = services.map {|s| s["id"] }
+      duplicated_ids = all_ids.select {|id| all_ids.count(id) > 1}.uniq
+      expect(duplicated_ids).to be_empty,
+        "found duplicate service ids (#{duplicated_ids.join(',')})"
+    end
+
+    specify "all services have a unique name" do
+      all_names = services.map {|s| s["name"] }
+      duplicated_names = all_names.select {|name| all_names.count(name) > 1}.uniq
+      expect(duplicated_names).to be_empty,
+        "found duplicate service names (#{duplicated_names.join(',')})"
+    end
+
+    specify "all plans have a unique id" do
+      all_ids = all_plans.map {|p| p["id"] }
+      duplicated_ids = all_ids.select {|id| all_ids.count(id) > 1}.uniq
+      expect(duplicated_ids).to be_empty,
+        "found duplicate plan ids (#{duplicated_ids.join(',')})"
+    end
+
+    specify "all plans have a unique name" do
+      all_names = all_plans.map {|p| p["name"] }
+      duplicated_names = all_names.select {|name| all_names.count(name) > 1}.uniq
+      expect(duplicated_names).to be_empty,
+        "found duplicate plan names (#{duplicated_names.join(',')})"
+    end
+  end
 end
