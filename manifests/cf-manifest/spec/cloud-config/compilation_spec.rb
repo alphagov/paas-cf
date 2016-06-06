6453169ad21a9bc44b756a5133f01fc09c46c1e1
HenryTK <henry.knott@digital.cabinet-office.gov.uk>
2016-06-06 18:17:11 +0100

    Add cloud-config tests
    Jobs have been unified due to Bosh availability zones, therefore
    the names no longer have `_z(n)`.
    
    The networks spec is now under Cloud Config instead of the manifest.
    Network definitions now contain multiple subnets, so we assert
    we are using at least two subnets, which correspond to using two
    availability zones.
    
    The VM types spec has just been moved from the manifest to the new Cloud
    Config.
    
    Under Cloud Config stemcells are defined in a top level property
    and referenced in jobs. This test ensures that all stemcell
    definitions referenced actually exist.

---
+++ manifests/cf-manifest/spec/cloud-config/compilation_spec.rb
@@ -0,0 +1,19 @@
+
+RSpec.describe "compilation" do
+  let(:manifest) { manifest_with_defaults }
+
+  it "is defined" do
+    expect(manifest["compilation"]).not_to be_empty
+  end
+
+  it "references an existing AZ" do
+    az_list = manifest["azs"].map{ |az| az["name"] }
+    expect(az_list).to include(manifest["compilation"]["az"])
+  end
+
+  it "references an existing vm_type" do
+    vm_type_list = manifest["vm_types"].map{ |az| az["name"] }
+    expect(vm_type_list).to include(manifest["compilation"]["vm_type"])
+  end
+
+end
