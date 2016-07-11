ab2e819d77869d9e08d4e90d3c3b3c5c35c79835
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2016-07-11 16:04:48 +0100

    Disable SSH access to apps.
    We don't want to enable this until we have a validated need for it.
    
    From the story[1]:
    
    > Having discussed with users they have indicated that they don't
    > believe that they need to use this functionality, and would prefer
    > that they didn't have it as it encourages better behaviour.
    
    This includes a custom acceptance test to verify that SSH access is
    indeed disabled.
    
    [1] https://www.pivotaltracker.com/story/show/121928523

---
+++ platform-tests/src/acceptance/cf_ssh_test.go
@@ -0,0 +1,27 @@
+package acceptance_test
+
+import (
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("CF SSH", func() {
+	It("should be disabled", func() {
+		appName := generator.PrefixedRandomName("CATS-APP-")
+		Expect(cf.Cf(
+			"push", appName,
+			"-b", config.StaticFileBuildpackName,
+			"-p", "../../example-apps/static-app",
+			"-d", config.AppsDomain,
+			"-i", "1",
+			"-m", "64M",
+		).Wait(CF_PUSH_TIMEOUT)).To(Exit(0))
+		cfSSH := cf.Cf("ssh", appName, "-c", "uptime").Wait(DEFAULT_TIMEOUT)
+		Expect(cfSSH).NotTo(Exit(0))
+		Expect(cfSSH).To(Say("Error opening SSH connection:"))
+	})
+})
