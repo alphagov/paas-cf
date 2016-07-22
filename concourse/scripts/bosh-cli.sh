761b1bcb98e3b78c6598bca66f653c1be173e7ac
Michal Tekel <mtekel@gmail.com>
2016-07-22 12:10:14 +0100

    Make bosh-cli work with 1.5.1 concourse
    Because latest concourse doesn't support exec when using custom resources
    ( https://github.com/concourse/concourse/issues/318 )
    we instead run a bosh-cli job defined directly in the pipeline and hijack
    it.Job is visible only in 'bosh' tab.
    
    We are using old way of specifying container (`image`), because that only
    creates one container for the task, as opposed to image_resource, which
    does create a 'check' and 'get' containers apart from 'task'. Fly then
    doesn't know which one of these to hijack and it opens interactive prompt.
    
    We can switch to image_resource once fly supports specifying container
    type selection when hijacking: https://github.com/concourse/fly/issues/98

--- concourse/scripts/bosh-cli.sh
+++ concourse/scripts/bosh-cli.sh
@@ -13,44 +13,9 @@ $("${SCRIPT_DIR}/environment.sh")
 OUTPUT_FILE=$(mktemp -t bosh-cli.XXXXXX)
 trap 'rm -f "${OUTPUT_FILE}"' EXIT
 
-generate_config(){
-  cat <<EOF
----
-platform: linux
-image_resource:
-  type: docker-image
-  source:
-    repository: governmentpaas/bosh-cli
-inputs:
-  - name: paas-cf
-  - name: cf-manifest
-  - name: bosh-secrets
-run:
-  path: sh
-  args:
-  - -c
-  - -e
-  - |
-    ./paas-cf/concourse/scripts/bosh_login.sh bosh.${SYSTEM_DNS_ZONE_NAME} bosh-secrets/bosh-secrets.yml
+$FLY_CMD -t "${FLY_TARGET}" trigger-job -j create-bosh-cloudfoundry/bosh-cli -w | tee "${OUTPUT_FILE}"
 
-    uuid=\$(bosh status --uuid)
-    sed -e "s/^director_uuid:.*$/director_uuid: \${uuid}/" cf-manifest/cf-manifest.yml > cf-manifest-with-uuid.yml
-    bosh deployment ./cf-manifest-with-uuid.yml
-EOF
-}
+BUILD_NUMBER=$(awk '/started create-bosh-cloudfoundry\/bosh-cli/ { print $3 }' "${OUTPUT_FILE}" | tr -d '#')
 
-generate_config > /dev/null
-
-$FLY_CMD -t "${FLY_TARGET}" \
-  execute \
-  --inputs-from=create-bosh-cloudfoundry/cf-deploy \
-  --config=<(generate_config) \
-  | tee "${OUTPUT_FILE}"
-
-BUILD_NUMBER=$(awk '/executing build/ { print $3 }' "${OUTPUT_FILE}")
-
-$FLY_CMD -t "${FLY_TARGET}" \
-  intercept \
-  --build="${BUILD_NUMBER}"\
-  --step=one-off \
-  "${@:-sh}"
+$FLY_CMD -t "${FLY_TARGET}" intercept -j create-bosh-cloudfoundry/bosh-cli -b "${BUILD_NUMBER}" \
+   -s run-bosh-cli "${@:-ash}"
