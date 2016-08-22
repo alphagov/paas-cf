d6ca1af42fd80b592f45af2c0c5d6b6e40969a9c
Michal Tekel <mtekel@gmail.com>
2016-08-22 16:54:11 +0100

    Concourse - use own ssh key
    Generate and use a separate, concourse only ssh key for deployer concourse.
    This improves security and our ability to rotate credentials specific
    only to concourse, instead of having to rotate shared SSH key, which
    in practice means additional re-deployment of bosh and every VM
    deployed by it.

--- terraform/concourse/outputs.tf
+++ terraform/concourse/outputs.tf
@@ -22,3 +22,7 @@ output "git_concourse_pool_clone_full_url_ssh" {
   # convert the ssh:// url to a scp like connect string and add the git user
   value = "ssh://${aws_iam_user_ssh_key.git.ssh_public_key_id}@${replace(aws_codecommit_repository.concourse-pool.clone_url_ssh, "/^ssh://([^/]+)//", "$1/")}"
 }
+
+output "concourse_key_pair_name" {
+  value = "${aws_key_pair.concourse_key_pair.key_name}"
+}
