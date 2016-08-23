d34d07a0cc72ba492e03ceef2b841a7fb1b059b6
HenryTK <henry.knott@digital.cabinet-office.gov.uk>
2016-08-23 10:32:20 +0100

    Automate rotation of Cloud Foundry user password
    We want an automated, repeatable, and standardised way of rotating
    a Cloud Foundry user's password and emailing it to them when they
    are unable to log in.
    
    Common functions have been taken from the `create-user.sh` script,
    so they have been split into a separate file. This means Shellcheck
    is now configured to check included source files.
    
    Run the script without arguments to see its documentation.

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 SCRIPT=$0
+
+# shellcheck disable=SC1090
+source "${SCRIPT_DIR}/common.sh"
 
 ###########################################################################
 # Defaults
@@ -65,33 +69,11 @@ EOF
   exit 1
 }
 
-load_colors() {
-  if [ -t 1 ] ; then
-    export ESC_SEQ="\x1b["
-    export COL_RESET="${ESC_SEQ}39;49;00m"
-    export COL_LIGHT="${ESC_SEQ}1m"
-    export COL_DIM="${ESC_SEQ}2m"
-    export COL_BLINK="${ESC_SEQ}5m"
-    export COL_RED="${ESC_SEQ}31m"
-    export COL_GREEN="${ESC_SEQ}32m"
-    export COL_YELLOW="${ESC_SEQ}33m"
-    export COL_BLUE="${ESC_SEQ}34m"
-    export COL_MAGENTA="${ESC_SEQ}35m"
-    export COL_CYAN="${ESC_SEQ}36m"
-  fi
-}
-
-abort() {
-  echo -e "${COL_RED:-}${COL_LIGHT:-}ERROR:${COL_RESET:-} $*" 1>&2
-  exit 1
-}
-
 abort_usage() {
   echo -e "${COL_RED:-}${COL_LIGHT:-}ERROR:${COL_RESET:-} $*" 1>&2
   usage
   exit 1
 }
-
 
 check_params_and_environment() {
   if [ -z "${EMAIL:-}" ]; then
