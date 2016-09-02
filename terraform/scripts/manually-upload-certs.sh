cbcff8d380d80827133bfa02ff76329966dfc11f
benhyland <ben.hyland@digital.cabinet-office.gov.uk>
2016-09-02 10:46:08 +0100

    Handle local terraform state with a temporary working directory
    We tried to handle local state by running `rm -rf .terraform`
    before and after the terraform command.
    This was a little difficult to get right and could also remove local
    state not related to these scripts.
    
    Instead, work within a new temporary directory and clean it up on exit.

--- terraform/scripts/manually-upload-certs.sh
+++ terraform/scripts/manually-upload-certs.sh
@@ -13,11 +13,12 @@ pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain.crt" > /dev/null
 pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain.key" > /dev/null
 pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain_intermediate.crt" > /dev/null
 
-# Ensure there's no local state before we kick off terraform, it prioritises it
-rm -rf .terraform
-
+PAAS_CF_DIR=$(pwd)
 WORKING_DIR=$(mktemp -d cf-certs.XXXXXX)
-trap 'rm -r "${WORKING_DIR}"' EXIT
+trap 'rm -r "${PAAS_CF_DIR}/${WORKING_DIR}"' EXIT
+
+# Work in tmp dir to ensure there's no local state before we kick off terraform, it prioritises it
+cd "${WORKING_DIR}"
 
 # Configure Terraform remote state
 terraform remote config \
@@ -27,14 +28,11 @@ terraform remote config \
   -backend-config="region=${AWS_DEFAULT_REGION}"
 
 terraform "${TERRAFORM_ACTION}" -var env="${DEPLOY_ENV}" \
-  -var-file="terraform/${AWS_ACCOUNT}.tfvars" \
+  -var-file="${PAAS_CF_DIR}/terraform/${AWS_ACCOUNT}.tfvars" \
   -var system_domain_crt="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/system_domain.crt")" \
   -var system_domain_key="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/system_domain.key")" \
   -var system_domain_intermediate_crt="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/system_domain_intermediate.crt")" \
   -var apps_domain_crt="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain.crt")" \
   -var apps_domain_key="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain.key")" \
   -var apps_domain_intermediate_crt="$(pass "certs/${AWS_ACCOUNT}/${DEPLOY_ENV}/apps_domain_intermediate.crt")" \
-  terraform/cf-certs
-
-# Delete new local terraform state
-rm -rf .terraform
+  "${PAAS_CF_DIR}"/terraform/cf-certs
