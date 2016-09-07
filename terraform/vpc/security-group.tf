2a5ab32112e9a346ef47d20dc9e7b374e2349fe1
Michal Tekel <mtekel@gmail.com>
2016-09-07 13:57:02 +0100

    TF: fix concat for strings
    Ever since TF 0.7, concat only works for list. It will not contat with
    string any more. Turn the string into a single member list so that it
    can be concatenated.

--- terraform/vpc/security-group.tf
+++ terraform/vpc/security-group.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "office-access-ssh" {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
-    cidr_blocks = ["${compact(concat(split(",", var.admin_cidrs), var.vagrant_cidr))}"]
+    cidr_blocks = ["${compact(concat(split(",", var.admin_cidrs), list(var.vagrant_cidr)))}"]
   }
 
   tags {
