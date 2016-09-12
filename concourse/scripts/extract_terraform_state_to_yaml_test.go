ce2ed94e975b1e6bdb050c30230db82b51fcc3fc
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2016-09-12 09:39:54 +0100

    Error on old format tfstate file in tfvars extract
    Previously, these scripts would output blank values when given an old
    format tfstate file. This is bad, and led to some breakage on staging.
    
    This updates the scripts to error when given an old format tfstate file.

---
+++ concourse/scripts/extract_terraform_state_to_yaml_test.go
@@ -0,0 +1,106 @@
+package scripts_test
+
+import (
+	"os/exec"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("ExtractTerraformStateToYAML", func() {
+
+	var (
+		cmdInput string
+		session  *gexec.Session
+	)
+
+	JustBeforeEach(func() {
+		command := exec.Command("./extract_terraform_state_to_yaml.rb")
+		command.Stdin = strings.NewReader(cmdInput)
+
+		var err error
+		session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	Context("with a valid tfstate file", func() {
+		BeforeEach(func() {
+			cmdInput = `
+{
+    "version": 3,
+    "terraform_version": "0.7.3",
+    "serial": 5,
+    "lineage": "bfa4e77c-4e4e-462e-92a9-dba07be0f409",
+    "modules": [
+        {
+            "path": [
+                "root"
+            ],
+            "outputs": {
+                "foo_dns_name": {
+                    "sensitive": false,
+                    "type": "string",
+                    "value": "foo.example.com"
+                },
+                "foo_elastic_ip": {
+                    "sensitive": false,
+                    "type": "string",
+                    "value": "10.210.221.248"
+                },
+                "foo_security_group_id": {
+                    "sensitive": false,
+                    "type": "string",
+                    "value": "sg-12345678"
+                }
+            },
+			"resources": {},
+			"depends_on": []
+		}
+	]
+}
+			`
+		})
+
+		It("returns a YAML containing all the output values", func() {
+			Eventually(session).Should(gexec.Exit(0))
+			Expect(session.Out).To(gbytes.Say("terraform_outputs:\n"))
+			Expect(session.Out).To(gbytes.Say("\\s+foo_dns_name: foo.example.com"))
+			Expect(session.Out).To(gbytes.Say("\\s+foo_elastic_ip: 10.210.221.248"))
+			Expect(session.Out).To(gbytes.Say("\\s+foo_security_group_id: sg-12345678"))
+		})
+	})
+
+	Context("with an old-format tfstate file", func() {
+		BeforeEach(func() {
+			cmdInput = `
+{
+    "version": 1,
+    "serial": 5,
+    "modules": [
+        {
+            "path": [
+                "root"
+            ],
+            "outputs": {
+                "foo_dns_name": "foo.example.com",
+                "foo_elastic_ip": "10.210.221.248",
+                "foo_security_group_id": "sg-12345678"
+            },
+			"resources": {},
+			"depends_on": []
+		}
+	]
+}
+			`
+		})
+
+		It("Exits non-zero with an error on STDERR", func() {
+			Eventually(session).Should(gexec.Exit())
+			Expect(session.ExitCode()).NotTo(Equal(0))
+			Expect(session.Out.Contents()).To(BeEmpty())
+		})
+	})
+})
---
+++ concourse/scripts/extract_tf_vars_from_terraform_state_test.go
@@ -0,0 +1,105 @@
+package scripts_test
+
+import (
+	"os/exec"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("ExtractTFVarsFromTFState", func() {
+
+	var (
+		cmdInput string
+		session  *gexec.Session
+	)
+
+	JustBeforeEach(func() {
+		command := exec.Command("./extract_tf_vars_from_terraform_state.rb")
+		command.Stdin = strings.NewReader(cmdInput)
+
+		var err error
+		session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	Context("with a valid tfstate file", func() {
+		BeforeEach(func() {
+			cmdInput = `
+{
+    "version": 3,
+    "terraform_version": "0.7.3",
+    "serial": 5,
+    "lineage": "bfa4e77c-4e4e-462e-92a9-dba07be0f409",
+    "modules": [
+        {
+            "path": [
+                "root"
+            ],
+            "outputs": {
+                "foo_dns_name": {
+                    "sensitive": false,
+                    "type": "string",
+                    "value": "foo.example.com"
+                },
+                "foo_elastic_ip": {
+                    "sensitive": false,
+                    "type": "string",
+                    "value": "10.210.221.248"
+                },
+                "foo_security_group_id": {
+                    "sensitive": false,
+                    "type": "string",
+                    "value": "sg-12345678"
+                }
+            },
+			"resources": {},
+			"depends_on": []
+		}
+	]
+}
+			`
+		})
+
+		It("returns a shell export statement for each output", func() {
+			Eventually(session).Should(gexec.Exit(0))
+			Expect(session.Out).To(gbytes.Say("export TF_VAR_foo_dns_name='foo.example.com'"))
+			Expect(session.Out).To(gbytes.Say("export TF_VAR_foo_elastic_ip='10.210.221.248'"))
+			Expect(session.Out).To(gbytes.Say("export TF_VAR_foo_security_group_id='sg-12345678'"))
+		})
+	})
+
+	Context("with an old-format tfstate file", func() {
+		BeforeEach(func() {
+			cmdInput = `
+{
+    "version": 1,
+    "serial": 5,
+    "modules": [
+        {
+            "path": [
+                "root"
+            ],
+            "outputs": {
+                "foo_dns_name": "foo.example.com",
+                "foo_elastic_ip": "10.210.221.248",
+                "foo_security_group_id": "sg-12345678"
+            },
+			"resources": {},
+			"depends_on": []
+		}
+	]
+}
+			`
+		})
+
+		It("Exits non-zero and outputs nothing on STDOUT", func() {
+			Eventually(session).Should(gexec.Exit())
+			Expect(session.ExitCode()).NotTo(Equal(0))
+			Expect(session.Out.Contents()).To(BeEmpty())
+		})
+	})
+})
