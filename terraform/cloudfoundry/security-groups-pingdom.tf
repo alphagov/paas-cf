ebdd9a99f2e7a6bfda8fa691fcf078af5de3c8bd
Piotr Komborski <piotr.komborski@gmail.com>
2016-09-26 11:21:04 +0100

    Add one more pingdom groups
    This is temporary fix to allow provisioning of more security groups to
    allow more than 96 pingdom IPs ( 32 per group ) to access our
    infrastructure. Right now pingdom reports 108 IPs.

--- terraform/cloudfoundry/security-groups-pingdom.tf
+++ terraform/cloudfoundry/security-groups-pingdom.tf
@@ -10,6 +10,10 @@ variable "pingdom_probe_cidrs_2" {
   description = "CSV of additional CIDR addresses for which we allow web access, provided externally"
   default     = ""
 }
+variable "pingdom_probe_cidrs_3" {
+  description = "CSV of additional CIDR addresses for which we allow web access, provided externally"
+  default     = ""
+}
 resource "aws_security_group" "pingdom-probes-0" {
   name = "${var.env}-cf-pingdom-probes-0"
   description = "Security group for pingdom probes to reach the HTTPS port"
@@ -83,5 +87,30 @@ resource "aws_security_group" "pingdom-probes-2" {
 
   tags {
     Name = "${var.env}-cf-pingdom-probes-2"
+  }
+}
+resource "aws_security_group" "pingdom-probes-3" {
+  name = "${var.env}-cf-pingdom-probes-3"
+  description = "Security group for pingdom probes to reach the HTTPS port"
+  vpc_id = "${var.vpc_id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = [
+      "${split(",", var.pingdom_probe_cidrs_3)}",
+    ]
+  }
+
+  tags {
+    Name = "${var.env}-cf-pingdom-probes-3"
   }
 }
