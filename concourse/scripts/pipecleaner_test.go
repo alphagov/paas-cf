f117653013bc7499cfcb6c36fc6984e338a8b00c
Jonty Wareing <jonty@jonty.co.uk>
2016-09-28 17:42:36 +0100

    Make pipecleaner shellcheck failures fatal

--- concourse/scripts/pipecleaner_test.go
+++ concourse/scripts/pipecleaner_test.go
@@ -52,9 +52,9 @@ var _ = Describe("PipeCleaner", func() {
 				command = exec.Command("./pipecleaner.py", "spec/fixtures/pipecleaner_shellcheck.yml")
 			})
 
-			It("should warn about the non-portable compare", func() {
-				Eventually(session).Should(gexec.Exit(0))
-				Expect(session.Out).To(gbytes.Say("WARNING.*?job='shellcheck', task='bad-compare'"))
+			It("should fatal with non-portable compare", func() {
+				Eventually(session).Should(gexec.Exit(10))
+				Expect(session.Out).To(gbytes.Say("ERROR.*?job='shellcheck', task='bad-compare'"))
 				Expect(session.Err.Contents()).To(BeEmpty())
 			})
 		})
@@ -67,18 +67,6 @@ var _ = Describe("PipeCleaner", func() {
 			It("should not report an issue", func() {
 				Eventually(session).Should(gexec.Exit(0))
 				Expect(session.Out.Contents()).To(BeEmpty())
-				Expect(session.Err.Contents()).To(BeEmpty())
-			})
-		})
-
-		Context("with --fatal-warnings", func() {
-			BeforeEach(func() {
-				command = exec.Command("./pipecleaner.py", "--fatal-warnings", "spec/fixtures/pipecleaner_shellcheck.yml")
-			})
-
-			It("should warn about the non-portable compare, and exit as though fatal", func() {
-				Eventually(session).Should(gexec.Exit(20))
-				Expect(session.Out).To(gbytes.Say("WARNING.*?job='shellcheck', task='bad-compare'"))
 				Expect(session.Err.Contents()).To(BeEmpty())
 			})
 		})
