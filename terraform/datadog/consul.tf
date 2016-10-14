ac1bfba041549385591b2b59a68cba5e7d9f33ea
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2016-10-14 15:37:10 +0100

    Create consul monitors
    We need to create few monitors for the datadog, to have alerts on our
    consul instances.
    
    First of the checks, will monitor the health of the VMs, whilst the
    second will be testing the connections to the consul.

---
+++ terraform/datadog/consul.tf
@@ -0,0 +1,40 @@
+resource "datadog_monitor" "consul" {
+  name = "${format("%s Consul hosts", var.env)}"
+  type = "service check"
+  message = "Missing consul hosts in environment {{host.environment}}. Notify: @the-multi-cloud-paas-team@digital.cabinet-office.gov.uk"
+  escalation_message = "Missing consul hosts! Check VM state."
+  no_data_timeframe = "2"
+  query = "${format("'process.up'.over('environment:%s','process:consul').last(6).count_by_status()", var.env)}"
+
+  thresholds {
+    ok = 1
+    warning = 3
+    critical = 5
+  }
+
+  require_full_window = true
+  tags {
+    "environment" = "${var.env}"
+    "job" = "consul"
+  }
+}
+
+resource "datadog_monitor" "consul_connect_to_port" {
+  name = "${format("%s consul cluster service is accepting connections", var.env)}"
+  type = "service check"
+  message = "Large portion of consul service are not accepting connections. Check deployment state."
+  escalation_message = "Large portion of consul service are still not accepting connections. Check deployment state."
+  no_data_timeframe = "5"
+  require_full_window = true
+
+  query = "${format("'tcp.can_connect'.over('environment:%s','instance:consul_server').by('*').last(1).pct_by_status()", var.env)}"
+
+  thresholds {
+    critical = 50
+  }
+
+  tags {
+    "environment" = "${var.env}"
+    "job" = "router"
+  }
+}
