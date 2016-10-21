651289c76723dc1a3a18f7813897c82bd1cb4b2e
Richard Clamp <richardc@unixbeard.net>
2016-10-21 12:05:47 +0100

    Revert "Merge pull request #495 from alphagov/feature/124720541-add-metron_agent-to-all-vms"
    This reverts commit 7cac814425c860985acb2b9a35259954c175e4e4, reversing
    changes made to b5569530477322b39f36c6af23640a2ee458f9f0.
    
    We have added metron_agent as a shortcut to getting rsyslogd configured
    for log forwarding.  This in hindsight was a mistake, as it cannot be
    configured to just forward, so we have extra broken instances of the
    metron_agent service.
    
    Here we revert the addition so we can reimplement this using a different
    release.

--- manifests/cf-manifest/spec/runtime-config/shared_config_spec.rb
+++ manifests/cf-manifest/spec/runtime-config/shared_config_spec.rb
@@ -1,7 +1,6 @@
-RSpec.describe "Runtime config use shared config" do
+
+RSpec.describe "Runtime config" do
   let(:runtime_config) { load_runtime_config }
-  let(:manifest) { manifest_with_defaults }
-  let(:properties) { manifest.fetch("properties") }
 
   it "uses a shared collectd config file" do
     collectd_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "collectd" }
@@ -11,13 +10,5 @@ RSpec.describe "Runtime config use shared config" do
   it "has datadog included with properties from shared config" do
     datadog_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "datadog-agent" }
     expect(datadog_addon.fetch("properties").fetch("use_dogstatsd")).to eq false
-  end
-
-  it "the dropsonde_incoming_port is the same metron_agent and loggregator" do
-    metron_agent_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "metron_agent" }
-    metron_agent_dropsonde_incoming_port = metron_agent_addon.fetch("properties").fetch("metron_agent").fetch("dropsonde_incoming_port")
-    loggregator_dropsonde_incoming_port = properties.fetch("loggregator").fetch("dropsonde_incoming_port")
-
-    expect(metron_agent_dropsonde_incoming_port).to eq loggregator_dropsonde_incoming_port
   end
 end
