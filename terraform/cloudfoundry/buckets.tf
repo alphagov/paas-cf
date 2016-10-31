d539f613e2e1329101569058fc4a0e0f06ec8bfb
Michal Tekel <mtekel@gmail.com>
2016-10-31 14:40:51 +0000

    TF: enable versioning for CF buckets
    We want to be able to do point-in-time restore for blobstore and replicate
    the buckets, both of which require versioning enabled.

--- terraform/cloudfoundry/buckets.tf
+++ terraform/cloudfoundry/buckets.tf
@@ -2,24 +2,36 @@ resource "aws_s3_bucket" "droplets-s3" {
   bucket = "${var.env}-cf-droplets"
   acl = "private"
   force_destroy = "true"
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket" "buildpacks-s3" {
   bucket = "${var.env}-cf-buildpacks"
   acl = "private"
   force_destroy = "true"
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket" "packages-s3" {
   bucket = "${var.env}-cf-packages"
   acl = "private"
   force_destroy = "true"
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket" "resources-s3" {
   bucket = "${var.env}-cf-resources"
   acl = "private"
   force_destroy = "true"
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket" "test-artifacts" {
