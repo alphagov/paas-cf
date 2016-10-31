b92a35eb2f0284865bff56edb65457252b894570
Michal Tekel <mtekel@gmail.com>
2016-10-31 14:39:25 +0000

    TF: fix CF buckets formatting
    2 spaces is standard. Use it everywhere, including in buckets.tf, which
    contains bucket definitions for CF.

--- terraform/cloudfoundry/buckets.tf
+++ terraform/cloudfoundry/buckets.tf
@@ -1,25 +1,25 @@
 resource "aws_s3_bucket" "droplets-s3" {
-    bucket = "${var.env}-cf-droplets"
-    acl = "private"
-    force_destroy = "true"
+  bucket = "${var.env}-cf-droplets"
+  acl = "private"
+  force_destroy = "true"
 }
 
 resource "aws_s3_bucket" "buildpacks-s3" {
-    bucket = "${var.env}-cf-buildpacks"
-    acl = "private"
-    force_destroy = "true"
+  bucket = "${var.env}-cf-buildpacks"
+  acl = "private"
+  force_destroy = "true"
 }
 
 resource "aws_s3_bucket" "packages-s3" {
-    bucket = "${var.env}-cf-packages"
-    acl = "private"
-    force_destroy = "true"
+  bucket = "${var.env}-cf-packages"
+  acl = "private"
+  force_destroy = "true"
 }
 
 resource "aws_s3_bucket" "resources-s3" {
-    bucket = "${var.env}-cf-resources"
-    acl = "private"
-    force_destroy = "true"
+  bucket = "${var.env}-cf-resources"
+  acl = "private"
+  force_destroy = "true"
 }
 
 resource "aws_s3_bucket" "test-artifacts" {
