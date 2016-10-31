c8ff4d2acbf5880a2df7bba6b6ca6bae18beb979
Hector Rivas Gandara <keymon@gmail.com>
2016-10-31 16:18:38 +0000

    Change test for datadog agent tags
    Test that the tags are defined globally in the runtime config instead
    of job by job. Only check that aws_account is set.

--- manifests/cf-manifest/spec/runtime-config/shared_config_spec.rb
+++ manifests/cf-manifest/spec/runtime-config/shared_config_spec.rb
@@ -7,9 +7,17 @@ RSpec.describe "Runtime config" do
     expect(collectd_addon.fetch("properties").fetch("collectd").fetch("interval")).to eq 10
   end
 
-  it "has datadog included with properties from shared config" do
-    datadog_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "datadog-agent" }
-    expect(datadog_addon.fetch("properties").fetch("use_dogstatsd")).to eq false
+  describe "in order to monitor all hosts via datadog" do
+    let(:datadog_addon) { runtime_config.fetch("addons").find { |addon| addon["name"] == "datadog-agent" } }
+
+    it "has datadog included with properties from shared config" do
+      expect(datadog_addon.fetch("properties").fetch("use_dogstatsd")).to eq false
+    end
+
+    it "with some tags like aws_account" do
+      expect(datadog_addon.fetch("properties").fetch("tags")).not_to be_nil
+      expect(datadog_addon.fetch("properties").fetch("tags").fetch("aws_account")).to eq(ENV["AWS_ACCOUNT"])
+    end
   end
 
   it "has syslog_forwarder configured with the address from terraform output" do
