c1b650ba0ea5d11aa5520925b0fb2df0f1e3446d
Hector Rivas Gandara <keymon@gmail.com>
2016-11-23 14:19:05 +0000

    Output the certificate ID when uploading certs
    When configuring a cloudfront distribution with SSL, if you want to
    use IAM server certificates, you must pass the ID of the certificate,
    not the ARN.
    
    Because that, we must output the terraform variable of the ID, so
    later we can use it as an input of the cloudfront config.

--- terraform/cf-certs/outputs.tf
+++ terraform/cf-certs/outputs.tf
@@ -2,6 +2,14 @@ output "system_domain_cert_arn" {
   value = "${aws_iam_server_certificate.system.arn}"
 }
 
+output "system_domain_cert_id" {
+  value = "${aws_iam_server_certificate.system.id}"
+}
+
 output "apps_domain_cert_arn" {
   value = "${aws_iam_server_certificate.apps.arn}"
+}
+
+output "apps_domain_cert_id" {
+  value = "${aws_iam_server_certificate.apps.id}"
 }
