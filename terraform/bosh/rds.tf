204c77483f688320c37eb035a45eee1fe04fba53
Colin Saliceti <c.saliceti@gmail.com>
2016-11-30 15:57:41 +0000

    Disable RDS auto minor version upgrade
    We are not sure how the next upgrade will impact us and we are planning
    a major upgrade anyway in #135224045. This removes the unknown for now
    and we will decide if we want to re-enable it or not in that story.
    
    Otherwise we face the risk of AWS upgrading our instances automatically
    and when our terraform runs, this would happen:
    
    ```
    * aws_db_instance.cf: Error modifying DB Instance colin-nov-cf:
    InvalidParameterCombination: Cannot upgrade postgres from 9.4.7 to 9.4.5
      status code: 400, request id: a4a6b0d0-b716-11e6-8cf8-29bf468258f8
    ```

--- terraform/bosh/rds.tf
+++ terraform/bosh/rds.tf
@@ -36,23 +36,24 @@ resource "aws_db_parameter_group" "default" {
 }
 
 resource "aws_db_instance" "bosh" {
-  identifier                = "${var.env}-bosh"
-  name                      = "bosh"
-  allocated_storage         = 5
-  storage_type              = "gp2"
-  engine                    = "postgres"
-  engine_version            = "9.4.5"
-  instance_class            = "db.t2.medium"
-  username                  = "dbadmin"
-  password                  = "${var.secrets_bosh_postgres_password}"
-  db_subnet_group_name      = "${aws_db_subnet_group.bosh_rds.name}"
-  parameter_group_name      = "${aws_db_parameter_group.default.id}"
-  backup_window             = "01:00-02:00"
-  maintenance_window        = "Thu:03:00-Thu:04:00"
-  multi_az                  = "${var.bosh_db_multi_az}"
-  backup_retention_period   = "${var.bosh_db_backup_retention_period}"
-  final_snapshot_identifier = "${var.env}-bosh-rds-final-snapshot"
-  skip_final_snapshot       = "${var.bosh_db_skip_final_snapshot}"
+  identifier                 = "${var.env}-bosh"
+  name                       = "bosh"
+  allocated_storage          = 5
+  storage_type               = "gp2"
+  engine                     = "postgres"
+  engine_version             = "9.4.5"
+  instance_class             = "db.t2.medium"
+  username                   = "dbadmin"
+  password                   = "${var.secrets_bosh_postgres_password}"
+  db_subnet_group_name       = "${aws_db_subnet_group.bosh_rds.name}"
+  parameter_group_name       = "${aws_db_parameter_group.default.id}"
+  backup_window              = "01:00-02:00"
+  maintenance_window         = "Thu:03:00-Thu:04:00"
+  multi_az                   = "${var.bosh_db_multi_az}"
+  backup_retention_period    = "${var.bosh_db_backup_retention_period}"
+  final_snapshot_identifier  = "${var.env}-bosh-rds-final-snapshot"
+  skip_final_snapshot        = "${var.bosh_db_skip_final_snapshot}"
+  auto_minor_version_upgrade = false
 
   vpc_security_group_ids = ["${aws_security_group.bosh_rds.id}"]
 
