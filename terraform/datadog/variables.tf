bdf50cb7e60a56204a1a61530b8e4995a6f180ff
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2016-12-02 13:21:56 +0000

    Create the CVE alert
    In order to respond with grace to any upcoming CVE, we need the CVE
    Notifier do its job properly.
    
    We're creating the monitor alert, which will send an email to our
    DeskPro account, creating the CVE ticket for the support to deal with.

--- terraform/datadog/variables.tf
+++ terraform/datadog/variables.tf
@@ -7,3 +7,8 @@ variable "job_instances" {
   description = "List of pairs <job_name>:<job_count> of expected bosh job instance count"
   default     = []
 }
+
+variable "support_email" {
+  description = "DeskPro email address"
+  default     = "govpaas-alerting-dev@digital.cabinet-office.gov.uk"
+}
