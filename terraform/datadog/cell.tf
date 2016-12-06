0f8d03955065376c846fc38a97b5bd7640e492be
Hector Rivas Gandara <keymon@gmail.com>
2016-12-06 16:43:05 +0000

    Add monitors to check the Cell rep
    We check if the process is up and if more than 50% of the rep processes
    are reachable to the service port.

--- terraform/datadog/cell.tf
+++ terraform/datadog/cell.tf
@@ -19,3 +19,47 @@ resource "datadog_monitor" "cell-available-memory" {
     "job"        = "cell"
   }
 }
+
+resource "datadog_monitor" "rep_process_running" {
+  name                = "${format("%s Cell rep process running", var.env)}"
+  type                = "service check"
+  message             = "Cell rep process not running. Check router state."
+  escalation_message  = "Cell rep process still not running. Check router state."
+  notify_no_data      = false
+  require_full_window = true
+
+  query = "${format("'process.up'.over('bosh-deployment:%s','process:rep').last(4).count_by_status()", var.env)}"
+
+  thresholds {
+    ok       = 1
+    warning  = 2
+    critical = 3
+  }
+
+  tags {
+    "deployment" = "${var.env}"
+    "service"    = "${var.env}_monitors"
+    "job"        = "cell"
+  }
+}
+
+resource "datadog_monitor" "rep_healthy" {
+  name                = "${format("%s Cell rep healthy", var.env)}"
+  type                = "service check"
+  message             = "Large portion of Cell reps unhealthy. Check deployment state."
+  escalation_message  = "Large portion of Cell reps still unhealthy. Check deployment state."
+  no_data_timeframe   = "7"
+  require_full_window = true
+
+  query = "${format("'http.can_connect'.over('bosh-deployment:%s','instance:rep_service_endpoint').by('*').last(1).pct_by_status()", var.env)}"
+
+  thresholds {
+    critical = 50
+  }
+
+  tags {
+    "deployment" = "${var.env}"
+    "service"    = "${var.env}_monitors"
+    "job"        = "cell"
+  }
+}
