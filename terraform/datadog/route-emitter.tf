6382fddb9a2ef919f95b8d1438ad58f4e9fabad9
Hector Rivas Gandara <keymon@gmail.com>
2016-12-12 10:51:43 +0000

    Add checks for route-emitter process
    Add checks for router-emitter process, including:
    
     * Monitor if the route_emitter process process is present
     * Monitor if the debug HTTP endpoint is open and responds. This can
       tell us if the process is running and ready.
     * Monitor if the local consul has the route_emitter lock
       The lock is not necessarily own by the route_emitter running on the
       host where the check runs. Because that, we alert for 100% of all
       nodes failing.

---
+++ terraform/datadog/route-emitter.tf
@@ -0,0 +1,64 @@
+resource "datadog_monitor" "route_emitter_process_running" {
+  name                = "${format("%s route-emitter process running", var.env)}"
+  type                = "service check"
+  message             = "route-emitter process not running. Check router state."
+  escalation_message  = "route-emitter rep process still not running. Check router state."
+  notify_no_data      = false
+  require_full_window = true
+
+  query = "${format("'process.up'.over('bosh-deployment:%s','process:route-emitter').last(4).count_by_status()", var.env)}"
+
+  thresholds {
+    ok       = 1
+    warning  = 2
+    critical = 3
+  }
+
+  tags {
+    "deployment" = "${var.env}"
+    "service"    = "${var.env}_monitors"
+    "job"        = "route_emitter"
+  }
+}
+
+resource "datadog_monitor" "route_emitter_healthy" {
+  name                = "${format("%s route-emitter healthy", var.env)}"
+  type                = "service check"
+  message             = "Large portion of route-emitter unhealthy. Check deployment state."
+  escalation_message  = "Large portion of route-emitter still unhealthy. Check deployment state."
+  no_data_timeframe   = "7"
+  require_full_window = true
+
+  query = "${format("'http.can_connect'.over('bosh-deployment:%s','instance:route_emitter_debug_endpoint').by('*').last(1).pct_by_status()", var.env)}"
+
+  thresholds {
+    critical = 50
+  }
+
+  tags {
+    "deployment" = "${var.env}"
+    "service"    = "${var.env}_monitors"
+    "job"        = "route_emitter"
+  }
+}
+
+resource "datadog_monitor" "route_emitter_consul_lock" {
+  name                = "${format("%s route-emitter consul lock", var.env)}"
+  type                = "service check"
+  message             = "route-emitter consul lock not present in any VM. Check route-emitter state."
+  escalation_message  = "route-emitter consul lock still not present in any VM. Check route-emitter state."
+  no_data_timeframe   = "7"
+  require_full_window = true
+
+  query = "${format("'http.can_connect'.over('bosh-deployment:%s','instance:route_emitter_consul_lock').by('*').last(1).pct_by_status()", var.env)}"
+
+  thresholds {
+    critical = 100
+  }
+
+  tags {
+    "deployment" = "${var.env}"
+    "service"    = "${var.env}_monitors"
+    "job"        = "route_emitter"
+  }
+}
