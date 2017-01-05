b2f63b7f2befd0a1063c24cb5d44e3dc97d070b7
Michal Tekel <mtekel@gmail.com>
2017-01-05 11:00:04 +0000

    DATADOG: no special treatment for concourse disk
    Concourse is now deployed by BOSH, meaning it is much faster and root disk
    is not occupied by bosh-init package cache. This means that we can use same
    check and thresholds as for other VMs.

--- terraform/datadog/general.tf
+++ terraform/datadog/general.tf
@@ -19,25 +19,3 @@ resource "datadog_monitor" "disk-space" {
     "job"        = "all"
   }
 }
-
-resource "datadog_monitor" "concourse-disk-space" {
-  name               = "${format("%s concourse disk space", var.env)}"
-  type               = "query alert"
-  message            = "${format("More than {{threshold}}%% disk used. @govpaas-alerting-%s@digital.cabinet-office.gov.uk", var.aws_account)}"
-  escalation_message = "There is still {{value}} % disk used. Check the VM!"
-  no_data_timeframe  = "30"
-  query              = "${format("max(last_5m):max:system.disk.in_use{deploy_env:%s,!device:/dev/loop0,!device:tmpfs,!device:cgroup,!device:udev,!device:/var/vcap/data/root_log,!device:/var/vcap/data/root_tmp,bosh-job:concourse} by {bosh-job,device,bosh-index} * 100 > 97", var.env)}"
-
-  thresholds {
-    warning  = "95.0"
-    critical = "97.0"
-  }
-
-  require_full_window = true
-
-  tags {
-    "deployment" = "${var.env}"
-    "service"    = "${var.env}_monitors"
-    "job"        = "concourse"
-  }
-}
