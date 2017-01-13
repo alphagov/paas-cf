0cb32660251026981737b343989ca7c82d5c2e35
Michal Tekel <mtekel@gmail.com>
2017-01-13 15:03:51 +0000

    CF TF: resolve SG FIXME
    We now use TF version that has fix for the issue, so no need to use
    workaround.

--- terraform/cloudfoundry/security-groups.tf
+++ terraform/cloudfoundry/security-groups.tf
@@ -20,8 +20,6 @@ resource "aws_security_group" "cf_api_elb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  /* FIXME: Merge these two ingress block back together once */
-  /* https://github.com/hashicorp/terraform/issues/5301 is resolved. */
   ingress {
     from_port = 443
     to_port   = 443
@@ -31,15 +29,6 @@ resource "aws_security_group" "cf_api_elb" {
       "${compact(split(",", var.admin_cidrs))}",
       "${compact(split(",", var.tenant_cidrs))}",
       "${var.concourse_elastic_ip}/32",
-    ]
-  }
-
-  ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-
-    cidr_blocks = [
       "${formatlist("%s/32", aws_eip.cf.*.public_ip)}",
     ]
   }
@@ -98,8 +87,6 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  /* FIXME: Merge these two ingress block back together once */
-  /* https://github.com/hashicorp/terraform/issues/5301 is resolved. */
   ingress {
     from_port = 443
     to_port   = 443
@@ -110,15 +97,6 @@ resource "aws_security_group" "web" {
       "${compact(split(",", var.tenant_cidrs))}",
       "${compact(split(",", var.web_access_cidrs))}",
       "${var.concourse_elastic_ip}/32",
-    ]
-  }
-
-  ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-
-    cidr_blocks = [
       "${formatlist("%s/32", aws_eip.cf.*.public_ip)}",
     ]
   }
