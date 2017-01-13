172a63b1c964389943f1f7e97dc1fbc0aa208e2e
Michal Tekel <mtekel@gmail.com>
2017-01-13 13:43:21 +0000

    CF TF: merge router subnet in cf-subnets.tf
    So that we have all platform subnets defined in a single file.

--- terraform/cloudfoundry/cf-subnets.tf
+++ terraform/cloudfoundry/cf-subnets.tf
@@ -9,3 +9,15 @@ resource "aws_subnet" "cf" {
     Name = "${var.env}-cf-subnet-${count.index}"
   }
 }
+
+resource "aws_subnet" "router" {
+  count                   = "${var.zone_count}"
+  vpc_id                  = "${var.vpc_id}"
+  cidr_block              = "${lookup(var.router_cidrs, format("zone%d", count.index))}"
+  availability_zone       = "${lookup(var.zones, format("zone%d", count.index))}"
+  map_public_ip_on_launch = false
+
+  tags {
+    Name = "${var.env}-router-subnet-${count.index}"
+  }
+}
