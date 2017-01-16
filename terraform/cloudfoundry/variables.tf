b9963d2d18f6f95ffe27d26d189345c7c6272362
Michal Tekel <mtekel@gmail.com>
2017-01-16 16:11:13 +0000

    TF: remove subnet0_id
    This has moved to paas-bootstrap now and is used for concourse deployment.

--- terraform/cloudfoundry/variables.tf
+++ terraform/cloudfoundry/variables.tf
@@ -68,10 +68,6 @@ variable "elb_idle_timeout" {
   default     = 300
 }
 
-variable "subnet0_id" {
-  description = "Subnet that is used to provision ELB"
-}
-
 variable "cf_subnet_count" {
   description = "Number of CF subnets"
   default     = 2
