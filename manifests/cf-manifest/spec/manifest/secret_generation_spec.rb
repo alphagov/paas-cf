d11093af2852c09f73b9665895862c8999341a1e
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2017-01-17 10:03:57 +0000

    Use pre-generated secrets file in YAML lint check.
    No need to generate the secrets again in this test any more as we
    already have generated them for the rest of the tests.

--- manifests/cf-manifest/spec/manifest/secret_generation_spec.rb
+++ manifests/cf-manifest/spec/manifest/secret_generation_spec.rb
@@ -2,30 +2,12 @@ require 'tempfile'
 
 RSpec.describe "secret generation" do
   describe "generate-cf-secrets" do
-    let(:script) {
-      File.expand_path("../../../scripts/generate-cf-secrets.rb", __FILE__)
-    }
-
     specify "it should produce lint-free YAML" do
-      yaml, error, status = Open3.capture3(script)
-      expect(status).to be_success, "script exited #{status.exitstatus}, stderr:\n#{error}"
-      expect(yaml).to_not be_empty
-
-      tempfile = Tempfile.new('yamllint')
-      begin
-        tempfile.write(yaml)
-        tempfile.close
-
-        output, status = Open3.capture2e(
-          [
-            'yamllint',
-            '-c', File.expand_path("../../../../../yamllint.yml", __FILE__),
-            tempfile.path,
-          ].join(' ')
-        )
-      ensure
-        tempfile.unlink
-      end
+      output, status = Open3.capture2e(*[
+        'yamllint',
+        '-c', File.expand_path("../../../../../yamllint.yml", __FILE__),
+        cf_secrets_file,
+      ])
 
       expect(status).to be_success, "yamllint exited #{status.exitstatus}, output:\n#{output}"
       expect(output).to be_empty
