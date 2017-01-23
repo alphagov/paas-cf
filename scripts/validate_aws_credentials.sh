17aa29f1be34402183889540fcf9e48b70d711d7
Michal Tekel <mtekel@gmail.com>
2017-01-23 11:05:43 +0000

    validate_aws_credentials: also check account
    Enhance script by checking if the user is working on the account they
    expect to.

--- scripts/validate_aws_credentials.sh
+++ scripts/validate_aws_credentials.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
 if [ -z "$AWS_SESSION_TOKEN" ]; then
   echo "No temporary AWS credentials found, please run create_sts_token.sh"
+  exit 255;
+fi
+
+if [ -z "$AWS_ACCOUNT" ]; then
+  echo "No AWS_ACCOUNT specified, please populate the environment variable"
   exit 255;
 fi
 
@@ -9,3 +16,7 @@ if [[ $? != 0 ]]; then
   echo "Current AWS credentials are invalid, please refresh them using create_sts_token.sh"
   exit 255;
 fi
+
+# shellcheck disable=SC1090
+source "${SCRIPT_DIR}/common.sh"
+check_aws_account_used "${AWS_ACCOUNT}"
