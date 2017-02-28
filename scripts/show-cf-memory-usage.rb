6a9f77942e0e2bbcf6012f96b9944637cc3c7161
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2017-02-28 13:40:39 +0000

    Include memory required for ADR017 in memory stats
    This outputs the total memory required across all cells to meet the
    requirements defined in ADR017[1]. This will help us judge whether we
    need to increase capacity when adding orgs.
    
    [1]https://government-paas-team-manual.readthedocs.io/en/latest/architecture_decision_records/ADR017-cell-capacity-assignment/

--- scripts/show-cf-memory-usage.rb
+++ scripts/show-cf-memory-usage.rb
@@ -44,6 +44,10 @@ orgs.each { |org|
   puts "Memory reserved by apps in org '#{org['entity']['name']}': #{org_apps_reserved_memory} MB"
 }
 
+# ADR017 requires capacity for 50% of orgs_reserved_memory in the case of a region failure.
+# So we require 50% * 3/2 when all 3 regions are running.
+required_cell_memory = (orgs_reserved_memory / 2) * 3 / 2
+
 def format_memory(amount)
   "#{amount} MB (#{amount / 1024} GB)"
 end
@@ -54,3 +58,5 @@ puts "Allocated routes: #{allocated_routes}"
 puts
 puts "Memory reserved by orgs: #{format_memory(orgs_reserved_memory)}"
 puts "Memory reserved by apps: #{format_memory(apps_reserved_memory)}"
+puts
+puts "Total cell memory required to meet ADR017: #{format_memory(required_cell_memory)}"
