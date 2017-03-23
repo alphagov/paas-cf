f19d6cb7a83b93cc28035e7dc156303959582535
Jonty Wareing <jonty@jonty.co.uk>
2017-03-23 17:07:14 +0000

    VPC peering
    This introduces VPC peering infrastructure to allow tenants access to
    their own VPC so they can expose services we don't have as brokers to
    their applications.
    
    It does this by having a per-environment json definition of the VPCs to
    peer with, along with who they belong to and what ranges to route. This
    json definition is then parsed during the pipeline and both a tfvars
    file is generated so that the VPC peering can be established & routing
    configured, and a CF manifest file is generated to configure a CF
    security group that can be bound to an organisation.
    
    The workflow is as follows:
    
    * We give a tenant a CIDR in the 172 range
    * A tenant creates a VPC using this range, then gives us their account ID
      & VPC ID
    * We add this information to our prod.vpc_peering.json file and deploy to
      production
    * We bind the newly generated security group to the tenants organisations
    * The tenant accepts the VPC peering request
    * The tenant creates a route directing all 10.0.0.0/16 traffic to the new
      vpc peering
    * Tenants now have access to their VPC in the orgs that are bound

---
+++ terraform/scripts/generate_vpc_peering_manifest.rb
@@ -0,0 +1,29 @@
+require "rubygems"
+require "json"
+require "yaml"
+
+security_groups = Array.new
+
+if File.file?(ARGV[0])
+  peers = JSON.parse(File.read(ARGV[0]))
+  peers.each do |peer|
+    group = {
+      "name" => "vpc_peer_" + peer["peer_name"],
+      "rules" => [{
+          "protocol" => "all",
+          "destination" => peer["subnet_cidr"],
+      }]
+    }
+    security_groups.push(group)
+  end
+end
+
+manifest = {
+  "properties" => {
+    "cc" => {
+      "security_group_definitions" => security_groups
+    }
+  }
+}
+
+puts manifest.to_yaml
