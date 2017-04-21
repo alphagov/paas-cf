5c2f66d239c25238708eb9a55b04990128018257
Dan Carley <dan.carley@gmail.com>
2017-04-21 15:18:57 +0100

    Fix typo in ec2-cpu-credits alert message
    It shouldn't read "low on on CPU credits" or "out of on CPU credits" :)

--- terraform/datadog/aws.tf
+++ terraform/datadog/aws.tf
@@ -1,7 +1,7 @@
 resource "datadog_monitor" "ec2-cpu-credits" {
   name           = "${format("%s EC2 CPU credits", var.env)}"
   type           = "query alert"
-  message        = "${format("Instance is {{#is_warning}}low on{{/is_warning}}{{#is_alert}}out of{{/is_alert}} on CPU credits and may perform badly. See: https://government-paas-team-manual.readthedocs.io/en/latest/support/responding_to_alerts/#cpu-credits @govpaas-alerting-%s@digital.cabinet-office.gov.uk", var.aws_account)}"
+  message        = "${format("Instance is {{#is_warning}}low on{{/is_warning}}{{#is_alert}}out of{{/is_alert}} CPU credits and may perform badly. See: https://government-paas-team-manual.readthedocs.io/en/latest/support/responding_to_alerts/#cpu-credits @govpaas-alerting-%s@digital.cabinet-office.gov.uk", var.aws_account)}"
   notify_no_data = false
   query          = "${format("avg(last_30m):avg:aws.ec2.cpucredit_balance{deploy_env:%s} by {bosh-job,bosh-index} <= 1", var.env)}"
 
