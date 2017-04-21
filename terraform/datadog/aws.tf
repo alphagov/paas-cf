d8cfb6d8a7f0bdf774fdb78fe0ca37120ed9ae3d
Dan Carley <dan.carley@gmail.com>
2017-04-21 15:18:57 +0100

    Monitor CPU credits for RDS t2 instances
    Similar to 1f33b77 for EC2 instances. I omitted this from the first PR
    because I thought we didn't have any RDS t2 instances, but BOSH has been
    since alphagov/paas-bootstrap@043f3dd.
    
    The use of the `deploy_env` tag depends on alphagov/paas-bootstrap#59
    
    I've used `dbinstanceidentifier` as the group `by` because it's a mandatory
    attribute for RDS instances, unlike `dbname` (Terraform's `name` attribute)
    or `name` (which comes from a standard AWS tag).
    
    The `!created_by:aws_rds_service_broker` will prevent us from ever being
    warned about the performance of tenant database instances which aren't
    relevant to the health of our platform. The filter isn't strictly necessary
    at the moment because they aren't tagged with `deploy_env`, but it's
    possible that they could be in the future.

--- terraform/datadog/aws.tf
+++ terraform/datadog/aws.tf
@@ -14,3 +14,20 @@ resource "datadog_monitor" "ec2-cpu-credits" {
 
   tags = ["deployment:${var.env}", "service:${var.env}_monitors", "job:all"]
 }
+
+resource "datadog_monitor" "rds-cpu-credits" {
+  name           = "${format("%s RDS CPU credits", var.env)}"
+  type           = "query alert"
+  message        = "${format("Instance is {{#is_warning}}low on{{/is_warning}}{{#is_alert}}out of{{/is_alert}} CPU credits and may perform badly. See: %s#cpu-credits @govpaas-alerting-%s@digital.cabinet-office.gov.uk", var.datadog_documentation_url, var.aws_account)}"
+  notify_no_data = false
+  query          = "${format("avg(last_30m):avg:aws.rds.cpucredit_balance{deploy_env:%s,!created_by:aws_rds_service_broker} by {dbinstanceidentifier} <= 1", var.env)}"
+
+  thresholds {
+    warning  = "20"
+    critical = "1"
+  }
+
+  require_full_window = true
+
+  tags = ["deployment:${var.env}", "service:${var.env}_monitors", "job:all"]
+}
