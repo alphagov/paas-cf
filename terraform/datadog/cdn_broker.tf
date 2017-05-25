f92bf46e1d3935117ee0050a8a2ee5d300c6882f
Jonty Wareing <jonty@jonty.co.uk>
2017-05-25 17:01:04 +0100

    Monitor the CDN broker healthcheck endpoint

---
+++ terraform/datadog/cdn_broker.tf
@@ -0,0 +1,16 @@
+resource "datadog_monitor" "cdn_broker_healthy" {
+  name                = "${format("%s cdn_broker healthy", var.env)}"
+  type                = "service check"
+  message             = "Large portion of cdn brokers unhealthy. Check deployment state."
+  escalation_message  = "Large portion of cdn brokers still unhealthy. Check deployment state."
+  no_data_timeframe   = "7"
+  require_full_window = true
+
+  query = "${format("'http.can_connect'.over('deploy_env:%s','instance:cdn_broker','url:http://localhost:3000/healthcheck').by('*').last(1).pct_by_status()", var.env)}"
+
+  thresholds {
+    critical = 50
+  }
+
+  tags = ["deployment:${var.env}", "service:${var.env}_monitors", "job:cdn_broker"]
+}
