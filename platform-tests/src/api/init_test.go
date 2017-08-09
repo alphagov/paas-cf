36e67737e9ff4dad62b5e56e58946f6cdf532e2c
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2017-08-09 08:49:31 +0100

    Add CloudFoundary API monitoring framework
    We want to be able to gather basic statistics on the availability of the
    cloudfoundry API, specifically during deployment.
    
    We want this to simulate real world usage for example "does `cf app
    myapp` work"
    
    This change adds a simple task running framework where:
      * each "task" is a func that gets given a cf client instances
      * each task should execute a set of api calls to simulate a given
    scenaerio (ie `cf app myapp`)
      * each task will be executed repeatably at a given rate
      * statistics will be gathered grouped by task for
        - executions per second
        - error rate
    
    It's a bit like a cloud foundry api specific version of vegeta. Sort of.

---
+++ platform-tests/src/api/init_test.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"testing"
+	"time"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var (
+	appName        string
+	pushTimeout    = 2 * time.Minute
+	defaultTimeout = 30 * time.Second
+)
+
+var _ = BeforeSuite(func() {
+	appName = generator.PrefixedRandomName("API-AVAILABILITY-", "myapp")
+
+	Expect(cf.Cf(
+		"push", appName,
+		"-p", "../../example-apps/static-app",
+	).Wait(pushTimeout)).To(Exit(0))
+})
+
+var _ = AfterSuite(func() {
+	cf.Cf("delete", appName, "-f").Wait(defaultTimeout)
+})
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "API Availability test")
+}
---
+++ platform-tests/src/api/run_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -eu
+
+go test -timeout 12h -ginkgo.v
+
