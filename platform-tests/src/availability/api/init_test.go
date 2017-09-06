69ea04612ba447e271d4530af63c0bf10030a496
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2017-09-06 17:15:15 +0100

    Refine creation of api-availability test app.
    Give it a fixed name. This avoids stale app instances being left behind
    in the persistent healthchecks space.
    
    Reduce memory usage. By default this allocates 1G, which is unnecessary
    as it typically uses less than 5M.
    
    Specify the buildpack to speed up the staging process as it doesn't
    need to detect which one to use.
    
    Delete the route when deleting the app. This prevents stale routes being
    left behind in the space. Prior to the change to use a fixed app name,
    this would have eventually let to the quota for routes being exhausted.

--- platform-tests/src/availability/api/init_test.go
+++ platform-tests/src/availability/api/init_test.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
-	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gexec"
@@ -18,16 +17,18 @@ var (
 )
 
 var _ = BeforeSuite(func() {
-	appName = generator.PrefixedRandomName("API-AVAILABILITY-", "myapp")
+	appName = "api-availability-test-app"
 
 	Expect(cf.Cf(
 		"push", appName,
 		"-p", "../../../example-apps/static-app",
+		"-b", "staticfile_buildpack",
+		"-m", "64M",
 	).Wait(pushTimeout)).To(Exit(0))
 })
 
 var _ = AfterSuite(func() {
-	cf.Cf("delete", appName, "-f").Wait(defaultTimeout)
+	cf.Cf("delete", appName, "-f", "-r").Wait(defaultTimeout)
 })
 
 func TestSuite(t *testing.T) {
