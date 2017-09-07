781a15822e31f30510f39f3fbf5fba946bc5a425
henrytk <henry.knott@digital.cabinet-office.gov.uk>
2017-09-07 14:56:58 +0100

    Remove deprecated Compose secrets key
    This key was the old name for this credential. When the name was changed
    it was left in to preserve the function of existing deployment pipelines
    that may rely on the old value. Plenty of time has passed and all environments
    should now have the updated key in their Compose secrets file.

--- scripts/upload-compose-secrets.sh
+++ scripts/upload-compose-secrets.sh
@@ -12,7 +12,6 @@ trap 'rm  "${SECRETS}"' EXIT
 cat > "${SECRETS}" << EOF
 ---
 compose_api_key: ${COMPOSE_API_KEY}
-compose_access_token: ${COMPOSE_API_KEY}
 EOF
 
 aws s3 cp "${SECRETS}" "s3://gds-paas-${DEPLOY_ENV}-state/compose-secrets.yml"
