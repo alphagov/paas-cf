4f8e240e92695ffb04dca0d3a812e5512044aac9
Timothy Mower <timothy.mower@gmail.com>
2017-09-11 17:28:15 +0100

    Rename email to billing email

--- scripts/upload-compose-secrets.sh
+++ scripts/upload-compose-secrets.sh
@@ -5,8 +5,8 @@ set -eu
 export PASSWORD_STORE_DIR=${COMPOSE_PASSWORD_STORE_DIR}
 
 COMPOSE_API_KEY=$(pass "compose/${AWS_ACCOUNT}/access_token")
-COMPOSE_EMAIL=$(pass "compose/billing/email_address")
-COMPOSE_PASSWORD=$(pass "compose/billing/password")
+COMPOSE_BILLING_EMAIL=$(pass "compose/billing/email_address")
+COMPOSE_BILLING_PASSWORD=$(pass "compose/billing/password")
 
 SECRETS=$(mktemp secrets.yml.XXXXXX)
 trap 'rm  "${SECRETS}"' EXIT
@@ -14,8 +14,8 @@ trap 'rm  "${SECRETS}"' EXIT
 cat > "${SECRETS}" << EOF
 ---
 compose_api_key: ${COMPOSE_API_KEY}
-compose_email: ${COMPOSE_EMAIL}
-compose_password: ${COMPOSE_PASSWORD}
+compose_billing_email: ${COMPOSE_BILLING_EMAIL}
+compose_billing_password: ${COMPOSE_BILLING_PASSWORD}
 EOF
 
 aws s3 cp "${SECRETS}" "s3://gds-paas-${DEPLOY_ENV}-state/compose-secrets.yml"
