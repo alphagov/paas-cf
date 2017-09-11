6c5716ac7aadf2f2b64c31e0ea6ae24559afd8fc
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2017-09-11 14:35:13 +0100

    Terraform create Datadog alerts for RAM and Disk usage

--- terraform/datadog/compose_scraper.tf
+++ terraform/datadog/compose_scraper.tf
@@ -16,3 +16,39 @@ resource "datadog_monitor" "compose_scraper" {
 
   tags = ["deployment:${var.env}", "service:${var.env}_monitors"]
 }
+
+resource "datadog_monitor" "compose_host_ram_in_use" {
+  count             = "${var.enable_compose_scraper}"
+  name              = "${format("%s High memory utilisation on a Compose cluster host", var.env)}"
+  type              = "metric alert"
+  message           = "Host {{host.name}} in the Compose cluster is using {{value}}% of RAM. As this is above {{#is_alert}}{{threshold}}%{{/is_alert}}{{#is_warning}}{{warn_threshold}}%{{/is_warning}} the cluster may need scaling."
+  no_data_timeframe = "20"
+  query             = "${format("max(last_10m):max:compose.cluster.host.ram.in_use{deployment:%s} by {host} >= 80", var.env)}"
+
+  thresholds {
+    warning  = "70"
+    critical = "80"
+  }
+
+  require_full_window = false
+
+  tags = ["deployment:${var.env}", "service:${var.env}_monitors"]
+}
+
+resource "datadog_monitor" "compose_host_disk_in_use" {
+  count             = "${var.enable_compose_scraper}"
+  name              = "${format("%s High disk utilisation on a Compose cluster host", var.env)}"
+  type              = "metric alert"
+  message           = "Host {{host.name}} in the Compose cluster is using {{value}}% of disk. As this is above {{#is_alert}}{{threshold}}%{{/is_alert}}{{#is_warning}}{{warn_threshold}}%{{/is_warning}} the cluster may need scaling."
+  no_data_timeframe = "20"
+  query             = "${format("max(last_10m):max:compose.cluster.host.disk.in_use{deployment:%s} by {host} >= 80", var.env)}"
+
+  thresholds {
+    warning  = "70"
+    critical = "80"
+  }
+
+  require_full_window = false
+
+  tags = ["deployment:${var.env}", "service:${var.env}_monitors"]
+}
