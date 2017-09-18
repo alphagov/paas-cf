d31d5fe5435f092e70f87c0c13249b9e28cfe705
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2017-09-18 15:32:07 +0100

    Drop "ok" compose monitor in favour of "no data"
    Previously there was a monitor configured that would notify if the compose scraper
    did not report back with an "ok" health check. The health was only being
    reported when the scraper started up and so was not very useful.
    
    This has been dropped in favour of notifying when "no data" has been
    returned for any of the other metrics that are being monitored (cluster
    memory and disk usage).

--- terraform/datadog/compose_scraper.tf
+++ terraform/datadog/compose_scraper.tf
@@ -1,26 +1,10 @@
-resource "datadog_monitor" "compose_scraper" {
-  name               = "${format("%s compose-scraper", var.env)}"
-  type               = "service check"
-  message            = "${format("{{#is_alert}} No data from compose-scraper. Check the compose-scraper application in org admin space monitoring, account: %s. It should be up and running. {{/is_alert}}", var.aws_account)}"
-  escalation_message = "${format("{{#is_alert}} Still no data from compose-scraper. Check the compose-scraper application in org admin space monitoring, account: %s. It should be up and running. {{/is_alert}}", var.aws_account)}"
-  no_data_timeframe  = "5"
-  query              = "${format("'compose.scraper.ok'.over('host:compose-scraper-%s').last(1).count_by_status()", var.env)}"
-
-  thresholds {
-    warning  = "1"
-    critical = "1"
-  }
-
-  require_full_window = true
-
-  tags = ["deployment:${var.env}", "service:${var.env}_monitors"]
-}
-
 resource "datadog_monitor" "compose_host_ram_in_use" {
   name              = "${format("%s High memory utilisation on a Compose cluster host", var.env)}"
   type              = "metric alert"
   message           = "Host {{host.name}} in the Compose cluster is using {{value}}% of RAM. As this is above {{#is_alert}}{{threshold}}%{{/is_alert}}{{#is_warning}}{{warn_threshold}}%{{/is_warning}} the cluster may need scaling."
   no_data_timeframe = "20"
+  notify_no_data    = true
+  renotify_interval = "20"
   query             = "${format("max(last_10m):max:compose.cluster.host.ram.in_use{deployment:%s} by {host} >= 80", var.env)}"
 
   thresholds {
@@ -38,6 +22,8 @@ resource "datadog_monitor" "compose_host_disk_in_use" {
   type              = "metric alert"
   message           = "Host {{host.name}} in the Compose cluster is using {{value}}% of disk. As this is above {{#is_alert}}{{threshold}}%{{/is_alert}}{{#is_warning}}{{warn_threshold}}%{{/is_warning}} the cluster may need scaling."
   no_data_timeframe = "20"
+  notify_no_data    = true
+  renotify_interval = "20"
   query             = "${format("max(last_10m):max:compose.cluster.host.disk.in_use{deployment:%s} by {host} >= 80", var.env)}"
 
   thresholds {
