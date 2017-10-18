a38085018acd364c70220045aca492805a3558c8
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2017-10-18 16:33:14 +0100

    Add acceptance tests for http redirect behaviour.

--- platform-tests/src/acceptance/plain_http_test.go
+++ platform-tests/src/acceptance/plain_http_test.go
@@ -1,15 +1,13 @@
 package acceptance_test
 
 import (
+	"fmt"
 	"net"
+	"net/http"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	. "github.com/onsi/gomega/gexec"
-
-	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
-	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
 )
 
 var _ = Describe("plain HTTP requests", func() {
@@ -27,19 +25,24 @@ var _ = Describe("plain HTTP requests", func() {
 	})
 
 	Describe("to an app", func() {
-		It("has the connection refused", func() {
-			appName := generator.PrefixedRandomName("CATS-APP-")
-			Expect(cf.Cf(
-				"push", appName,
-				"-b", config.StaticFileBuildpackName,
-				"-p", "../../example-apps/static-app",
-				"-d", config.AppsDomain,
-			).Wait(CF_PUSH_TIMEOUT)).To(Exit(0))
+		It("is redirected to the https endpoint", func() {
+			req, err := http.NewRequest("GET", fmt.Sprintf("http://foo.%s/", config.AppsDomain), nil)
+			Expect(err).NotTo(HaveOccurred())
+			resp, err := http.DefaultTransport.RoundTrip(req)
+			Expect(err).NotTo(HaveOccurred())
 
-			uri := appName + "." + config.AppsDomain + ":80"
-			_, err := net.DialTimeout("tcp", uri, CONNECTION_TIMEOUT)
-			Expect(err).To(HaveOccurred(), "should not connect")
-			Expect(err.(net.Error).Timeout()).To(BeTrue(), "should timeout")
+			Expect(resp.StatusCode).To(Equal(301))
+			Expect(resp.Header.Get("Location")).To(Equal(fmt.Sprintf("https://foo.%s/", config.AppsDomain)))
+		})
+
+		It("has any path and query components removed when redirecting", func() {
+			req, err := http.NewRequest("GET", fmt.Sprintf("http://foo.%s/bar?baz=qux", config.AppsDomain), nil)
+			Expect(err).NotTo(HaveOccurred())
+			resp, err := http.DefaultTransport.RoundTrip(req)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(resp.StatusCode).To(Equal(301))
+			Expect(resp.Header.Get("Location")).To(Equal(fmt.Sprintf("https://foo.%s/", config.AppsDomain)))
 		})
 	})
 })
