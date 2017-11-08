536086edf0b26371d523fbf3afd07bb034f8829f
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2017-11-08 12:54:21 +0000

    Remove Router ELB latency monitor
    We're removing the router monitor as it's not strictly part of the API
    we mean to monitor and we may get some false alerts if tenant
    application would take time to serve response.

--- terraform/datadog/elb.tf
+++ terraform/datadog/elb.tf
@@ -32,23 +32,6 @@ resource "datadog_monitor" "abnormal_api_latency_doppler" {
   tags = ["deployment:${var.env}", "service:${var.env}_monitors", "job:doppler"]
 }
 
-resource "datadog_monitor" "abnormal_api_latency_router" {
-  name    = "${format("%s Abnormal API Latency - Router", var.env)}"
-  type    = "query alert"
-  message = "${format("{{#is_alert}}We're experiencing >= {{threshold}} change in ELB Latency.{{/is_alert}} \n{{#is_warning}}We're experiencing >= {{warn_threshold}} change in ELB Latency.{{/is_warning}} \n\nVisit the [Team Manual > Responding to alerts > API Latency](%s#api-latency) for more info.", var.datadog_documentation_url)}"
-
-  query = "${format("avg(last_15m):anomalies(avg:aws.elb.latency{name:%s-cf-router}, 'basic', 2, direction='both') > 0.3", var.env)}"
-
-  require_full_window = true
-
-  thresholds {
-    warning  = 0.15
-    critical = 0.3
-  }
-
-  tags = ["deployment:${var.env}", "service:${var.env}_monitors", "job:router"]
-}
-
 resource "datadog_monitor" "abnormal_api_latency_uaa" {
   name    = "${format("%s Abnormal API Latency - UAA", var.env)}"
   type    = "query alert"
