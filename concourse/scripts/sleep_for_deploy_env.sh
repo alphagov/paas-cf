d9354229fe769128e765e04b830276100a8c61a0
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2017-11-15 10:08:36 +0000

    Extract the splay duration into a variable.
    This makes it clearer what the splay is set to, and how it's applied.

--- concourse/scripts/sleep_for_deploy_env.sh
+++ concourse/scripts/sleep_for_deploy_env.sh
@@ -2,14 +2,15 @@
 
 set -eu
 
+splay=$((60*20))
 # The following sleep monstrosity deterministically sleeps for a
-# period of time between 0-20mins in order to prevent all our
+# period of time between 0-${splay} seconds in order to prevent all our
 # deletion jobs running at the same time. See the commit message for
 # how it works.
 
 sum=$(echo "${DEPLOY_ENV}" | md5sum);
 short=$(echo "${sum}" | cut -b 1-15)
 decimal=$((0x${short}));
-sleeptime=$((${decimal##-} % 60*20));
+sleeptime=$((${decimal##-} % splay));
 echo "Sleeping for ${sleeptime} seconds before continuing..."
 sleep ${sleeptime}
