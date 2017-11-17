46fb454a252c3a99e6c3498fbabadde8306325e2
bandesz <andras.ferencz-szabo@digital.cabinet-office.gov.uk>
2017-11-17 15:10:24 +0000

    Use the new Go Router health check on the UAA and router ELBs
    As we start to use the new health check we have to point the ELB health checks
    to the /health path. During release the old health check endpoint will still
    respond as expected as it's ignores the path.
    
    The HAProxy upgrade is required to remove the now redundant `HTTP-Monitor/1.1`
    User Agent-header and set up a dedicated backend configuration for healthcheck
    requests.

--- terraform/cloudfoundry/elbs.tf
+++ terraform/cloudfoundry/elbs.tf
@@ -61,7 +61,7 @@ resource "aws_elb" "cf_uaa" {
   }
 
   health_check {
-    target              = "HTTP:82/"
+    target              = "HTTP:82/health"
     interval            = "${var.health_check_interval}"
     timeout             = "${var.health_check_timeout}"
     healthy_threshold   = "${var.health_check_healthy}"
@@ -152,7 +152,7 @@ resource "aws_elb" "cf_router" {
   }
 
   health_check {
-    target              = "HTTP:82/"
+    target              = "HTTP:82/health"
     interval            = "${var.health_check_interval}"
     timeout             = "${var.health_check_timeout}"
     healthy_threshold   = "${var.health_check_healthy}"
