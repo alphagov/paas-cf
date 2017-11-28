e466668959bf6937ef4a257f332158f28e615b0d
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2017-11-28 17:09:24 +0000

    Update cell usable memory alert for ADR021  - Increase time period to 2 hours
     - Use Exponential Weighted Moving Average over a window of 5

--- terraform/datadog/cell.tf
+++ terraform/datadog/cell.tf
@@ -1,10 +1,10 @@
 resource "datadog_monitor" "cell-available-memory" {
   name               = "${format("%s cell available memory", var.env)}"
   type               = "query alert"
-  message            = "${format("Less than {{threshold}}%% memory free on cells. There is only {{value}} %% memory free on average on cells. Review if this is temporary or we really need to scale... @govpaas-alerting-%s@digital.cabinet-office.gov.uk", var.aws_account)}"
-  escalation_message = "There is only {{value}} % memory free on average on cells. Check the deployment!"
+  message            = "${format("Less than {{threshold}}%% memory free on cells. There is only {{value}}%% memory free on average on cells. Review if we need to scale... @govpaas-alerting-%s@digital.cabinet-office.gov.uk", var.aws_account)}"
+  escalation_message = "There is only {{value}}% memory free on average on cells. Check the deployment!"
   no_data_timeframe  = "7"
-  query              = "${format("avg(last_1m):avg:system.mem.pct_usable{bosh-job:cell,deploy_env:%s} * 100 < 50", var.env)}"
+  query              = "${format("avg(last_2h):ewma_5(avg:system.mem.pct_usable{bosh-job:cell,deploy_env:%s}) * 100 < 50", var.env)}"
 
   thresholds {
     warning  = "55.0"
