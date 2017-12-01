bae4c94019ae4648741f21cb93e47a8c283c8062
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2017-12-01 12:07:04 +0000

    Script to cleanup unused CDN broker IAM certs
    We're seeing multiple IAM server certificates for earh cdn-broker
    service instance. Only the most recent one for each instance is actually
    in use. These unused certs are causing us to come close to hitting the
    AWS service limits for IAM server certificates.
    
    This adds a script to clean these up. This is only a temporary solution
    until the cdn-broker can be updated to clean these up itself.

---
+++ scripts/cleanup_unused_cdn_broker_iam_certs.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+unless system "aws iam list-account-aliases > /dev/null"
+  abort "Make sure you've configured your AWS keys"
+end
+
+all_cdn_certs = JSON.parse %x(aws iam list-server-certificates --query 'ServerCertificateMetadataList[?Path == `/cloudfront/prod-letsencrypt/`]')
+
+cloudfront_used_certs = JSON.parse %x(aws cloudfront list-distributions --query 'DistributionList.Items[].ViewerCertificate.Certificate')
+
+all_cdn_certs.each do |cert|
+  if cloudfront_used_certs.include?(cert['ServerCertificateId'])
+    puts "skipping used cert #{cert['ServerCertificateId']} #{cert['ServerCertificateName']}"
+    next
+  end
+
+  puts "deleting unused cert #{cert['ServerCertificateId']} #{cert['ServerCertificateName']}"
+
+  system "aws iam delete-server-certificate --server-certificate-name '#{cert['ServerCertificateName']}'"
+end
