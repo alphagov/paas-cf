cd644d01dd3d5bcea7c233297d230884d9dcccd2
Hector Rivas Gandara <hector.rivasgandara@digital.cabinet-office.gov.uk>
2018-01-30 12:01:32 +0000

    Fix wrong name for old jwt signing key
    We made a typo in the name of the key when pulling the old jwt
    signing key for UAA during the certificate rotation.
    
    The name should be <cert-name> + _old + _key, as per [1].
    
    The issue would be shadowed by spruce, as we pull a default value
    if the key is not defined. We added a test for this specific
    case in the manifest.
    
    [1] https://github.com/alphagov/paas-cf/blob/0567d631f09a8b352fc8629375a81731f523069a/concourse/pipelines/create-cloudfoundry.yml#L1304-L1313

---
+++ manifests/cf-manifest/spec/manifest/uaa_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe "uaa properties" do
+  context "with the default certificates" do
+    let(:manifest) { manifest_with_defaults }
+    let(:properties) { manifest.fetch("properties") }
+
+    it "has the same certificate for jwt policy signing keys" do
+      jwt_keys = properties.fetch("uaa").fetch("jwt").fetch("policy").fetch("keys")
+      default_key = jwt_keys.fetch("default")
+      previous_key = jwt_keys.fetch("previous")
+      expect(default_key.fetch("signingKey")).not_to be_empty
+      expect(default_key).to eq(previous_key)
+    end
+  end
+  context "with and old certificate for jwt signing" do
+    let(:manifest) {
+      manifest_with_custom_stub %{---
+secrets:
+  uaa_jwt_signing_old_key: |
+    -----BEGIN RSA PRIVATE KEY-----
+    STUB_UAA_JWT_SIGNING_KEY_111111111111111111111111111111111111111
+    1111111111111111111111111111111111111111111111111111111111111111
+    1111111111111111111111111111111111111111111111111111111111111111
+    1111111111111111111111111111111111111111111111111111111111111111
+    1111111111111111111111111111111111111111111111111111111111111111
+    -----END RSA PRIVATE KEY-----}
+    }
+    let(:properties) { manifest.fetch("properties") }
+
+    it "has a different certificate for jwt policy signing keys" do
+      jwt_keys = properties.fetch("uaa").fetch("jwt").fetch("policy").fetch("keys")
+      default_key = jwt_keys.fetch("default")
+      previous_key = jwt_keys.fetch("previous")
+      expect(default_key.fetch("signingKey")).not_to be_empty
+      expect(default_key).not_to eq(previous_key)
+    end
+  end
+end
