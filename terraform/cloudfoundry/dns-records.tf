36fbac759e31f872f4ed147dd0e3d6f8362a81e3
henrytk <henry.knott@digital.cabinet-office.gov.uk>
2018-02-08 09:11:23 +0000

    Remove UAA DNS entries
    We now provide a wildcard DNS entry sending all subdomains of the system
    domain to the GoRouters via a dedicated ELB. Having specific DNS entries
    for any CloudFoundry routes directly under the system domain is no
    longer required.

--- terraform/cloudfoundry/dns-records.tf
+++ terraform/cloudfoundry/dns-records.tf
@@ -6,22 +6,6 @@ resource "aws_route53_record" "cf_cc" {
   records = ["${aws_elb.cf_cc.dns_name}"]
 }
 
-resource "aws_route53_record" "cf_uaa" {
-  zone_id = "${var.system_dns_zone_id}"
-  name    = "uaa.${var.system_dns_zone_name}."
-  type    = "CNAME"
-  ttl     = "60"
-  records = ["${aws_elb.cf_uaa.dns_name}"]
-}
-
-resource "aws_route53_record" "cf_login" {
-  zone_id = "${var.system_dns_zone_id}"
-  name    = "login.${var.system_dns_zone_name}."
-  type    = "CNAME"
-  ttl     = "60"
-  records = ["${aws_elb.cf_uaa.dns_name}"]
-}
-
 resource "aws_route53_record" "cf_doppler" {
   zone_id = "${var.system_dns_zone_id}"
   name    = "doppler.${var.system_dns_zone_name}."
