a1e3a1ad7a7bd8377a117575afa35e1b52aafd07
henrytk <henry.knott@digital.cabinet-office.gov.uk>
2018-02-08 09:24:21 +0000

    Remove Product Pages and Tech Docs DNS entries
    We now provide a wildcard DNS entry sending all subdomains of the system
    domain to the GoRouters via a dedicated ELB. Having specific DNS entries
    for any CloudFoundry routes directly under the system domain is no
    longer required.
    
    However, we do have to add a DNS record for the system domain itself
    because the CloudFront distributions were redirecting this domain
    to the Product Page.
    
    We keep the rest of the CloudFront infrastructure in place to ensure
    there is no downtime due to DNS caching. It will be removed in a
    subsequent deploy.

--- terraform/cloudfoundry/dns-records.tf
+++ terraform/cloudfoundry/dns-records.tf
@@ -38,6 +38,18 @@ resource "aws_route53_record" "apps_wildcard" {
   records = ["${aws_elb.cf_router.dns_name}"]
 }
 
+resource "aws_route53_record" "system_apex" {
+  zone_id = "${var.system_dns_zone_id}"
+  name    = "${var.system_dns_zone_name}."
+  type    = "A"
+
+  alias {
+    name                   = "${aws_elb.cf_router_system_domain.dns_name}"
+    zone_id                = "${aws_elb.cf_router_system_domain.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "apps_apex" {
   zone_id = "${var.apps_dns_zone_id}"
   name    = "${var.apps_dns_zone_name}."
