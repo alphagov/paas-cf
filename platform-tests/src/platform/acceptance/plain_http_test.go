add16c68a328f9a1d540fd16e5f1fd883d88ffc3
Michael Mokrysz <hi@46b.it>
2018-02-16 12:26:38 +0000

    Format HTTP acceptance tests under their domains
    This adds `Context` blocks to distinguish between things on the system
    domain and things on the apps domain. I think this improves clarity.

--- platform-tests/src/platform/acceptance/plain_http_test.go
+++ platform-tests/src/platform/acceptance/plain_http_test.go
@@ -20,74 +20,78 @@ var _ = Describe("plain HTTP requests", func() {
 		CONNECTION_TIMEOUT = 11 * time.Second
 	)
 
-	Describe("to the CC API", func() {
-		It("has the connection refused", func() {
-			uri := testConfig.ApiEndpoint + ":80"
-			_, err := net.DialTimeout("tcp", uri, CONNECTION_TIMEOUT)
-			Expect(err).To(HaveOccurred(), "should not connect")
-			Expect(err.(net.Error).Timeout()).To(BeTrue(), "should timeout")
+	Context("to the system domain", func() {
+		Describe("for the CC API", func() {
+			It("has the connection refused", func() {
+				uri := testConfig.ApiEndpoint + ":80"
+				_, err := net.DialTimeout("tcp", uri, CONNECTION_TIMEOUT)
+				Expect(err).To(HaveOccurred(), "should not connect")
+				Expect(err.(net.Error).Timeout()).To(BeTrue(), "should timeout")
+			})
+		})
+
+		Describe("for UAA", func() {
+			var uaaDomain string
+
+			BeforeEach(func() {
+				infoCommand := cf.Cf("curl", "/v2/info")
+				Expect(infoCommand.Wait(testConfig.DefaultTimeoutDuration())).To(Exit(0))
+
+				var infoResp struct {
+					TokenEndpoint string `json:"token_endpoint"`
+				}
+				err := json.Unmarshal(infoCommand.Buffer().Contents(), &infoResp)
+				Expect(err).NotTo(HaveOccurred())
+
+				uaaHttpsURL, err := url.Parse(infoResp.TokenEndpoint)
+				Expect(err).NotTo(HaveOccurred())
+				uaaDomain = strings.Split(uaaHttpsURL.Host, ":")[0]
+			})
+
+			It("has the connection refused", func() {
+				uri := uaaDomain + ":80"
+				_, err := net.DialTimeout("tcp", uri, CONNECTION_TIMEOUT)
+				Expect(err).To(HaveOccurred(), "should not connect")
+				Expect(err.(net.Error).Timeout()).To(BeTrue(), "should timeout")
+			})
+		})
+
+		Describe("for the product page", func() {
+			It("has the connection refused", func() {
+				systemDomain := GetConfigFromEnvironment("SYSTEM_DNS_ZONE_NAME")
+				uri := fmt.Sprintf("www.%s:80", systemDomain)
+				_, err := net.DialTimeout("tcp", uri, CONNECTION_TIMEOUT)
+				Expect(err).To(HaveOccurred(), "should not connect")
+				Expect(err.(net.Error).Timeout()).To(BeTrue(), "should timeout")
+			})
 		})
 	})
 
-	Describe("to UAA", func() {
-		var uaaDomain string
+	Context("to the apps domain", func() {
+		Describe("for an app", func() {
+			It("is redirected to the https endpoint", func() {
+				req, err := http.NewRequest("GET", fmt.Sprintf("http://foo.%s/", testConfig.AppsDomain), nil)
+				Expect(err).NotTo(HaveOccurred())
+				resp, err := http.DefaultTransport.RoundTrip(req)
+				Expect(err).NotTo(HaveOccurred())
 
-		BeforeEach(func() {
-			infoCommand := cf.Cf("curl", "/v2/info")
-			Expect(infoCommand.Wait(testConfig.DefaultTimeoutDuration())).To(Exit(0))
+				Expect(resp.StatusCode).To(Equal(301))
+				Expect(resp.Header.Get("Location")).To(Equal(fmt.Sprintf("https://foo.%s/", testConfig.AppsDomain)))
 
-			var infoResp struct {
-				TokenEndpoint string `json:"token_endpoint"`
-			}
-			err := json.Unmarshal(infoCommand.Buffer().Contents(), &infoResp)
-			Expect(err).NotTo(HaveOccurred())
+				By("does not include an HSTS header")
+				// See https://tools.ietf.org/html/rfc6797#section-7.2
+				Expect(resp.Header.Get("Strict-Transport-Security")).To(BeEmpty())
+			})
 
-			uaaHttpsURL, err := url.Parse(infoResp.TokenEndpoint)
-			Expect(err).NotTo(HaveOccurred())
-			uaaDomain = strings.Split(uaaHttpsURL.Host, ":")[0]
-		})
+			It("has any path and query components removed when redirecting", func() {
+				req, err := http.NewRequest("GET", fmt.Sprintf("http://foo.%s/bar?baz=qux", testConfig.AppsDomain), nil)
+				Expect(err).NotTo(HaveOccurred())
+				resp, err := http.DefaultTransport.RoundTrip(req)
+				Expect(err).NotTo(HaveOccurred())
 
-		It("has the connection refused", func() {
-			uri := uaaDomain + ":80"
-			_, err := net.DialTimeout("tcp", uri, CONNECTION_TIMEOUT)
-			Expect(err).To(HaveOccurred(), "should not connect")
-			Expect(err.(net.Error).Timeout()).To(BeTrue(), "should timeout")
-		})
-	})
-
-	Describe("to the product page", func() {
-		It("has the connection refused", func() {
-			systemDomain := GetConfigFromEnvironment("SYSTEM_DNS_ZONE_NAME")
-			uri := fmt.Sprintf("www.%s:80", systemDomain)
-			_, err := net.DialTimeout("tcp", uri, CONNECTION_TIMEOUT)
-			Expect(err).To(HaveOccurred(), "should not connect")
-			Expect(err.(net.Error).Timeout()).To(BeTrue(), "should timeout")
-		})
-	})
-
-	Describe("to an app", func() {
-		It("is redirected to the https endpoint", func() {
-			req, err := http.NewRequest("GET", fmt.Sprintf("http://foo.%s/", testConfig.AppsDomain), nil)
-			Expect(err).NotTo(HaveOccurred())
-			resp, err := http.DefaultTransport.RoundTrip(req)
-			Expect(err).NotTo(HaveOccurred())
-
-			Expect(resp.StatusCode).To(Equal(301))
-			Expect(resp.Header.Get("Location")).To(Equal(fmt.Sprintf("https://foo.%s/", testConfig.AppsDomain)))
-
-			By("does not include an HSTS header")
-			// See https://tools.ietf.org/html/rfc6797#section-7.2
-			Expect(resp.Header.Get("Strict-Transport-Security")).To(BeEmpty())
-		})
-
-		It("has any path and query components removed when redirecting", func() {
-			req, err := http.NewRequest("GET", fmt.Sprintf("http://foo.%s/bar?baz=qux", testConfig.AppsDomain), nil)
-			Expect(err).NotTo(HaveOccurred())
-			resp, err := http.DefaultTransport.RoundTrip(req)
-			Expect(err).NotTo(HaveOccurred())
-
-			Expect(resp.StatusCode).To(Equal(301))
-			Expect(resp.Header.Get("Location")).To(Equal(fmt.Sprintf("https://foo.%s/", testConfig.AppsDomain)))
+				Expect(resp.StatusCode).To(Equal(301))
+				Expect(resp.Header.Get("Location")).To(Equal(fmt.Sprintf("https://foo.%s/", testConfig.AppsDomain)))
+			})
 		})
 	})
 })
