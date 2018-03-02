b7e869b46ac1bd89fbadbf7a507a05e5f7ebdb4c
Hector Rivas Gandara <keymon@gmail.com>
2018-03-02 13:22:18 +0000

    Revert "Add jobs for scheduler vm type"
    This reverts commit 6868059395982f17fbc3e5e92c520240d90785d6.
    This reverts commit b7d0bd7efb89da16e78d514e25aa49e4d36ccdc0.
    This reverts commit 7d8797cdf986b981b1124943720026a1b7f84dbb.
    This reverts commit 32e1e085bfdb2c2aaef4a29ba2cb27bf7e7f323b.
    This reverts commit 76ed8e63b5c5b129bd7deb23232da301faae2533.
    This reverts commit 7ee9adb49a891a29e197ea7e6e168207e8a872e4.

--- manifests/cf-manifest/spec/manifest/logsearch_spec.rb
+++ manifests/cf-manifest/spec/manifest/logsearch_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe "Logsearch properties" do
   let(:manifest) { manifest_with_defaults }
   let(:properties) { manifest.fetch("properties") }
 
-  describe "curator instance group" do
+  describe "curator job" do
     it "contains purge_logs.retention_period. We expect retention period to be 30 days." do
       defs = properties.fetch("curator").fetch("purge_logs")
       expect(defs.fetch("retention_period")).to eq(30)
