9bb8a1293ef8fb5e2d7c26144f9e954fcb4c0f48
bandesz <andras.ferencz-szabo@digital.cabinet-office.gov.uk>
2018-03-23 11:25:16 +0000

    Move cloud config generation to a separate script file
    We want to be able to test the exact generation command therefore we move it to
    a separate script file and load the same file from the spec tests.

---
+++ manifests/cf-manifest/scripts/generate-cloud-config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+PAAS_CF_DIR=${PAAS_CF_DIR:-paas-cf}
+WORKDIR=${WORKDIR:-}
+
+bosh interpolate \
+  --var-errs \
+  --vars-file="${WORKDIR}/terraform-outputs/vpc.yml" \
+  --vars-file="${WORKDIR}/terraform-outputs/bosh.yml" \
+  --vars-file="${WORKDIR}/terraform-outputs/concourse.yml" \
+  --vars-file="${WORKDIR}/terraform-outputs/cf.yml" \
+  --vars-file="${WORKDIR}/cf-secrets/cf-secrets.yml" \
+  --vars-file="${PAAS_CF_DIR}/manifests/variables.yml" \
+  --vars-file="${CF_ENV_SPECIFIC_MANIFEST}" \
+  --vars-file="${WORKDIR}/environment-variables/environment-variables.yml" \
+  --ops-file="${PAAS_CF_DIR}/manifests/cf-manifest/cloud-config/operations/040-graphite.yml" \
+  --ops-file="${PAAS_CF_DIR}/manifests/cf-manifest/cloud-config/operations/050-rds-broker.yml" \
+  --ops-file="${PAAS_CF_DIR}/manifests/cf-manifest/cloud-config/operations/060-cdn-broker.yml" \
+  --ops-file="${PAAS_CF_DIR}/manifests/cf-manifest/cloud-config/operations/070-elasticache-broker.yml" \
+  --ops-file="${PAAS_CF_DIR}/manifests/cf-manifest/cloud-config/operations/080-logsearch.yml" \
+  "$@" \
+  "${PAAS_CF_DIR}/manifests/cf-manifest/cloud-config/000-base-cloud-config.yml"
