4792d33174b006be334656e9b115f9d603b8be51
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2018-04-27 13:48:22 +0100

    Add Datadog monitors for DNS resolution on VMs
    This adds 2 monitors around DNS resolution on each VM: 1 checks that DNS
    resolution works, the 2nd checks that it resolves in a timely manner.
    
    These are being added because we had a recent issue that we eventally
    tracked down to DNS resolution on one VM being flakey. In that instance
    restarting the VM fixed it. Having this check will enable us to see this
    sort of cause much more quickly.

---
+++ terraform/datadog/dns.tf
@@ -0,0 +1,31 @@
+resource "datadog_monitor" "dns_can_resolve" {
+  name    = "${format("%s DNS resolution working", var.env)}"
+  type    = "service check"
+  message = "DNS resolution is failing on {{host.name}}"
+
+  query = "${format("'dns.can_resolve'.over('deploy_env:%s').by('bosh-job','bosh-index').last(4).count_by_status()", var.env)}"
+
+  thresholds {
+    warning  = 1
+    critical = 3
+  }
+
+  tags = ["deployment:${var.env}", "service:${var.env}_monitors"]
+}
+
+resource "datadog_monitor" "dns_response_time" {
+  name                = "${format("%s DNS resolution response time", var.env)}"
+  type                = "metric alert"
+  message             = "DNS resolution is slow on {{host.name}}"
+  notify_no_data      = false
+  require_full_window = true
+
+  query = "${format("avg(last_5m):max:dns.response_time{deploy_env:%s} by {bosh-job,bosh-index} > 0.2", var.env)}"
+
+  thresholds {
+    warning  = 0.1
+    critical = 0.2
+  }
+
+  tags = ["deployment:${var.env}", "service:${var.env}_monitors"]
+}
