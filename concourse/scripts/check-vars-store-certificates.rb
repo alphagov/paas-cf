bac8b8d53a8c676dfc52f9f38e7fc8400b2a205f
bandesz <andras.ferencz-szabo@digital.cabinet-office.gov.uk>
2018-05-29 15:05:52 +0100

    Don't fail certificate checking if vars-store is empty
    In new dev environments the check-vars-store-certs job used to fail as the
    vars-store is empty.

--- concourse/scripts/check-vars-store-certificates.rb
+++ concourse/scripts/check-vars-store-certificates.rb
@@ -9,18 +9,20 @@ certs = YAML.safe_load(STDIN)
 
 alert = false
 
-certs.each { |variable, value|
-  next unless value.is_a?(Hash) && value.has_key?('certificate')
-  next if variable =~ /_old$/
-  certificate = OpenSSL::X509::Certificate.new value['certificate']
-  days_to_expire = ((certificate.not_after - Time.now) / (24 * 3600)).floor
+unless certs.nil?
+  certs.each { |variable, value|
+    next unless value.is_a?(Hash) && value.has_key?('certificate')
+    next if variable =~ /_old$/
+    certificate = OpenSSL::X509::Certificate.new value['certificate']
+    days_to_expire = ((certificate.not_after - Time.now) / (24 * 3600)).floor
 
-  if days_to_expire > ALERT_DAYS
-    puts "#{variable}: #{days_to_expire} days to expire. OK."
-  else
-    puts "#{variable}: #{days_to_expire} days to expire. ERROR! less than #{ALERT_DAYS} days."
-    alert = true
-  end
-}
+    if days_to_expire > ALERT_DAYS
+      puts "#{variable}: #{days_to_expire} days to expire. OK."
+    else
+      puts "#{variable}: #{days_to_expire} days to expire. ERROR! less than #{ALERT_DAYS} days."
+      alert = true
+    end
+  }
+end
 
 exit 1 if alert
