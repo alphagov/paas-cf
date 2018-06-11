e9136eadbc3fe0e71ed9eb971730fcacf4943174
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2018-06-11 15:21:16 +0100

    Revert "Extend rotate-cf-certs script to regenerate CA certs with SKIs"
    This has now been run in all persistent environments, so is no longer
    needed.
    
    This reverts commit 0ad96f6dc64aa4d5c9e29eedd94d9274b7cb4dba.

--- manifests/cf-manifest/scripts/rotate-cf-certs.rb
+++ manifests/cf-manifest/scripts/rotate-cf-certs.rb
@@ -2,8 +2,6 @@
 
 require 'optparse'
 require 'yaml'
-require 'open3'
-require 'tmpdir'
 
 BLANK_CERT = {
   "ca" => "",
@@ -18,7 +16,6 @@ def parse_args
   parser.on("--ca", "Rotate CA certs") { options[:ca] = true }
   parser.on("--leaf", "Rotate leaf certs") { options[:leaf] = true }
   parser.on("--delete", "Delete _old certificates") { options[:delete] = true }
-  parser.on("--ca-add-ski", "Regenerate CA certs to include a Subject Key Identifier") { options[:ca_add_ski] = true }
   parser.on("--manifest MANIFEST", "BOSH manifest") { |v| options[:manifest] = v }
   parser.on("--vars-store VARS", "BOSH variable store") { |v| options[:vars_store] = v }
   parser.parse!
@@ -68,45 +65,7 @@ def delete_old(vars, certs)
   certs
 end
 
-def regenerate_cas(vars, certs)
-  STDERR.puts "Regenerating CA certificates with Subject Key Identifiers"
-
-  workdir = Dir.mktmpdir('rotate-cf-certs')
-
-  vars.each do |var|
-    next unless var.fetch("options", {}).fetch("is_ca", false)
-    name = var.fetch("name")
-    next unless certs.is_a?(Hash) && certs.has_key?(name)
-    next if name.end_with?("_old")
-
-    common_name = var.fetch("options").fetch("common_name")
-    private_key = certs.fetch(name).fetch("private_key")
-
-    STDERR.puts "Generating #{name} with common name #{common_name}"
-
-    private_key_path = File.join(workdir, "private.key")
-    File.open(private_key_path, 'w') { |file| file.write(private_key) }
-
-    output = `certstrap init --expires "12 months" --common-name "#{common_name}" --passphrase "" --key "#{private_key_path}" --c "USA" -o "Cloud Foundry"`
-    STDERR.puts output
-    status = $?
-    raise "certstrap exited #{status}" if status != 0
-
-    cert = File.read("out/#{common_name}.crt")
-    certs[name]["ca"] = cert
-    certs[name]["certificate"] = cert
-
-    `rm -rf out/#{common_name}.*`
-  end
-
-  if !workdir.nil?
-    FileUtils.rm_rf(workdir)
-  end
-
-  certs
-end
-
-def rotate(manifest, certs, ca: false, leaf: false, delete: false, ca_add_ski: false)
+def rotate(manifest, certs, ca: false, leaf: false, delete: false)
   vars = manifest.fetch("variables").select { |v| v["type"] == 'certificate' }
 
   if delete
@@ -115,7 +74,6 @@ def rotate(manifest, certs, ca: false, leaf: false, delete: false, ca_add_ski: f
 
   certs = rotate_cas(vars, certs) if ca
   certs = rotate_leafs(vars, certs) if leaf
-  certs = regenerate_cas(vars, certs) if ca_add_ski
 
   certs
 end
