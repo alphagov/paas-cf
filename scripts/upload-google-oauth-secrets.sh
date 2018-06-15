2a1544fa99fa02dc0666fdf62e4cb69a486af206
henrytk <henry.knott@digital.cabinet-office.gov.uk>
2018-06-15 10:50:19 +0100

    Use MAKEFILE_ENV_TARGET for secret upload tasks
    This ensures we pull the correct env-specific secrets from the cred
    stores

--- scripts/upload-google-oauth-secrets.sh
+++ scripts/upload-google-oauth-secrets.sh
@@ -4,8 +4,8 @@ set -eu
 
 export PASSWORD_STORE_DIR=${OAUTH_PASSWORD_STORE_DIR}
 
-OAUTH_CLIENT_ID=$(pass "google/${AWS_ACCOUNT}/oauth/client_id")
-OAUTH_CLIENT_SECRET=$(pass "google/${AWS_ACCOUNT}/oauth/client_secret")
+OAUTH_CLIENT_ID=$(pass "google/${MAKEFILE_ENV_TARGET}/oauth/client_id")
+OAUTH_CLIENT_SECRET=$(pass "google/${MAKEFILE_ENV_TARGET}/oauth/client_secret")
 
 SECRETS=$(mktemp secrets.yml.XXXXXX)
 trap 'rm  "${SECRETS}"' EXIT
