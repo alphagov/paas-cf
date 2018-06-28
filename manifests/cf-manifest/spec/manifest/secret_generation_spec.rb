97fde30d3229a76e53fd1a228723343f0c06cfca
henrytk <henry.knott@digital.cabinet-office.gov.uk>
2018-06-28 12:32:36 +0100

    Add scripts to rename/rename cf-secrets to cf-vars
    We want to start using bosh to generate the passwords used in the
    bosh manifest, so that we are more aligned with the upstream
    cf-deployment.
    
    The passwords generated by bosh with bosh interpolate will be
    stored in the cf-vars-store.yml file in S3, which is a plain
    `key: value` similar to cf-secrets.yml
    
    We want to move here as many secrets as possible to cf-vars-store.yml
    but there are some cases we need to keep in cf-secrets:
    
       - vcap user password for cloud config, that needs
         to be passed as a SHA512 encoded password.
         We can actually stop setting this password, as the
         base stemcells set a random password by default
         as in [1]
    
       - The RDS passwords: The master password is used by terraform
         and then each password is configured in the create-cf-dbs.sh
         script. cf-deployment expects these variables to be passed
         as `external_*`[2], etc.
    
    We also want to rename variables to align them to the upstream
    names used in the original cf-deployment.
    
    To do this migration, we add two scripts and a task that calls
    them before the generate-cf-secrets:
    
     - migrate-cf-secrets.rb: will move and rename a given
       set of variables from cf-secrets.yml to cf-vars-store.yml.
       It also does some conversion for special cases with ssh
       keys, lists of passwords, or one-to-many mapping.
     - rename-cf-secrets.yml: renames variables in cf-secrets.yml
    
    The task contains the list of all the variables migrated and/or
    renamed and their new names.
    
    We keep in generate-cf-secrets.rb the secrets that we want to
    perserve in cf-secrets.yml, otherwise the script deletes them.
    There are a part of secrets that are not needed anymore:
    
     - secrets_loggregator_endpoint_shared_secret
     - secrets_uaa_cc_client_secret
    
    These scripts and task would be used for the migration, and removed later.
    
    [1] https://www.pivotaltracker.com/story/show/114110115
    [2] https://github.com/cloudfoundry/cf-deployment/blob/72c064181ab9420126b248cf43d6f0f97ced6394/operations/use-external-dbs.yml
    
    Signed-off-by: henrytk <henry.knott@digital.cabinet-office.gov.uk>
    Signed-off-by: Hector Rivas Gandara <keymon@gmail.com>

--- manifests/cf-manifest/spec/manifest/secret_generation_spec.rb
+++ manifests/cf-manifest/spec/manifest/secret_generation_spec.rb
@@ -8,41 +8,5 @@ RSpec.describe "secret generation" do
       expect(status).to be_success, "yamllint exited #{status.exitstatus}, output:\n#{output}"
       expect(output).to be_empty
     end
-
-    specify "it should be able to update and old secrets file" do
-      Tempfile.open(['original-cf-secrets', '.yml']) { |file|
-        file.write(%{
----
-secrets:
-  bbs_encryption_key: ___previous_bbs_encryption_key___
-  bulk_api_password: ___previous_bulk_api_password___
-  cc_db_encryption_key: ___previous_cc_db_encryption_key___
-  ssh_proxy_host_key:
-    private_key: |
-      aaaaaaaaaaa
-      bbbbbbbbbbb
-    public_fingerprint: f4:cc:45:d3:bb:33:ca:94:6a:51:7a:1c:fe:06:b7:a6
-})
-        file.flush
-        file.close
-
-        output, error, status = Open3.capture3(
-          root.join("manifests/cf-manifest/scripts/generate-cf-secrets.rb").to_s,
-          "--existing-secrets",
-          file.path.to_s
-        )
-        unless status.success?
-          raise "Error updating cf-secrets #{file.path}, exit: #{status.exitstatus}, output:\n#{output}\n#{error}"
-        end
-
-        resulting_secrets = YAML.safe_load(output)
-        expect(resulting_secrets["secrets"]).to be_nil
-        expect(resulting_secrets["secrets_bbs_encryption_key"]).to eq("___previous_bbs_encryption_key___")
-        expect(resulting_secrets["secrets_bulk_api_password"]).to eq("___previous_bulk_api_password___")
-        expect(resulting_secrets["secrets_cc_db_encryption_key"]).to eq("___previous_cc_db_encryption_key___")
-        expect(resulting_secrets["secrets_ssh_proxy_host_key"]).to eq("private_key" => "aaaaaaaaaaa\nbbbbbbbbbbb\n",
-          "public_fingerprint" => "f4:cc:45:d3:bb:33:ca:94:6a:51:7a:1c:fe:06:b7:a6")
-      }
-    end
   end
 end
