567a88becc753a6ca6a7370eaa6b8d8660360591
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2018-08-02 11:45:45 +0100

    Alert when a VM has >=25% swap utilisation
    In a recent incident [1] our gorouter EC2 instances gradually
    exhausted their RAM and then gradually exhausted their swap. We did
    not detect this until gorouter functionality was degraded.
    
    This commit adds a Datadog alert that will trigger when any of our
    VMs reach 25% swap utilisation. From examining the last 6 months, this
    would have detected some of our Elasticsearch queue problems and not
    have triggered otherwise.
    
    Our hope is that this alert will tell us if the memory exhaustion
    recurs. We'll revisit this at Incident Retro and identify wider
    mitigations and investigatory work.
    
    [1] https://status.cloud.service.gov.uk/incidents/m5n04kn1jw6g

--- terraform/datadog/general.tf
+++ terraform/datadog/general.tf
@@ -14,3 +14,19 @@ resource "datadog_monitor" "disk-space" {
 
   tags = ["deployment:${var.env}", "service:${var.env}_monitors", "job:all"]
 }
+
+resource "datadog_monitor" "vm-swap-utilisation" {
+  name  = "${format("%s VM swap utilisation", var.env)}"
+  type  = "metric alert"
+  query = "${format("avg(last_30m):100 - 100 * avg:system.swap.pct_free{deploy_env:%s} by {bosh-job,bosh-index} >= 25", var.env)}"
+
+  message             = "${format("{{bosh-job.name}}/{{bosh-index.name}} swap was {{#is_warning}}{{warn_threshold}}{{/is_warning}}{{#is_alert}}{{threshold}}{{/is_alert}}%% utilised over 30m @govpaas-alerting-%s@digital.cabinet-office.gov.uk", var.aws_account)}"
+  notify_no_data      = false
+  require_full_window = false
+
+  thresholds {
+    critical = "25"
+  }
+
+  tags = ["deployment:${var.env}", "service:${var.env}_monitors", "job:all"]
+}
