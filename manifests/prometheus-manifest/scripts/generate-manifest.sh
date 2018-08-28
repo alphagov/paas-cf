0529d8a0348a0e85206267dbe23bb4aa814a2eb3
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2018-08-28 15:17:57 +0100

    Opsfile to set vm_types to match cloud-config.
    The default prometheus manifest specifies a single 'default' VM type
    which doesn't exist in our config. We therefore update them to match our
    types.
    
    All but the prometheus2 vms have very minimal requirements, so get the
    nano VM type. the prometheus2 VM stores the metrics itself (the
    database VM is for grafana config alone), so it gets the medium size. It
    might need scaling in the future.

--- manifests/prometheus-manifest/scripts/generate-manifest.sh
+++ manifests/prometheus-manifest/scripts/generate-manifest.sh
@@ -5,4 +5,12 @@ set -euo pipefail
 PAAS_CF_DIR=${PAAS_CF_DIR:-paas-cf}
 PROM_BOSHRELEASE_DIR=${PAAS_CF_DIR}/manifests/prometheus-boshrelease
 
-cat "${PROM_BOSHRELEASE_DIR}/manifests/prometheus.yml"
+opsfile_args=""
+for i in "${PAAS_CF_DIR}"/manifests/prometheus-manifest/operations.d/*.yml; do
+  opsfile_args+="-o $i "
+done
+
+# shellcheck disable=SC2086
+bosh interpolate \
+  ${opsfile_args} \
+  "${PROM_BOSHRELEASE_DIR}/manifests/prometheus.yml"
