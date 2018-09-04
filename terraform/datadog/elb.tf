fdc8340562f0561537813cab299b9c03e54bfdf4
henrytk <henry.knott@digital.cabinet-office.gov.uk>
2018-09-04 11:24:17 +0100

    Replace Doppler latency monitor
    Despite various previous attempts to make this monitor less noisy we
    still get spurious alerts. I believe this is because the nature of the
    metric we are monitoring: the ELB latency is a very small value (around
    9µs) and is subject to high spikes, but not to a level where the latency
    is something we would worry about.
    
    This monitor achieves the same result: it gives us an indication of the
    health of the Dopplers and whether they are coping with the log and
    metric volumes.
    
    The monitor is a change alert because the metric is a count (and will
    therefore keep increasing for the lifecycle of the VM). It checks the
    maximum count in the last 10 minutes and compares it to the value an
    hour ago[1].
    
    [1] https://docs.datadoghq.com/api/?lang=python#create-a-monitor

--- terraform/datadog/elb.tf
+++ terraform/datadog/elb.tf
@@ -1,20 +1,3 @@
-resource "datadog_monitor" "abnormal_api_latency_doppler" {
-  name    = "${format("%s Abnormal API Latency - Doppler", var.env)}"
-  type    = "query alert"
-  message = "${format("{{#is_alert}}We're experiencing >= {{threshold}} change in ELB Latency.{{/is_alert}} \n{{#is_warning}}We're experiencing >= {{warn_threshold}} change in ELB Latency.{{/is_warning}} \n\nVisit the [Team Manual > Responding to alerts > API Latency](%s#api-latency) for more info. @govpaas-alerting-%s@digital.cabinet-office.gov.uk", var.datadog_documentation_url, var.aws_account)}"
-
-  query = "${format("avg(last_1h):anomalies(avg:aws.elb.latency{name:%s-cf-doppler}, 'agile', 2, direction='above', alert_window='last_30m', interval=20, count_default_zero='false', seasonality='weekly') > 0.5", var.env)}"
-
-  require_full_window = true
-
-  thresholds {
-    warning  = 0.25
-    critical = 0.5
-  }
-
-  tags = ["deployment:${var.env}", "service:${var.env}_monitors", "job:doppler"]
-}
-
 resource "datadog_monitor" "unhealthy_elb_node" {
   name           = "${format("%s At least one ELB node is not responding", var.env)}"
   type           = "metric alert"
