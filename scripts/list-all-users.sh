0c75cb2184b9344ae37bb4ace94bb20bcdd2056b
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-10-19 11:30:21 +0100

    Handle errors in ./scripts/list-all-users.sh

--- scripts/list-all-users.sh
+++ scripts/list-all-users.sh
@@ -6,11 +6,17 @@ uaac_endpoint=$(cf curl /v2/info | jq -r '.authorization_endpoint')
 oauth_token=$(cf oauth-token)
 
 get_number_of_users() {
-  curl --silent --fail "$uaac_endpoint/Users?count=0" -H "Authorization: $oauth_token" | jq -r '.totalResults'
+  if ! curl --silent --fail "$uaac_endpoint/Users?count=0" -H "Authorization: $oauth_token" | jq -r '.totalResults'; then
+    >&2 echo 'Error talking to UAA - are you logged in as an admin?'
+    exit 1
+  fi
 }
 
 get_users() {
-  curl --silent --fail "$uaac_endpoint/Users?attributes=userName&startIndex=$1&count=500" -H "Authorization: $oauth_token" | jq -r '.resources[].userName'
+  if ! curl --silent --fail "$uaac_endpoint/Users?attributes=userName&startIndex=$1&count=500" -H "Authorization: $oauth_token" | jq -r '.resources[].userName'; then
+    >&2 echo 'Error getting users from UAA - aborting'
+    exit 2
+  fi
 }
 
 for s in $(seq 0 500 "$(get_number_of_users)"); do
