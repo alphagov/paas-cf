ea5580cef551faf84b43d6142c254aa61a81727e
Hector Rivas Gandara <keymon@gmail.com>
2018-10-22 15:37:40 +0100

    Add bosh-api-client security group to prometheus
    We want to use the bosh-exporter in the Prometheus deployment.
    This exporter needs access to the bosh-api.
    
    We add the corresponding security group to the prometheus VMs,
    by adding the vm_extension definition in our cloudconfig and
    attaching that extension the prometheus VM in the
    prometheus deployment.

---
+++ manifests/cf-manifest/spec/cloud-config/vm_extensions_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe "vm_extensions" do
+  let(:manifest) { cloud_config_with_defaults }
+
+  describe "prometheus_lb" do
+    it "Should add the prometheus lb config" do
+      expect(manifest['vm_extensions.prometheus_lb.cloud_properties.lb_target_groups']).to_not be_empty
+    end
+  end
+
+  describe "bosh_client" do
+    it "Should add the bosh_client security group" do
+      puts manifest['vm_extensions']
+      expect(manifest['vm_extensions.bosh_client.cloud_properties.security_groups']).to_not be_empty
+    end
+  end
+end
