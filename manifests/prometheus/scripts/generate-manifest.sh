55fc8757dbeada7d84eeefa49dde3f4d00150f3f
Hector Rivas Gandara <keymon@gmail.com>
2018-10-22 14:53:26 +0100

    Enable monitor bosh in prometheus deployment
    We add the bosh_exporter with alerts and dashboards.
    
    For the time being we configure the bosh_exporter to use the same
    unique admin user from bosh, reading directly from the bosh-secrets.yml.
    
    We pass these credentials into a YAML vars-file in the prometheus
    generate-manifest.sh.
    
    In the future we should use a readonly user as described in the
    upstream opsfile[1], but this requires UAA in the bosh deployment.
    
    [1] https://github.com/bosh-prometheus/prometheus-boshrelease/blob/master/manifests/operators/bosh/add-bosh-exporter-uaa-clients.yml

--- manifests/prometheus/scripts/generate-manifest.sh
+++ manifests/prometheus/scripts/generate-manifest.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 PAAS_CF_DIR=${PAAS_CF_DIR:-paas-cf}
 PROM_BOSHRELEASE_DIR=${PAAS_CF_DIR}/manifests/prometheus/upstream
+WORKDIR=${WORKDIR:-.}
 
 opsfile_args=""
 for i in "${PAAS_CF_DIR}"/manifests/prometheus/operations.d/*.yml; do
@@ -14,5 +15,6 @@ done
 bosh interpolate \
   --var-errs \
   --vars-store "${VARS_STORE}" \
+  --vars-file "${VARS_FILE}" \
   ${opsfile_args} \
   "${PROM_BOSHRELEASE_DIR}/manifests/prometheus.yml"
