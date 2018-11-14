61db0df44c81e247ee6c543881b0ae714d0321b4
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2018-11-14 17:02:07 +0000

    Add test for prometheus monitor CF manifest details

---
+++ manifests/prometheus/spec/operations/monitor_cf_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe "Monitor CF" do
+  it "adds the firehose instance_group" do
+    firehose = manifest_with_defaults.get("instance_groups.prometheus2")
+    expect(firehose).not_to be_nil
+    expect(firehose["vm_type"]).to eq("medium")
+    expect(firehose["networks"]).to eq([{ "name" => "cf" }])
+  end
+
+  it "adds the cf_exporter job" do
+    expect(manifest_with_defaults.get("instance_groups.prometheus2.jobs.cf_exporter")).to_not be_nil
+  end
+
+  it "adds the cloudfoundry_dashboards job" do
+    expect(manifest_with_defaults.get("instance_groups.grafana.jobs.cloudfoundry_dashboards")).to_not be_nil
+  end
+end
