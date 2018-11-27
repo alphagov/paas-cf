2a0840dd39cea5d26fa6ff7eeb6b585ded4f3512
bandesz <andras.ferencz-szabo@digital.cabinet-office.gov.uk>
2018-11-27 10:50:08 +0000

    [paas-metrics] Add a simple test counter gauge

---
+++ tools/metrics/test_counter_gauge.go
@@ -0,0 +1,16 @@
+package main
+
+import "time"
+
+func TestCounterGauge(interval time.Duration) MetricReadCloser {
+	return NewMetricPoller(interval, func(w MetricWriter) error {
+		metrics := []Metric{}
+		metrics = append(metrics, Metric{
+			Kind:  Counter,
+			Time:  time.Now(),
+			Name:  "test.counter",
+			Value: 1,
+		})
+		return w.WriteMetrics(metrics)
+	})
+}
