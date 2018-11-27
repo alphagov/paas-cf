0d3c5456d36607a83988db4abedb452c0b09f16f
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2018-11-27 14:37:28 +0000

    Manage ses and metrics user group membership with terraform.
    Now that the aws_iam_user_group_membership resource exists[1], we can
    add these users to the relevant groups without clobbering existing
    members of the group, meaning that our workaround is no longer
    necessary.
    
    [1]https://github.com/terraform-providers/terraform-provider-aws/pull/3365

--- terraform/cloudfoundry/metrics-exporter.tf
+++ terraform/cloudfoundry/metrics-exporter.tf
@@ -4,17 +4,10 @@ resource "aws_iam_user" "metrics_exporter" {
   force_destroy = true
 }
 
-# Until this feature request is not solved https://github.com/terraform-providers/terraform-provider-aws/issues/113,
-# `aws_iam_group_membership` will wipe all the other members from the
-# shared group.
-#
-# The workaround is use aws cli:
-#
-#   aws iam add-user-to-group --user-name "metrics-exporter-${DEPLOY_ENV}" --group-name metrics-exporters
-#
-# We could do it using terraform provisioner local-exec calling out awscli
-# but we want to avoid this pattern so we will do it in a script in
-# the next step.
+resource "aws_iam_user_group_membership" "metrics_exporter" {
+  user   = "${aws_iam_user.metrics_exporter.name}"
+  groups = ["metrics-exporters"]
+}
 
 resource "aws_iam_access_key" "metrics_exporter" {
   user = "${aws_iam_user.metrics_exporter.name}"
--- terraform/cloudfoundry/smtp.tf
+++ terraform/cloudfoundry/smtp.tf
@@ -4,28 +4,10 @@ resource "aws_iam_user" "ses_smtp" {
   force_destroy = true
 }
 
-# Until this feature request is not solved https://github.com/terraform-providers/terraform-provider-aws/issues/113,
-# `aws_iam_group_membership` will wipe all the other members from the
-# shared group.
-#
-# The workaround is use aws cli:
-#
-#   aws iam add-user-to-group --user-name "ses-smtp-${DEPLOY_ENV}" --group-name ses-smtp
-#
-# We could do it using terraform provisioner local-exec calling out awscli
-# but we want to avoid this pattern so we will do it in a script in
-# the next step.
-#
-# Once they fix it upstream, we can replace it with this code:
-#
-# resource "aws_iam_group_membership" "ses_smtp" {
-#    name = "ses_smtp"
-#    group = "ses_smtp"
-#    users = [
-#        "${aws_iam_user.ses_smtp.name}",
-#    ]
-#    append = true
-#}
+resource "aws_iam_user_group_membership" "ses_smtp" {
+  user   = "${aws_iam_user.ses_smtp.name}"
+  groups = ["email-senders"]
+}
 
 resource "aws_iam_access_key" "ses_smtp" {
   user = "${aws_iam_user.ses_smtp.name}"
