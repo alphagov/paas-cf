eabfadc5e3557ac3b39d40fbd3383211b0e2cbf8
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2018-12-14 16:11:50 +0000

    Remove DataDog part 1: Remove all monitors
    In order to remove DataDog without causing loads of noise this runs a
    DataDog terraform destroy to remove all the monitors ahead of upcoming
    changes to remove the agents etc that generate the metrics.
    
    This also removes the config step to genetate the list of job_instances
    that's only used in the datadog terraform apply task.

--- scripts/job_instances.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-require File.expand_path("../lib/job_instances_lib", __FILE__)
-
-manifest_path = ARGV[0]
-manifest = File.read(manifest_path)
-
-puts JobInstances.generate manifest
