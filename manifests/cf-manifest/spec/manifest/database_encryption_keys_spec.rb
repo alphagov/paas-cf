964def694c8284d2084c2939cc174686478749c4
Hector Rivas Gandara <keymon@gmail.com>
2018-12-19 16:56:02 +0000

    Allow rotate CC database encryption key
    We will implement a logic similar to the one implemented for
    the jwt token[1]. The CC encryption key name would be a password itself
    that would be rotated, called `cc_db_encryption_key_id`
    
    We use the `cc_db_encryption_key_id_old` and `cc_db_encryption_key_old`
    to allow transit between key rotations.
    
    Here we add a dedicated job to rotate the DB keys, as it is a more
    sensitive task that will not be run as often. This task rotates
    the database key `cc_db_encryption_key` and the id
    `cc_db_encryption_key_id` at the same time.
    
    We need to also add cc_db_encryption_key_id as a secret to preserve
    during the key rotation.
    
    We move the related configuration of the vm_extension for the errand
    definition to the operations file including all the rotation
    config.
    
    We drop the the default `encryption_key_0` that comes from
    cf-deployment, but we will add logic in a later commit to retrieve the
    current value to allow rotation for the first time.
    
    [1] https://github.com/alphagov/paas-cf/pull/1184

---
+++ manifests/cf-manifest/spec/manifest/database_encryption_keys_spec.rb
@@ -0,0 +1,48 @@
+RSpec.describe "database encryption keys" do
+  let(:manifest) { manifest_without_vars_store }
+
+  describe "CC database encryption key" do
+    %w{
+      instance_groups.api.jobs.cloud_controller_ng.properties
+      instance_groups.cc-worker.jobs.cloud_controller_worker.properties
+      instance_groups.scheduler.jobs.cloud_controller_clock.properties
+    }.each do |job_properties_path|
+
+      it "has a default encryption key configured" do
+        properties = manifest.fetch(job_properties_path)
+
+        current_key_label = properties.fetch("cc").fetch("database_encryption").fetch("current_key_label")
+        keys = properties.fetch("cc").fetch("database_encryption").fetch("keys")
+
+        expect(current_key_label).to eq("((cc_db_encryption_key_id))")
+        expect(keys[current_key_label]).to eq("((cc_db_encryption_key))")
+      end
+
+      it "it keeps the _old key as key" do
+        properties = manifest.fetch(job_properties_path)
+
+        keys = properties.fetch("cc").fetch("database_encryption").fetch("keys")
+
+        expect(keys).to include("((cc_db_encryption_key_id))" => "((cc_db_encryption_key))")
+        expect(keys).to include("((cc_db_encryption_key_id_old))" => "((cc_db_encryption_key_old))")
+      end
+    end
+
+    it "only has ((cc_db_encryption_key)) in the expected locations" do
+      found_locations = manifest.inject([]) { |acum, v, path|
+        new_acum = acum
+        new_acum << path if v.is_a?(String) && v.include?('((cc_db_encryption_key))')
+        new_acum
+      }
+      expected_locations = %w{
+        /instance_groups/name=api/jobs/name=cloud_controller_ng/properties/cc/database_encryption/keys/((cc_db_encryption_key_id))
+        /instance_groups/name=api/jobs/name=cloud_controller_ng/properties/cc/db_encryption_key
+        /instance_groups/name=cc-worker/jobs/name=cloud_controller_worker/properties/cc/database_encryption/keys/((cc_db_encryption_key_id))
+        /instance_groups/name=cc-worker/jobs/name=cloud_controller_worker/properties/cc/db_encryption_key
+        /instance_groups/name=scheduler/jobs/name=cloud_controller_clock/properties/cc/database_encryption/keys/((cc_db_encryption_key_id))
+        /instance_groups/name=scheduler/jobs/name=cloud_controller_clock/properties/cc/db_encryption_key
+      }
+      expect(found_locations).to contain_exactly(*expected_locations)
+    end
+  end
+end
