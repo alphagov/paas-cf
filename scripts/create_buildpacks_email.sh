7e7b46d2be1a3484bff24e2a799c88933dacc26a
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2019-01-18 17:15:27 +0000

    Automate buildpack update notification creation
    Rather than picking through release pages on GitHub, this will allow generation of the notification email, and creation of a markdown file to describe the latest buildpack update
    
    This details:
    
    * The buildpacks that have been updated
    * The version they are now at
    * Any changes to the included dependencies
    * Release notes for all the interim release notes in the buildpack's repository
    
    This is run with the script `scripts/create_buildpacks_email.sh`. By default, it targets the last time `buildpacks.yml` changed, and compares that to now.
    If comparison to another point in time is required, it can be run with the commit hash as the first argument.
    
    Running this will generate the email text on stdout, and will write a file to `doc/buildpack_release.md` which can then be committed.
    
    Co-authored-by: Richard Towers <richard.towers@digital.cabinet-office.gov.uk>

---
+++ scripts/create_buildpacks_email.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e -u -o pipefail
+
+MIN_GO_VERSION=1.11
+
+GORAWVERSION=$(go version)
+if [[ ${GORAWVERSION} =~ ([0-9]+\.[0-9]+(\.[0-9])?) ]]
+then
+    if [ "$(echo -e "${MIN_GO_VERSION}\n${BASH_REMATCH[1]}" | sort -V | head -n 1)" != "${MIN_GO_VERSION}" ]
+    then
+        echo "at least go ${MIN_GO_VERSION} is required"
+        exit 1
+    fi
+fi
+cd "$(dirname "$0")/.."
+root_dir="$(pwd)"
+ARG=${1:-}
+
+if [ "${ARG}" == "-h" ] || [ "${ARG}" == "--help" ]
+then
+    echo -e "USAGE:\n   ${0} [old commit sha to diff against / --help / -h]"
+    exit 0
+elif [[ -n ${ARG} ]]
+then
+    previous_commit="${ARG}"
+else
+    previous_commit="$(git log --format=%H --max-count 1 --skip 1 -- "config/buildpacks.yml")"
+fi
+
+cd "${root_dir}/tools/buildpacks"
+
+go run email.go structs.go -old <(git show "$previous_commit:config/buildpacks.yml") -new <(git show "head:config/buildpacks.yml")
