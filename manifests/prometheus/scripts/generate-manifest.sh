69d247b70342d2cf7a4f0d0d96609b087b972972
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2019-01-31 15:38:56 +0000

    Move prometheus manifest generation logic out of pipeline
    This is easier to follow if all the logic is contained within the
    generate-manifest script. This also helps ensure parity between the
    tests and the pipeline.

--- manifests/prometheus/scripts/generate-manifest.sh
+++ manifests/prometheus/scripts/generate-manifest.sh
@@ -21,11 +21,6 @@ for i in "${PAAS_CF_DIR}"/manifests/prometheus/alerts.d/*.yml; do
   alerts_opsfile_args+="-o $i "
 done
 
-varsfile_args=""
-for i in ${VARS_FILES}; do
-  varsfile_args+="--vars-file $i "
-done
-
 vars_store_args=""
 if [ -n "${VARS_STORE:-}" ]; then
   vars_store_args=" --var-errs --vars-store ${VARS_STORE}"
@@ -35,9 +30,35 @@ if [ "${ENABLE_ALERT_EMAILS:-}" == "false" ]; then
   opsfile_args+="-o ${PAAS_CF_DIR}/manifests/prometheus/operations/disable-email.yml"
 fi
 
+variables_file="$(mktemp)"
+trap 'rm -f "${variables_file}"' EXIT
+
+bosh interpolate - \
+  --var-errs \
+  --vars-file "${WORKDIR}/bosh-vars-store/bosh-vars-store.yml" \
+  --vars-file "${WORKDIR}/cf-vars-store/cf-vars-store.yml" \
+> "${variables_file}" \
+<<EOF
+---
+metrics_environment: $DEPLOY_ENV
+bosh_url: $BOSH_URL
+uaa_bosh_exporter_client_secret: ((bosh_exporter_password))
+system_domain: $SYSTEM_DNS_ZONE_NAME
+app_domain: $APPS_DNS_ZONE_NAME
+metron_deployment_name: $DEPLOY_ENV
+skip_ssl_verify: false
+traffic_controller_external_port: 443
+uaa_clients_cf_exporter_secret: ((uaa_clients_cf_exporter_secret))
+uaa_clients_firehose_exporter_secret: ((uaa_clients_firehose_exporter_secret))
+aws_account: $AWS_ACCOUNT
+EOF
+
 # shellcheck disable=SC2086
 bosh interpolate \
-  ${varsfile_args} \
+  --vars-file="${variables_file}" \
+  --vars-file="${WORKDIR}/terraform-outputs/cf.yml" \
+  --vars-file="${PAAS_CF_DIR}/manifests/prometheus/env-specific/${ENV_SPECIFIC_BOSH_VARS_FILE}" \
+  --var-file bosh_ca_cert="${WORKDIR}/bosh-CA-crt/bosh-CA.crt" \
   ${opsfile_args} \
   ${alerts_opsfile_args} \
   ${vars_store_args} \
--- manifests/prometheus/spec/support/manifest_helpers.rb
+++ manifests/prometheus/spec/support/manifest_helpers.rb
@@ -15,23 +15,41 @@ module ManifestHelpers
 
 private
 
-  def render_manifest_with_defaults
-    root = Pathname.new(File.expand_path("../../../..", __dir__))
-    vars_store = Tempfile.new(['vars-store', '.yml'])
+  def root
+    Pathname.new(File.expand_path("../../../..", __dir__))
+  end
 
-    env = {
-      'PAAS_CF_DIR' => root.to_s,
-      'VARS_STORE' => vars_store.path,
-      'VARS_FILES' => [
-        "#{root}/manifests/prometheus/spec/fixtures/prometheus-vars-file.yml",
-        "#{root}/manifests/shared/spec/fixtures/terraform/cf.yml",
-        "#{root}/manifests/prometheus/env-specific/default.yml",
-      ].join(' ')
-    }
+  def fake_env_vars
+    env = {}
+    env['AWS_ACCOUNT'] = 'dev'
+    env['SYSTEM_DNS_ZONE_NAME'] = 'system.example.com'
+    env['APPS_DNS_ZONE_NAME'] = 'apps.example.com'
+    env['DEPLOY_ENV'] = 'test'
+    env['BOSH_URL'] = 'https://bosh.example.com:25555'
+    env['ENV_SPECIFIC_BOSH_VARS_FILE'] = 'default.yml'
+    env
+  end
+
+  def render_manifest_with_defaults
+    workdir = Dir.mktmpdir("workdir")
+
+    vars_store = Tempfile.new(['vars-store', '.yml'])
+    copy_terraform_fixtures("#{workdir}/terraform-outputs", %w(cf))
+    copy_fixture_file('bosh-vars-store.yml', "#{workdir}/bosh-vars-store")
+    copy_fixture_file('cf-vars-store.yml', "#{workdir}/cf-vars-store")
+    copy_fixture_file('bosh-CA.crt', "#{workdir}/bosh-CA-crt")
+
+    env = fake_env_vars
+    env['PAAS_CF_DIR'] = root.to_s
+    env['WORKDIR'] = workdir
+    env['VARS_STORE'] = vars_store.path
+
     output, error, status = Open3.capture3(env, "#{root}/manifests/prometheus/scripts/generate-manifest.sh")
     expect(status).to be_success, "generate-manifest.sh exited #{status.exitstatus}, stderr:\n#{error}"
 
     DeepFreeze.freeze(PropertyTree.load_yaml(output))
+  ensure
+    FileUtils.rm_r(workdir)
   end
 end
 
