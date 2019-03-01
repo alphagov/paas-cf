66843624e3a7a36e3fd806bd0f43e40116aa73f5
Mohamed Deerow <mogds@users.noreply.github.com>
2019-03-01 11:03:45 +0000

    Create pipeline definition & OpsUX for cross-region smoke testing
    This commit adds the ability for one DEPLOY_ENV's deployer Concourse to monitor
    one or more other DEPLOY_ENV's Cloud Foundries, via the standard CF smoke tests.
    
    In order for env1 to monitor env2, the following must be true:
    
    - env1 must be able to access env2's state bucket.
    - env1 must be able to access env2's CF API
      - this is more of a problem in dev than in prod; in dev you may wish to test
        this by having an env test itself - this works as expected.
    - env2 must have had the `set-smoke-test-creds` job run at least once
      - it's in the `credentials` job group of the `create-cloudfoundry` pipeline
      - this job also rotates creds if they already exist.
    - a symlink from `paas-cf::/concourse/pipelines/monitor-<env2>.yml` must
      point towards the target `paas-cf::/concourse/pipelines/monitor-remote.yml`.
      - `prod` and `prod-lon` have been set up as part of this commit.
    
    Once this is in place, run the following in an AWS STS context:
    
    ```
    $ DEPLOY_ENV=env1 make <AWS-account> monitor-<env2>
    ```
    
    e.g.
    
    ```
    $ DEPLOY_ENV=jonmtest make dev monitor-modeerow
    ```
    
    This will upload a new pipeline to env1's deployer Concourse, named
    `monitor-<env2>`.
    
    Once set up, this pipeline will update itself automatically as the contents
    of `monitor-remote.yml` changes.

--- concourse/scripts/smoke_tests_email.sh
+++ concourse/scripts/smoke_tests_email.sh
@@ -6,14 +6,23 @@ DEPLOY_ENV=$1
 SYSTEM_DNS_ZONE_NAME=$2
 ALERT_EMAIL_ADDRESS=$3
 
+set +u
+if [ "$4" = "" ]; then
+  message_file=message-local.json
+  MONITORED_DEPLOY_ENV=""
+else
+  message_file=message-remote.json
+  MONITORED_DEPLOY_ENV=$4
+fi
+set -u
+
 TO="${ALERT_EMAIL_ADDRESS}"
 FROM="${ALERT_EMAIL_ADDRESS}"
 # SMOKE_TEST_LOG=./smoke-tests-log/smoke-tests.log
 # LAST_COMMIT_LOG=./smoke-tests-log/last-commit.log
 
 write_message_json() {
-
-  cat <<EOF > message.json
+  cat <<EOF > message-local.json
 {
   "Subject": {
     "Data": "Smoke tests failed in ${DEPLOY_ENV}"
@@ -27,8 +36,22 @@ write_message_json() {
   }
 }
 EOF
+  cat <<EOF > message-remote.json
+{
+  "Subject": {
+    "Data": "Smoke tests failed in ${MONITORED_DEPLOY_ENV}, says ${DEPLOY_ENV}"
+  },
+  "Body": {
+    "Html": {
+      "Data": "The smoke tests monitoring <b>${MONITORED_DEPLOY_ENV}</b> have failed in ${DEPLOY_ENV}'s Concourse. See \
+      <a href='https://deployer.${SYSTEM_DNS_ZONE_NAME}/teams/main/pipelines/monitor-${MONITORED_DEPLOY_ENV}'>Concourse</a> \
+      for details<br/>"
+    }
+  }
+}
+EOF
 }
 
 write_message_json
 
-aws ses send-email --region eu-west-1 --to "${TO}" --message file://message.json --from "${FROM}"
+aws ses send-email --region eu-west-1 --to "${TO}" --message file://${message_file} --from "${FROM}"
