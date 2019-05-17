b6ad0b20db78a30137d7f1fe3ab50f89d13697d6
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-05-17 12:08:50 +0100

    Add a script to list the routes in a region
    This may be useful when we're trying to understand what things are
    running on the platform.
    
    Possible future improvements:
    
    - send a `dig` to custom domains to see if DNS is actually setup
    - send an HTTP request to the domain to see if it's serving 200s
    
    [skip-ci]

---
+++ scripts/list-routes.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'set'
+
+routes_by_domain_url = {}
+
+page = JSON.parse `cf curl '/v2/routes?results-per-page=100'`
+next_url = page["next_url"]
+
+while next_url
+  page = JSON.parse `cf curl '#{next_url}'`
+  page['resources'].map do |resource|
+    host = resource.dig('entity', 'host')
+    unless host.empty?
+      domain_url = resource.dig('entity', 'domain_url')
+      if routes_by_domain_url.include? domain_url
+        routes_by_domain_url[domain_url] << host
+      else
+        routes_by_domain_url[domain_url] = [host].to_set
+      end
+    end
+  end
+  next_url = page["next_url"]
+end
+
+routes_by_domain_url.each do |url, hosts|
+  domain = JSON.parse(`cf curl '#{url}'`).dig('entity', 'name')
+  hosts.each do |host|
+    puts "https://#{host}.#{domain}"
+  end
+end
