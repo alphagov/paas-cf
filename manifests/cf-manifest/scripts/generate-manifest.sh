c32f9f1a2e42574dc57bfa0fa3cded476dec4f4f
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-05-31 13:44:09 +0100

    Use cf-deployment ops file for internal route svc
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- manifests/cf-manifest/scripts/generate-manifest.sh
+++ manifests/cf-manifest/scripts/generate-manifest.sh
@@ -39,6 +39,7 @@ bosh interpolate \
   --vars-file="${PAAS_CF_DIR}/manifests/variables.yml" \
   --vars-file="${ENV_SPECIFIC_BOSH_VARS_FILE}" \
   --vars-file="${WORKDIR}/environment-variables.yml" \
+  --ops-file="${CF_DEPLOYMENT_DIR}/operations/use-internal-lookup-for-route-services.yml" \
   ${opsfile_args} \
   --ops-file="${PAAS_CF_DIR}/manifests/cf-manifest/operations/uaa-add-google-oauth.yml" \
   --ops-file="${PAAS_CF_DIR}/manifests/cf-manifest/operations/uaa-add-microsoft-oauth.yml" \
