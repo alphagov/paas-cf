5cf8466cbe3f25d1fe06adfe076700def49702c8
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-06-17 21:21:26 +0100

    Remove previous_generation instance types
    Now that the bosh-aws-cpi has been upgraded past v74, we can use the
    latest instance types for things with persistent disks
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- manifests/prometheus/spec/operations/monitor_cf_spec.rb
+++ manifests/prometheus/spec/operations/monitor_cf_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe "Monitor CF" do
   it "adds the firehose instance_group" do
     firehose = manifest_with_defaults.get("instance_groups.prometheus2")
     expect(firehose).not_to be_nil
-    expect(firehose["vm_type"]).to eq("medium_previous_generation")
+    expect(firehose["vm_type"]).to eq("medium")
     expect(firehose["networks"]).to eq([{ "name" => "cf" }])
   end
 
