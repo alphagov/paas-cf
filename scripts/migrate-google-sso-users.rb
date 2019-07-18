2094817f05055475c80b948142e981619f80f682
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2019-07-18 14:19:16 +0100

    Script to migrate Google SSO users to have id usernames
    Idempotent script to migrate Google SSO users to have Google id's as usernames,
    given a CSV of email addresses and corresponding id's. This is a one-off script
    that needs to be run against each environment.
    
    The script will first run through its inputs and update usernames, then runs
    through all UAA users and verifies Google SSO users have the correct type of
    username (not an email address, and looking like a Google id)
    
    Usage:
        CF_ADMIN_PASSWORD=foo ruby ./scripts/migrate-google-sso-users.rb CSV_FILE_PATH CF_API_URL

---
+++ scripts/migrate-google-sso-users.rb
@@ -0,0 +1,99 @@
+require 'csv'
+require 'uri'
+require File.expand_path("../lib/uaa_sync_admin_users", __FILE__)
+
+# rubocop:disable Lint/RequireParentheses, Style/MultilineTernaryOperator
+module CF
+  module UAA
+    module Http
+      def json_parse_reply(style, status, body, headers)
+        raise ArgumentError unless style.nil? || style.is_a?(Symbol)
+        unless [200, 201, 204, 400, 401, 403, 409, 422].include? status
+          raise (status == 404 ? NotFound : BadResponse), "invalid status response: #{status}"
+        end
+        if body && !body.empty? && (status == 204 || headers.nil? ||
+              headers['content-type'] !~ /application\/json/i)
+          raise BadResponse, 'received invalid response content or type'
+        end
+        parsed_reply = Util.json_parse(body, style)
+        if status >= 400
+          puts parsed_reply.inspect
+          raise parsed_reply && parsed_reply['error'] == 'invalid_token' ?
+              InvalidToken.new(parsed_reply) : TargetError.new(parsed_reply), 'error response'
+        end
+        parsed_reply
+      rescue DecodeError
+        raise BadResponse, 'invalid JSON response'
+      end
+    end
+  end
+end
+# rubocop:enable Lint/RequireParentheses, Style/MultilineTernaryOperator
+
+file_path = ARGV[0] || raise("You must pass the path to the user ids CSV as first argument")
+cf_api_url = ARGV[1] || raise("You must pass API endpoint as second argument")
+cf_admin_username = 'admin'
+cf_admin_password = ENV['CF_ADMIN_PASSWORD'] || raise("Must set $CF_ADMIN_PASSWORD env var")
+
+options = {}
+options[:skip_ssl_validation] = true if ENV['SKIP_SSL_VERIFICATION'].to_s.casecmp("true").zero?
+
+uaa_sync_admin_users = UaaSyncAdminUsers.new(cf_api_url, cf_admin_username, cf_admin_password, options)
+uaa_sync_admin_users.request_token
+
+
+csv_lines = CSV.read(file_path)
+
+email_to_id_map = {}
+
+csv_lines.each { |line|
+  email_to_id_map[line[0]] = line[1]
+}
+id_to_email_map = email_to_id_map.invert
+
+puts "===> Considering the following email addresses"
+puts email_to_id_map.keys
+
+
+puts ""
+puts "===> Fetching & updating users"
+pad = email_to_id_map.keys.max_by(&:length).length
+email_to_id_map.each { |email, id|
+  user = uaa_sync_admin_users.get_user_by_username(email)
+
+  if user == nil
+    puts email.rjust(pad) + ": NOT FOUND"
+  else
+    if user["origin"] != "google"
+      puts email.rjust(pad) + ": NOT A GOOGLE USER"
+      next
+    end
+
+    puts email.rjust(pad) + ": WILL UPDATE TO ID " + id
+    user["username"] = id
+    uaa_sync_admin_users.update_user(user)
+    puts email.rjust(pad) + ": Updated UAA user " + user["id"]
+  end
+}
+
+puts ""
+puts "==> Verifying all Google SSO users have been migrated"
+all_users = uaa_sync_admin_users.get_all_users
+usernames = all_users.map { |u| u["username"] }
+pad = usernames.max_by(&:length).length
+all_users.each { |user|
+  if user["origin"] != "google"
+    next
+  elsif user["origin"] == "google"
+    if user["username"].match("\d{20,}") != nil
+      puts user["username"].rjust(pad) + ": FAILURE. SHOULD HAVE BEEN SOMETHING LIKE A GOOGLE ID"
+      next
+    end
+  end
+
+  if user["username"].match(URI::MailTo::EMAIL_REGEXP) != nil
+    puts user["username"].rjust(pad) + ": FAILURE"
+  else
+    puts user["username"].rjust(pad) + ": PASS (username: " + id_to_email_map[user["username"]] + ")"
+  end
+}
