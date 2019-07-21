a55fa718c6e8b18345431f0ae2ad5f47777899bc
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-07-21 15:33:06 +0100

    cf-deployment v🔟 - remove containers remove op
    This remove operation was done to achieve a no-op manifest, however now
    we want rep containers to be set up, because we want mtls everywhere
    (envoy/istio).
    
    This commit also adds two simple tests to ensure that these properties
    are configured.
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

---
+++ manifests/cf-manifest/spec/manifest/diego_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe "diego" do
+  context "with the default certificates" do
+    let(:manifest) { manifest_with_defaults }
+    let(:properties) { manifest.fetch("instance_groups.diego-cell.jobs.rep.properties") }
+
+    it "has containers configured" do
+      expect(properties.dig('containers')).not_to be_nil
+    end
+
+    it "has containers/proxy enabled" do
+      expect(properties.dig('containers', 'proxy', 'enabled')).to be(true)
+    end
+  end
+end
