64b903100d4a8d9615a7bd365888ae4f3be5157c
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-08-02 15:13:18 +0100

    Fix acceptance tests for paas-metrics/currency
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- tools/metrics/acceptance/currency_test.go
+++ tools/metrics/acceptance/currency_test.go
@@ -8,8 +8,8 @@ import (
 var _ = Describe("Currency", func() {
 	It("should return currency metrics", func() {
 		Expect(metricFamilies).To(SatisfyAll(
-			HaveKey("paas_currency_real"),
-			HaveKey("paas_currency_configured"),
+			HaveKey("paas_currency_real_ratio"),
+			HaveKey("paas_currency_configured_ratio"),
 		))
 	})
 })
