f4fd82ae017842c2432b802bf5a0e59ec313fc7e
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2019-08-31 09:18:22 +0100

    WIP: remove shell scripts

--- scripts/upload_secrets.rb
+++ scripts/upload_secrets.rb
@@ -5,27 +5,37 @@ require 'json'
 require 'yaml'
 require 'tempfile'
 
-secrets = JSON.parse(STDIN.read)
+require_relative ARGV[0] # include the specified 'secret-getter'
+
+secrets = get_secrets # this method is provided by the secret-getter
 
 config = secrets['config']
 
+s3_secrets = []
 credhub_secrets = Hash['credentials', []]
+
+# create objects containing the secrets for s3 and credhub.
+# although we could parse the 'raw' object for s3, a new object is
+# created so we can `chomp` each secret to remove `\n`
 secrets['secrets'].each do |secret, value|
+  s3_secrets.push(Hash[secret, value.chomp])
   name = "#{config['credhub_namespace']}/#{secret}"
   credhub_secrets['credentials'].push(
-    Hash['name', name, 'type', 'value', 'value', value]
+    Hash['name', name, 'type', 'value', 'value', value.chomp]
   )
 end
 
 s3_secrets_file = Tempfile.new('s3-secrets')
 begin
-  s3_secrets_file.write({ 'secrets' => secrets['secrets'] }.to_yaml)
+  s3_secrets_file.write({ 'secrets' => s3_secrets }.to_yaml)
   s3_secrets_file.close
   system('aws', 's3', 'cp', s3_secrets_file.path, config['s3_path'])
 ensure
   s3_secrets_file.unlink
 end
 
+# create tempfile for credhub in /tmp because docker for mac can't mount
+# from the standard mac temp dir by default
 credhub_secrets_file = Tempfile.new('credhub-secrets', '/tmp')
 begin
   credhub_secrets_file.write(credhub_secrets.to_yaml)
