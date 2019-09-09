a7530a7ad626a6c61e211ed07296e4bb758474f2
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-09-09 11:54:02 +0100

    Remove pagerduty S3 secrets store from pipeline
    These are written to credhub now.

--- scripts/secrets/pagerduty.rb
+++ scripts/secrets/pagerduty.rb
@@ -8,7 +8,7 @@ ENV['PASSWORD_STORE_DIR'] = ENV['PAGERDUTY_PASSWORD_STORE_DIR']
 begin
   credhub_namespace = ENV['CREDHUB_NAMESPACE'].split(',')
 rescue NoMethodError # env var was not set
-  credhub_namespace = ['/concourse/main/create-cloudfoundry', "/#{ENV['DEPLOY_ENV']}/#{ENV['DEPLOY_ENV']}"]
+  credhub_namespace = ['/concourse/main/create-cloudfoundry', "/#{ENV['DEPLOY_ENV']}/#{ENV['DEPLOY_ENV']}", "/#{ENV['DEPLOY_ENV']}/prometheus"]
 end
 
 alertmanager_pagerduty_service_key = ENV['ALERTMANAGER_PAGERDUTY_SERVICE_KEY'] || `pass "pagerduty/${MAKEFILE_ENV_TARGET}/alertmanager_pagerduty_service_key"`
