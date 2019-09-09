8a34bf61c5f398c41ae84e04d470a564f358d2f3
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2019-09-09 11:20:07 +0100

    Correct TLS version acceptance tests for ALBs
    When connecting to a classic load balancer with an unsupported TLS version,
    Go's TLS library can report that the version is unsupported. However, when
    connecting to an ALB with an unsupported TLS version, the ALB simply closes the
    connection.
    
    This commit allows for that difference in the custom acceptance tests. Checking
    for a connection reset message isn't a perfect test, because it can show up as
    a false positive, but if the ALB is just closing the connection then this is
    the best we can do.

--- platform-tests/src/platform/acceptance/tls_version_test.go
+++ platform-tests/src/platform/acceptance/tls_version_test.go
@@ -2,32 +2,77 @@ package acceptance_test
 
 import (
 	"crypto/tls"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
+const (
+	LB_TYPE_CLASSIC = "classic"
+	LB_TYPE_ALB     = "ALB"
+)
+
+type tlsTestConfig struct {
+	HostPortGetter   func() string
+	LoadBalancerType string
+}
+
+func AssertTLSErrorOnClassicLB(err error) {
+	Expect(err).To(HaveOccurred())
+	Expect(err).To(SatisfyAny(
+		MatchError("tls: no supported versions satisfy MinVersion and MaxVersion"),
+		MatchError("EOF"),
+	))
+}
+
+func AssertTLSErrorOnALB(err error) {
+	Expect(err).To(HaveOccurred())
+	Expect(err.Error()).To(SatisfyAny(
+		// An ALB will close the connection when trying to
+		// connect with an unsupported version of TLS,
+		ContainSubstring("read: connection reset by peer"),
+		// but will return an error, which crypto/tls can
+		// translate in to the below, when connecting with SSL.
+		ContainSubstring("tls: no supported versions satisfy MinVersion and MaxVersion"),
+	))
+}
+
 var _ = Describe("Checking the TLS version", func() {
-	testHostPortGetters := map[string]func() string{
-		"apps_apex": func() string {
-			return testConfig.GetAppsDomain() + ":443"
+	testHostPortGetters := map[string]tlsTestConfig{
+		"apps_apex": {
+			func() string {
+				return testConfig.GetAppsDomain() + ":443"
+			},
+			LB_TYPE_CLASSIC,
 		},
-		"apps": func() string {
-			return "healthcheck." + testConfig.GetAppsDomain() + ":443"
+		"apps": {
+			func() string {
+				return "healthcheck." + testConfig.GetAppsDomain() + ":443"
+			},
+			LB_TYPE_CLASSIC,
 		},
-		"system": func() string {
-			return testConfig.GetApiEndpoint() + ":443"
+		"system": {
+			func() string {
+				return testConfig.GetApiEndpoint() + ":443"
+			},
+			LB_TYPE_CLASSIC,
 		},
-		"doppler": func() string {
-			return "doppler." + GetConfigFromEnvironment("SYSTEM_DNS_ZONE_NAME") + ":443"
+		"doppler": {
+			func() string {
+				return "doppler." + GetConfigFromEnvironment("SYSTEM_DNS_ZONE_NAME") + ":443"
+			},
+			LB_TYPE_ALB,
 		},
-		"concourse": func() string {
-			return "deployer." + GetConfigFromEnvironment("SYSTEM_DNS_ZONE_NAME") + ":443"
+		"concourse": {
+			func() string {
+				return "deployer." + GetConfigFromEnvironment("SYSTEM_DNS_ZONE_NAME") + ":443"
+			},
+			LB_TYPE_CLASSIC,
 		},
 	}
 
-	for domainClass, hostPortGetter := range testHostPortGetters {
-		domainClass, hostPortGetter := domainClass, hostPortGetter
+	for domainClass, testConfig := range testHostPortGetters {
+		hostPortGetter := testConfig.HostPortGetter
+		loadBalancerType := testConfig.LoadBalancerType
 
 		Context(domainClass+" domain", func() {
 			It("should not allow SSL 3.0", func() {
@@ -40,11 +85,17 @@ var _ = Describe("Checking the TLS version", func() {
 				if err == nil {
 					defer conn.Close()
 				}
-				Expect(err).To(HaveOccurred())
-				Expect(err).To(SatisfyAny(
-					MatchError("tls: no supported versions satisfy MinVersion and MaxVersion"),
-					MatchError("EOF"),
-				))
+
+				switch loadBalancerType {
+				case LB_TYPE_CLASSIC:
+				default:
+					AssertTLSErrorOnClassicLB(err)
+					break
+
+				case LB_TYPE_ALB:
+					AssertTLSErrorOnALB(err)
+					break
+				}
 			})
 			It("should not allow TLS 1.0", func() {
 				tlsConfig := &tls.Config{
@@ -56,11 +107,17 @@ var _ = Describe("Checking the TLS version", func() {
 				if err == nil {
 					defer conn.Close()
 				}
-				Expect(err).To(HaveOccurred())
-				Expect(err).To(SatisfyAny(
-					MatchError("tls: no supported versions satisfy MinVersion and MaxVersion"),
-					MatchError("EOF"),
-				))
+
+				switch loadBalancerType {
+				case LB_TYPE_CLASSIC:
+				default:
+					AssertTLSErrorOnClassicLB(err)
+					break
+
+				case LB_TYPE_ALB:
+					AssertTLSErrorOnALB(err)
+					break
+				}
 			})
 			It("should not allow TLS 1.1", func() {
 				tlsConfig := &tls.Config{
@@ -72,11 +129,17 @@ var _ = Describe("Checking the TLS version", func() {
 				if err == nil {
 					defer conn.Close()
 				}
-				Expect(err).To(HaveOccurred())
-				Expect(err).To(SatisfyAny(
-					MatchError("tls: no supported versions satisfy MinVersion and MaxVersion"),
-					MatchError("EOF"),
-				))
+
+				switch loadBalancerType {
+				case LB_TYPE_CLASSIC:
+				default:
+					AssertTLSErrorOnClassicLB(err)
+					break
+
+				case LB_TYPE_ALB:
+					AssertTLSErrorOnALB(err)
+					break
+				}
 			})
 
 			It("should allow TLS 1.2", func() {
