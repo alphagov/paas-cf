1f11d1e42294b8f5341f6c8ad5561a66ce224ea6
Michael Mokrysz <hi@46b.it>
2019-09-11 11:50:25 +0100

    Add in-hours-only PagerDuty configuration
    This commit reconfigures Alertmanager so that it is possible for alerts to only page us in-hours.
    The justification is that we only want to be paged about problems with billing in-hours, and until
    now we haven't had a simple way to achieve that.
    
    The existing PagerDuty secret `alertmanager_pagerduty_service_key` is removed and replaced by two
    new secrets: `alertmanager_pagerduty_24_7_service_key` and
    `alertmanager_pagerduty_in_hours_service_key`. In staging these will both be set to the in-hours
    PagerDuty service, as we don't want to be woken up about problems in staging.
    
    This commit is already large; altering the existing alerts to use the new configuration will be done
    in a followup commit.

--- manifests/prometheus/spec/operations/alertmanager_routes_spec.rb
+++ manifests/prometheus/spec/operations/alertmanager_routes_spec.rb
@@ -1,6 +1,11 @@
 RSpec.describe "alertmanager" do
-  it "should have a pagerduty receiver with a correct service key" do
-    pagerduty_configs = manifest_with_defaults.get("instance_groups.alertmanager.jobs.alertmanager.properties.alertmanager.receivers.pagerduty-receiver.pagerduty_configs")
-    expect(pagerduty_configs[0]['service_key']).to eq("test_alertmanager_pagerduty_service_key")
+  it "should have a 24/7 pagerduty receiver with a correct service key" do
+    pagerduty_24_7_configs = manifest_with_defaults.get("instance_groups.alertmanager.jobs.alertmanager.properties.alertmanager.receivers.pagerduty-24-7-receiver.pagerduty_configs")
+    expect(pagerduty_24_7_configs[0]['service_key']).to eq("test_alertmanager_pagerduty_24_7_service_key")
+  end
+
+  it "should have an in-hours pagerduty receiver with a correct service key" do
+    pagerduty_in_hours_configs = manifest_with_defaults.get("instance_groups.alertmanager.jobs.alertmanager.properties.alertmanager.receivers.pagerduty-in-hours-receiver.pagerduty_configs")
+    expect(pagerduty_in_hours_configs[0]['service_key']).to eq("test_alertmanager_pagerduty_in_hours_service_key")
   end
 end
