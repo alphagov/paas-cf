7a9d03d61043e462f016ee3442616ef40aedd852
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-09-11 09:04:19 +0100

    cve-2019-11279 bump uaa to v74.1.0
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- manifests/cf-manifest/spec/manifest/release_versions_spec.rb
+++ manifests/cf-manifest/spec/manifest/release_versions_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe "release versions" do
 
     pinned_releases = {
       'uaa' => {
-        local: '0.1.11',
+        local: '0.1.12',
         upstream: '74.0.0',
       },
       'loggregator' => {
