72d0ddb7eb4dae479d912d2593ba16c034e45852
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2019-09-16 15:54:23 +0100

    Implement race condition acceptance test for S3 broker
    The S3 broker operates against a single policy document per each bucket, and
    manages the users and policy statements associated with it. When two mutations
    to the state of the service (i.e. bindings and unbindings) happened in quick
    succession, it was observed that there was a race condition that left the
    service in a state that tenants could not recover from.
    
    Now that the broker obtains and releases locks on the bucket, that particular
    race condition should not happen. This acceptance test tests for that.

--- platform-tests/src/platform/broker-acceptance/s3_broker_test.go
+++ platform-tests/src/platform/broker-acceptance/s3_broker_test.go
@@ -85,4 +85,113 @@ var _ = Describe("S3 broker", func() {
 
 		})
 	})
+
+	Context("multiple operations against a single bucket", func(){
+		var (
+			appOneName          string
+			appTwoName          string
+			serviceInstanceName string
+		)
+
+		It("do not run in to race conditions", func(){
+			appOneName = generator.PrefixedRandomName(testConfig.GetNamePrefix(), "APP")
+			appTwoName = generator.PrefixedRandomName(testConfig.GetNamePrefix(), "APP")
+			serviceInstanceName = generator.PrefixedRandomName(testConfig.GetNamePrefix(), "test-s3-bucket")
+
+			By("creating the service: " + serviceInstanceName, func() {
+				Expect(
+					cf.
+						Cf("create-service", serviceName, testPlanName, serviceInstanceName).
+						Wait(testConfig.DefaultTimeoutDuration()),
+				).
+				To(Exit(0))
+				pollForServiceCreationCompletion(serviceInstanceName)
+			})
+
+			defer By("deleting the service", func() {
+				Expect(
+					cf.Cf("delete-service", serviceInstanceName, "-f").
+						Wait(testConfig.DefaultTimeoutDuration()),
+				).To(Exit(0))
+				pollForServiceDeletionCompletion(serviceInstanceName)
+			})
+
+			By("deploying a first app", func() {
+				Expect(cf.Cf(
+					"push", appOneName,
+					"--no-start",
+					"-b", testConfig.GetGoBuildpackName(),
+					"-p", "../../../example-apps/healthcheck",
+					"-f", "../../../example-apps/healthcheck/manifest.yml",
+					"-d", testConfig.GetAppsDomain(),
+				).Wait(testConfig.CfPushTimeoutDuration())).To(Exit(0))
+			})
+			defer By("deleting the first app", func() {
+				cf.Cf("delete", appOneName, "-f").Wait(testConfig.DefaultTimeoutDuration())
+			})
+
+			By("deploying a second app", func() {
+				Expect(cf.Cf(
+					"push", appOneName,
+					"--no-start",
+					"-b", testConfig.GetGoBuildpackName(),
+					"-p", "../../../example-apps/healthcheck",
+					"-f", "../../../example-apps/healthcheck/manifest.yml",
+					"-d", testConfig.GetAppsDomain(),
+				).Wait(testConfig.CfPushTimeoutDuration())).To(Exit(0))
+			})
+			defer By("deleting the second app", func() {
+				cf.Cf("delete", appTwoName, "-f").Wait(testConfig.DefaultTimeoutDuration())
+			})
+
+			By("binding and unbinding the two apps simultaneously, we should see no errors", func(){
+				// Prepare four channels
+				bindAppOneChan := make(chan int)
+				bindAppTwoChan := make(chan int)
+				unbindAppOneChan := make(chan int)
+				unbindAppTwoChan := make(chan int)
+
+				// Concurrently (as possible) bind and unbind the services
+				bindServiceToAppAsync(appOneName, serviceInstanceName, bindAppOneChan)
+				bindServiceToAppAsync(appTwoName, serviceInstanceName, bindAppTwoChan)
+				unbindServiceFromAppAsync(appOneName, serviceInstanceName, unbindAppOneChan)
+				unbindServiceFromAppAsync(appTwoName, serviceInstanceName, unbindAppTwoChan)
+
+				// Check that every channel exited 0 (ie didn't error)
+				Eventually(func() int {
+					return <-bindAppOneChan
+				}).Should(Equal(0))
+
+				Eventually(func() int {
+					return <-bindAppTwoChan
+				}).Should(Equal(0))
+
+				Eventually(func() int {
+					return <-unbindAppOneChan
+				}).Should(Equal(0))
+
+				Eventually(func() int {
+					return <-unbindAppTwoChan
+				}).Should(Equal(0))
+			})
+		})
+	})
 })
+
+func bindServiceToAppAsync(appName string, serviceInstanceName string, outChan chan<- int) {
+	go (func() {
+		session := cf.Cf("bind-service", appName, serviceInstanceName).Wait(testConfig.DefaultTimeoutDuration())
+		exitCode := session.ExitCode()
+
+		outChan <- exitCode
+	})()
+}
+
+func unbindServiceFromAppAsync(appName string, serviceInstanceName string, outChan chan<- int) {
+	go (func() {
+		session := cf.Cf("unbind-service", appName, serviceInstanceName).Wait(testConfig.DefaultTimeoutDuration())
+		exitCode := session.ExitCode()
+
+		outChan <- exitCode
+	})()
+}
