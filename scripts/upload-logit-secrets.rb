80dc417e421f3fa0038ab6832ccbbe4800281cd6
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2019-09-18 11:47:30 +0100

    Base all Logit secret paths on MAKEFILE_ENV_TARGET
    Most of the Logit secrets were already being fetched from paths based
    upon `MAKEFILE_ENV_TARGET`. However two were based upon `AWS_ACCOUNT`.
    In `paas-credentials` this meant that we needed both a `staging` and
    a `stg-lon` folder. It's cleaner to only have the one folder and be
    relatively consistent.

--- scripts/upload-logit-secrets.rb
+++ scripts/upload-logit-secrets.rb
@@ -13,8 +13,8 @@ credhub_namespaces = [
 logit_syslog_address = ENV['LOGIT_SYSLOG_ADDRESS'] || `pass "logit/${MAKEFILE_ENV_TARGET}/syslog_address"`
 logit_syslog_port = ENV['LOGIT_SYSLOG_PORT'] || `pass "logit/${MAKEFILE_ENV_TARGET}/syslog_port"`
 logit_ca_cert = ENV['LOGIT_CA_CERT'] || `pass "logit/${MAKEFILE_ENV_TARGET}/ca_cert"`
-logit_elasticsearch_url = ENV['LOGIT_ELASTICSEARCH_URL'] || `pass "logit/${AWS_ACCOUNT}/elasticsearch_url"`
-logit_elasticsearch_api_key = ENV['LOGIT_ELASTICSEARCH_API_KEY'] || `pass "logit/${AWS_ACCOUNT}/elasticsearch_api_key"`
+logit_elasticsearch_url = ENV['LOGIT_ELASTICSEARCH_URL'] || `pass "logit/${MAKEFILE_ENV_TARGET}/elasticsearch_url"`
+logit_elasticsearch_api_key = ENV['LOGIT_ELASTICSEARCH_API_KEY'] || `pass "logit/${MAKEFILE_ENV_TARGET}/elasticsearch_api_key"`
 
 upload_secrets(
   credhub_namespaces,
