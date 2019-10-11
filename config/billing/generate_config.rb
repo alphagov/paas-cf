b9bd989bff7c74b88e3594e021757d51b04214e6
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2019-10-11 12:34:12 +0100

    Re-generate billing config files
    Refactors the billing config template files to be more readable, by extracting
    each part in to its own file, and then re-combining them. This increases the
    number of files involved, and the complexity of generating the config, but
    should be more maintainable in the long run.

--- config/billing/generate_config.rb
+++ config/billing/generate_config.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'yaml'
+require 'json'
 require 'erb'
 
 REGIONS = %w(eu-west-1 eu-west-2).freeze
@@ -18,14 +19,39 @@ class Generator
   def generate(template)
     ERB.new(template).result(binding)
   end
+
+  def include(file)
+    fp = File.join(__dir__, file)
+    ERB.new(
+      File.read(File.expand_path(fp))
+    ).result(binding)
+  end
 end
 
 pricing_data = YAML.load_file("#{__dir__}/pricing_data.yml")
 template = File.read("#{__dir__}/config.json.erb")
 
 REGIONS.each do |region|
-  g = Generator.new(region, pricing_data)
-  File.open("#{__dir__}/output/#{region}.json", "w") do |f|
-    f.write g.generate(template)
+  begin
+    g = Generator.new(region, pricing_data)
+    json_content = g.generate(template)
+
+    JSON.parse json_content
+
+    File.open("#{__dir__}/output/#{region}.json", "w") do |f|
+      f.write json_content
+    end
+
+    if File.exist?("#{__dir__}/output/#{region}.err.json")
+      File.delete("#{__dir__}/output/#{region}.err.json")
+    end
+  rescue JSON::ParserError
+    puts "Config for #{region} is not valid JSON. Written content to #{__dir__}/output/#{region}.err.json"
+    File.open("#{__dir__}/output/#{region}.err.json", "w") do |f|
+      f.write json_content
+    end
+
+    puts `jq '.' #{__dir__}/output/#{region}.err.json`
+    exit 1
   end
 end
