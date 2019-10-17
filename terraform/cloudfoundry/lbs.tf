3a9dfe7a05f7079049856bb45521c50253588faa
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-10-17 08:24:24 +0100

    Re-add cf_router_app_domain_http target group
    We can only remove target groups after we remove them in bosh
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- terraform/cloudfoundry/lbs.tf
+++ terraform/cloudfoundry/lbs.tf
@@ -181,6 +181,29 @@ resource "aws_lb_target_group" "cf_router_app_domain_https" {
   }
 }
 
+resource "aws_lb_target_group" "cf_router_app_domain_http" {
+  name     = "${var.env}-app-tcp-tg"
+  port     = 83
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+
+  # This needs to be here whilst we deploy BOSH to deregister the instances
+
+  health_check {
+    port                = 8080
+    path                = "/health"
+    protocol            = "HTTP"
+    interval            = "${var.health_check_interval}"
+    timeout             = "${var.health_check_timeout}"
+    healthy_threshold   = "${var.health_check_healthy}"
+    unhealthy_threshold = "${var.health_check_unhealthy}"
+    matcher             = "200-299"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_lb" "cf_router_system_domain" {
   name               = "${var.env}-cf-rtr-sys"
   internal           = false
