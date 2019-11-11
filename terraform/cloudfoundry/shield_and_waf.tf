18979e6619b9621a55c6754b397fc566263b62be
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2019-11-11 16:04:05 +0000

    Apply WAF rules to app and system load balancers
    We apply an AWS WAF [1] request rate rule to the system and app domain load
    balancers for DDoS protection. We set the limit to allow 600,000 requests from
    the same IP in a 5 minute period, based on the maximum number of requests from
    any one IP address in our production environments in a 5 minute interval over
    the period 2019-10-28 to 2019-11-11, rounded up to provide some leeway.
    
    [1] https://docs.aws.amazon.com/waf/latest/APIReference/API_RateBasedRule.html

--- terraform/cloudfoundry/shield_and_waf.tf
+++ terraform/cloudfoundry/shield_and_waf.tf
@@ -1,9 +1,46 @@
 resource "aws_shield_protection" "shield_for_app_gorouter_alb" {
-  name = "${var.env}-app-gorouter-shield"
+  name         = "${var.env}-app-gorouter-shield"
   resource_arn = "${aws_lb.cf_router_app_domain.arn}"
 }
 
 resource "aws_shield_protection" "shield_for_system_gorouter_alb" {
-  name = "${var.env}-system-gorouter-shield"
+  name         = "${var.env}-system-gorouter-shield"
   resource_arn = "${aws_lb.cf_router_system_domain.arn}"
+}
+
+resource "aws_wafregional_rate_based_rule" "wafregional_max_request_rate_rule" {
+  name        = "${var.env}-waf-max-request-rate"
+  metric_name = "${replace(var.env, "-", "")}WafMaxRequestRate"
+
+  rate_key   = "IP"
+  rate_limit = "600000"
+}
+
+resource "aws_wafregional_web_acl" "wafregional_web_acl" {
+  name        = "${var.env}-waf-web-acl"
+  metric_name = "${replace(var.env, "-", "")}WafWebACL"
+
+  default_action {
+    type = "ALLOW"
+  }
+
+  rule {
+    action {
+      type = "BLOCK"
+    }
+
+    priority = 1
+    rule_id  = "${aws_wafregional_rate_based_rule.wafregional_max_request_rate_rule.id}"
+    type     = "RATE_BASED"
+  }
+}
+
+resource "aws_wafregional_web_acl_association" "wafregional_acl_app_alb_association" {
+  resource_arn = "${aws_lb.cf_router_app_domain.arn}"
+  web_acl_id   = "${aws_wafregional_web_acl.wafregional_web_acl.id}"
+}
+
+resource "aws_wafregional_web_acl_association" "wafregional_acl_system_alb_association" {
+  resource_arn = "${aws_lb.cf_router_system_domain.arn}"
+  web_acl_id   = "${aws_wafregional_web_acl.wafregional_web_acl.id}"
 }
