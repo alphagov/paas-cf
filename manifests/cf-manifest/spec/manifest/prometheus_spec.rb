95e7a9282b0c8ff6b095245a7637a9ae915847ad
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-11-28 18:24:52 +0000

    Add caddy to prometheus
    When prometheus is behind a round robin load balancer then the metric
    values change rapidly because each prometheus has different state
    
    We would like to always point at a known instance unless it is down in
    which case we can still serve metrics from the local instance
    
    We also need to do basic authentication so that not everyone can make
    arbitrary queries to prometheus
    
    Caddy solves both of these problems, we can enable basic auth for
    multiple users
    
    Caddy can try a known instance (found using bosh dns) and if not, fail
    back to localhost
    
    These Caddys run on the prometheus vm so there is no additional cost
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- manifests/cf-manifest/spec/manifest/prometheus_spec.rb
+++ manifests/cf-manifest/spec/manifest/prometheus_spec.rb
@@ -13,10 +13,19 @@ RSpec.describe 'prometheus' do
     manifest.fetch('instance_groups.prometheus.jobs.route_registrar.properties.route_registrar.routes')
   end
 
+  let(:caddy_config) do
+    manifest.fetch('instance_groups.prometheus.jobs.caddy.properties')
+  end
+
   context 'manifest' do
     it 'should have prometheus as a release' do
       release_names = releases.map { |r| r['name'] }
       expect(release_names).to include('prometheus')
+    end
+
+    it 'should have caddy as a release' do
+      release_names = releases.map { |r| r['name'] }
+      expect(release_names).to include('caddy')
     end
 
     it 'should have a prometheus instance group' do
@@ -65,11 +74,49 @@ RSpec.describe 'prometheus' do
       prometheus_route = route_registrar_routes.find { |r| r['name'] == 'prometheus' }
 
       expect(prometheus_route).not_to be_nil
-      expect(prometheus_route['port']).to eq(9090)
-      expect(prometheus_route['prepend_instance_index']).to eq(true)
+      expect(prometheus_route['port']).to eq(8080)
+      expect(prometheus_route['prepend_instance_index']).to eq(false)
       expect(prometheus_route['uris']).to eq(
         ["prometheus.#{terraform_fixture_value(:cf_root_domain)}"],
       )
+    end
+  end
+
+  context 'caddy job' do
+    it 'should listen on port 8080' do
+      http_port = caddy_config['http_port']
+      expect(http_port).to eq(8080)
+    end
+
+    context 'caddyfile' do
+      let(:caddyfile) {caddy_config['caddyfile']}
+
+      it 'should listen on all interfaces' do
+        # Caddy cannot just listen on localhost,
+        # otherwise it will not proxy traffic from gorouter
+
+        vhosts = caddyfile.gsub(/{.*}/m, '').lines.map(&:strip)
+        expect(vhosts).to eq(['http://:8080'])
+      end
+
+      it 'should be configured without TLS' do
+        # Caddy tries to infer what it should get a self-signed certificate
+        # We want to disable this feature explicitly
+
+        expect(caddyfile.lines.grep(/^\s+tls off/).first&.strip).to eq('tls off')
+      end
+
+      it 'should be configured for HA' do
+        proxy_config = caddyfile
+          .lines.grep(/^\s+proxy/).first
+          .strip.sub(/^proxy/, '').sub(' {', '')
+
+        proxy_listen, *proxy_hosts = proxy_config.split(' ')
+
+        expect(proxy_listen).to eq('/')
+        expect(proxy_hosts.first).to eq('http://q-s3-i0.prometheus.*.unit-test.bosh:9090')
+        expect(proxy_hosts.last).to eq('http://localhost:9090')
+      end
     end
   end
 end
