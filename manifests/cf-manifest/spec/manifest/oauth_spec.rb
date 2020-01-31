d1f8472f6cb53c81ec92f8a682646b9b8ac5900e
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-01-31 10:23:45 +0000

    Add some tests for cf-manifest oauth
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- manifests/cf-manifest/spec/manifest/oauth_spec.rb
+++ manifests/cf-manifest/spec/manifest/oauth_spec.rb
@@ -1,11 +1,32 @@
-RSpec.describe "Google OAuth" do
+RSpec.describe "OAuth" do
   let(:properties) { manifest.fetch("instance_groups.uaa.jobs.uaa.properties") }
 
   describe "by default" do
     let(:manifest) { manifest_with_defaults }
+
     it "enables the Google OAuth provider in UAA" do
       providers = properties.fetch("login").fetch("oauth").fetch("providers")
       expect(providers).to have_key 'google'
+    end
+
+    it "enables the Microsoft OAuth provider in UAA" do
+      providers = properties.fetch("login").fetch("oauth").fetch("providers")
+      expect(providers).to have_key 'microsoft'
+    end
+
+    it "enables a Google OAuth provider for administators in UAA" do
+      providers = properties.fetch("login").fetch("oauth").fetch("providers")
+      expect(providers).to have_key 'admin-google'
+    end
+
+    it "ensures unique username attr mappings to ensure unique usernames" do
+      providers = properties.fetch("login").fetch("oauth").fetch("providers")
+
+      mappings = providers
+        .values
+        .map { |p| p.dig('attributeMappings', 'user_name') }
+
+      expect(mappings).to eq(mappings.uniq)
     end
   end
 end
