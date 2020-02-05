1bc6c515f7e3d928b68161c63870344fc6fdf12e
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2020-02-05 14:56:18 +0000

    Remove unused ElasticacheClusterIdHashingFunction type

--- tools/metrics/pkg/elasticache/types.go
+++ tools/metrics/pkg/elasticache/types.go
@@ -4,8 +4,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/elasticache/elasticacheiface"
 )
 
-type ElasticacheClusterIdHashingFunction = func(value string) string
-
 type ElasticacheService struct {
 	Client elasticacheiface.ElastiCacheAPI
 }
