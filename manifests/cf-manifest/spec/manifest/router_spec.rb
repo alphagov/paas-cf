8d4a7406b3582c54fd43158de892d4ae3f7f2dc5
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-02-13 17:41:10 +0000

    Disable keepalives again
    This was causing base unreliability for Notify
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- manifests/cf-manifest/spec/manifest/router_spec.rb
+++ manifests/cf-manifest/spec/manifest/router_spec.rb
@@ -3,8 +3,8 @@ RSpec.describe 'router' do
     let(:manifest) { manifest_with_defaults }
     let(:gorouter_props) { manifest.fetch('instance_groups.router.jobs.gorouter.properties.router') }
 
-    it 'has max_idle_connections set' do
-      expect(gorouter_props.dig('max_idle_connections')).to eq(2500)
+    it 'has max_idle_connections set to disable keepalives' do
+      expect(gorouter_props.dig('max_idle_connections')).to eq(0)
     end
   end
 end
