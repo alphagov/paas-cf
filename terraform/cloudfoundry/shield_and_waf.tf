3b8c828f661e9ef6a9fca00cb1b6254104a5205e
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2020-05-12 13:51:23 +0100

    Add lifecycle to Shield resources
    We didn't have a lifecycle on shield resources with would cause a failed
    delete run to not be able to complete the teraform destroy.

--- terraform/cloudfoundry/shield_and_waf.tf
+++ terraform/cloudfoundry/shield_and_waf.tf
@@ -1,11 +1,19 @@
 resource "aws_shield_protection" "shield_for_app_gorouter_alb" {
   name         = "${var.env}-app-gorouter-shield"
   resource_arn = "${aws_lb.cf_router_app_domain.arn}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_shield_protection" "shield_for_system_gorouter_alb" {
   name         = "${var.env}-system-gorouter-shield"
   resource_arn = "${aws_lb.cf_router_system_domain.arn}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_wafregional_rate_based_rule" "wafregional_max_request_rate_rule" {
