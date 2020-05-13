4ce806e86eda7e0362c5274b33897f6365a6affa
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-05-13 15:13:24 +0100

    Extract concourse script formatting helpers to lib
    So we can re-use
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- concourse/scripts/check-certificates.rb
+++ concourse/scripts/check-certificates.rb
@@ -4,27 +4,7 @@ require 'openssl'
 require 'json'
 require 'date'
 
-def separator
-  puts('-' * 80)
-end
-
-class String
-  def red
-    "\e[31m#{self}\e[0m"
-  end
-
-  def green
-    "\e[32m#{self}\e[0m"
-  end
-
-  def yellow
-    "\e[33m#{self}\e[0m"
-  end
-
-  def blue;
-    "\e[34m#{self}\e[0m"
-  end
-end
+require_relative './lib/formatting'
 
 class CredHubClient
   attr_reader :api_url
