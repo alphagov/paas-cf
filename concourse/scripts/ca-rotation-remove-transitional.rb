19c9d02cd78cd34cb0297bde91b0d829db0b5cc1
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-05-14 14:33:21 +0100

    reformat output of ca-rotation-remote-transitional
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- concourse/scripts/ca-rotation-remove-transitional.rb
+++ concourse/scripts/ca-rotation-remove-transitional.rb
@@ -10,31 +10,50 @@ credhub_server = ENV['CREDHUB_SERVER'] || raise("Must set $CREDHUB_SERVER env va
 
 api_url = "#{credhub_server}/v1"
 
-puts "Getting certificates"
 client = CredHubClient.new(api_url)
-certs = client.certificates.reject { |c| c['name'].match?(/_old$/) }
 
-puts "Finding CA certs"
-ca_certs = certs.select { |c| c['name'] == c['signed_by'] }
+ca_certs = client
+  .certificates
+  .reject { |c| c['name'].match?(/_old$/) }
+  .select { |c| c['name'] == c['signed_by'] }
 
-ca_certs.each { |cert|
-  puts "Getting active ca certs for #{cert['name']}"
-  versions = client.current_certificates(cert['name'])
+updated_certificate_names = []
 
-  if versions.length > 1
-    puts "#{cert['name']} has more than one active version"
+puts "Checking CA certs"
+ca_certs.each do |cert|
+  cert_name = cert['name']
+  puts "Getting active ca certs for #{cert_name}"
+  versions = client.current_certificates(cert_name)
 
-    sorted_cas = versions
-      .sort_by { |version| Date.parse(version['expiry_date']) }
-      .reverse
-
-    new_ca, old_ca, *_other_cas = sorted_cas
-
-    if old_ca['transitional'] && !new_ca['transitional']
-      puts "Version #{old_ca['id']} has an expiry date of #{old_ca['expiry_date']} and the transitional flag is set to #{old_ca['transitional']}"
-      puts "Version #{new_ca['id']} has an expiry date of #{new_ca['expiry_date']} and the transitional flag is set to #{new_ca['transitional']}"
-      puts "Setting transitional flag for #{cert['name']} to null"
-      `credhub curl -p '#{api_url}/certificates/#{cert['id']}/update_transitional_version' -d '{\"version\": null}' -X PUT`
-    end
+  if versions.length <= 1
+    puts "#{cert_name.yellow} does not have multiple versions...#{'skipped'.green}"
+    next
   end
-}
+
+  sorted_cas = versions
+    .sort_by { |version| Date.parse(version['expiry_date']) }
+    .reverse
+
+  new_ca, old_ca, *_other_cas = sorted_cas
+
+  unless old_ca['transitional'] && !new_ca['transitional']
+    puts "#{cert_name.yellow} does not need transitioning...#{'skipped'.green}"
+    next
+  end
+
+  puts "Version #{old_ca['id']} has an expiry date of #{old_ca['expiry_date']} and the transitional flag is set to #{old_ca['transitional']}"
+  puts "Version #{new_ca['id']} has an expiry date of #{new_ca['expiry_date']} and the transitional flag is set to #{new_ca['transitional']}"
+  puts "#{cert_name.yellow} should not be transitional...#{'updated'.yellow}"
+  `credhub curl -p '#{api_url}/certificates/#{cert['id']}/update_transitional_version' -d '{\"version\": null}' -X PUT`
+  updated_certificate_names << cert_name
+end
+
+unless updated_certificate_names.empty?
+  separator
+
+  puts 'The following certificates have been updated as non-transitional:'
+
+  updated_certificate_names.sort.each do |cert|
+    puts cert.yellow
+  end
+end
