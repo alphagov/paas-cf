de69a408b3a920d36a8ea9ff13220cd82df8316f
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-05-28 18:25:35 +0100

    aws_redis_update_manager finds instances to update
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

---
+++ tools/aws_redis_update_manager/lib/elasticache_update_finder.rb
@@ -0,0 +1,32 @@
+class ElastiCacheUpdateFinder
+  def initialize(elasticache_client)
+    @elasticache_client = elasticache_client
+  end
+
+  def find_replication_groups_to_update
+    replication_group_ids = {}
+
+    marker = nil
+    loop do
+      response = @elasticache_client.describe_update_actions(
+        service_update_status: ['available'],
+        marker: marker,
+      )
+
+      marker = response.marker
+
+      response
+        .update_actions
+        .select { |update| update.update_action_status == 'not-applied' }
+        .reject { |update| update.replication_group_id.nil? }
+        .each do |update|
+          replication_group_ids[update.service_update_name] ||= []
+          replication_group_ids[update.service_update_name] << update.replication_group_id
+        end
+
+      break if marker.nil?
+    end
+
+    replication_group_ids
+  end
+end
