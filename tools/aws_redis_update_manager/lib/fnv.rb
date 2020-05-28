d8a25a10de43a1ed5e067a99e2d943c35502df08
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-05-28 18:22:38 +0100

    Add fnv hash helper for aws_redis_update_manager
    We use the fnv hash function for going from cloud foundry elasticsearch
    service instance guids to elasticache replication group identifiers
    because AWS has a character limit
    
    I couldnt find a good implementation gem that was easily base32
    encodeable so I just copied the tests from paas-admin and used
    
    https://github.com/pawandubey/fnv-hash/blob/master/lib/fnv.rb
    as reference
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

---
+++ tools/aws_redis_update_manager/lib/fnv.rb
@@ -0,0 +1,38 @@
+def fnv_base32_encode(inp)
+  alphabet = 'abcdefghijklmnopqrstuvwxyz234567'
+
+  remainder = inp.bytesize.modulo 5
+  padding = "\0" * (5 - remainder)
+  inp += padding
+
+  inp.gsub!(/.{5}/mn) do |s|
+    n32, i8 = s.unpack('NC')
+    n32 = (n32 << 8) | i8
+
+    8.times.reduce '' do |accumulator|
+      n32, i8 = n32.divmod 32
+      chr = alphabet[i8]
+      "#{chr}#{accumulator}"
+    end
+  end
+
+  first_12_chars = inp[0..12]
+  first_12_chars
+end
+
+def fnv(input)
+  offset = 0xcbf29ce484222325
+  prime = 1_099_511_628_211
+  mask = 18_446_744_073_709_551_615
+
+  hash = offset
+
+  input.each_byte do |b|
+    hash ^= b
+    hash *= prime
+    hash &= mask
+  end
+
+  packed = [hash.to_s(16)].pack('H*')
+  fnv_base32_encode(packed)
+end
