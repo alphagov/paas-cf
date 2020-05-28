1f8194fdef91af8160fcf3c2ddee98e9569169f1
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-05-28 18:24:41 +0100

    Add aws_redis_update_manager paas accounts client
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

---
+++ tools/aws_redis_update_manager/lib/paas_accounts_api_client.rb
@@ -0,0 +1,46 @@
+require 'http'
+require 'json'
+
+class PaaSAccountsAPIClient
+  User = Struct.new(:guid, :username, :email)
+
+  class UnhandledResponseError < RuntimeError
+    def initialize(resp)
+      @resp = resp
+      super(message)
+    end
+
+    def message
+      "PaaS accounts unhandled response (code: #{@resp.code}) (req: #{req_id})"
+    end
+
+    def req_id
+      @resp.headers['X-Vcap-Request-Id'] || 'no-request-id'
+    end
+  end
+
+  def initialize(url:, username: nil, password: nil)
+    @url = url
+    @username = username
+    @password = password
+  end
+
+  def http_client
+    return HTTP if @username.nil? || @password.nil?
+    HTTP.basic_auth(user: @username, pass: @password)
+  end
+
+  def find_user(guid)
+    response = http_client.get("#{@url}/users/#{guid}")
+
+    return nil if response.code == 404
+
+    if response.code == 200
+      p = JSON.parse(response.to_s)
+      return User
+        .new(guid, p['username'], p['user_email'])
+    end
+
+    raise UnhandledResponseError.new(response)
+  end
+end
---
+++ tools/aws_redis_update_manager/spec/paas_accounts_api_client_spec.rb
@@ -0,0 +1,82 @@
+require 'base64'
+require 'json'
+
+RSpec.describe PaaSAccountsAPIClient do
+  before(:all) do
+    WebMock.enable!
+  end
+
+  after(:all) do
+    WebMock.disable!
+  end
+
+  let(:accounts_url) { 'https://accounts.paas' }
+
+  context 'when a username and password is provided to the constructor' do
+    it 'sets the HTTP Authorization header correctly' do
+      stub_request(:get, "#{accounts_url}/users/a-guid")
+        .to_return(status: 404)
+
+      client = PaaSAccountsAPIClient.new(
+        url: accounts_url,
+        username: 'un', password: 'pw'
+      )
+      user = client.find_user('a-guid')
+
+      expect(user).to be_nil
+
+      expected_auth_header = "Basic #{Base64.strict_encode64('un:pw').strip}"
+
+      expect(WebMock).to have_requested(:get, "#{accounts_url}/users/a-guid")
+        .with(headers: { 'Authorization': expected_auth_header })
+    end
+  end
+
+  context 'when a username and password is not provided to the constructor' do
+    it 'does not set the HTTP Authorization header' do
+      stub_request(:get, "#{accounts_url}/users/a-guid")
+        .to_return(status: 404)
+
+      client = PaaSAccountsAPIClient.new(url: accounts_url)
+      user = client.find_user('a-guid')
+
+      expect(user).to be_nil
+
+      expect(WebMock).to have_requested(:get, "#{accounts_url}/users/a-guid")
+    end
+  end
+
+  context 'when a user is not found' do
+    it 'returns nil' do
+      stub_request(:get, "#{accounts_url}/users/a-guid")
+        .to_return(status: 404)
+
+      client = PaaSAccountsAPIClient.new(url: accounts_url)
+      user = client.find_user('a-guid')
+
+      expect(user).to be_nil
+    end
+  end
+
+  context 'when a user is found' do
+    it 'returns a user struct' do
+      stub_request(:get, "#{accounts_url}/users/a-guid")
+        .to_return(
+          status: 200,
+          body: {
+            username: 'a-username',
+            user_email: 'email@domain.tld'
+          }.to_json
+        )
+
+      client = PaaSAccountsAPIClient.new(url: accounts_url)
+      user = client.find_user('a-guid')
+
+      expect(user).to have_attributes(
+        guid: 'a-guid',
+        username: 'a-username',
+        email: 'email@domain.tld'
+      )
+    end
+  end
+end
