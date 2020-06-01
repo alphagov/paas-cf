187a82a02c413ddc0c2c61d052f876c0ad76ea67
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-06-01 14:47:49 +0100

    Add tenant notifier for redis update manager
    so we can email tenants
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

---
+++ tools/aws_redis_update_manager/lib/tenant_notifier.rb
@@ -0,0 +1,116 @@
+require 'erb'
+
+require 'notifications/client'
+
+class TenantNotifier
+  def initialize(notify_api_key)
+    @notify_api_key = notify_api_key
+  end
+
+  def client
+    @client ||= Notifications::Client.new(@notify_api_key)
+  end
+
+  AWS_REDIS_UPGRADE_TEMPLATE_ID = '9d0f232b-187c-49b1-8cd1-91e83e347f68'
+  PAAS_SUPPORT_REPLY_TO_ID = '76d63d5f-e140-6a37-92ae-fc0d0d136f6f'
+
+  TEMPLATE = <<~MESSAGE
+  Dear GOV.UK PaaS tenant,
+
+  We are contacting you, as the managers of the <%= org_name %> org, to ask you to review your choice of Redis plans ahead of an upgrade.
+
+  This will help you avoid unnecessary downtime for your users.
+
+  We will be upgrading these Redis instances in two weeks time, on <%= maintenance_window_date %>, between <%= maintenance_window_time_range %>:
+
+  ( Org / Space / Service )
+  <% service_instances.each do |si| %>
+  - <%= si.org_name %> / <%= si.space_name %> / <%= si.instance_name %>
+  <% end %>
+
+  # What this means for you
+
+  If you are using any of our “highly available” Redis plans, your users will only experience a small amount of downtime when the upgrade happens: up to a minute.
+
+  However, if you are using any “tiny” Redis plans that are not “highly available”, your users may experience up to 30 minutes of downtime.
+
+  If you want to minimise this downtime, we recommend that you upgrade these tiny instances to “highly available” plans, ahead of the upgrade on <%= maintenance_window_date %>.
+
+  Note that this will double your monthly charge for each Redis instance: from roughly £25 to £50 per month.
+
+  # How to review your Redis plans
+
+  - Log into your PaaS account: https://www.cloud.service.gov.uk/sign-in
+  - Click on your organisation
+  - Click “Explore your costs and usage”
+  - Click the dropdown “Services and apps”
+  - Look for any “tiny” Redis plans without “ha” in the name, e.g. “redis tiny-4.x”
+  - Repeat for any other organisations you manage
+
+  # How to upgrade your Redis tiny plans to highly available plans
+
+  - Follow these instructions for every tiny Redis instance you want to upgrade: https://docs.cloud.service.gov.uk/deploying_services/redis/#upgrade-redis-service-plan
+  - Upgrade to the “ha” version of your plan, e.g. from “redis tiny-4.x” to “redis tiny-ha-4.x”
+
+  # If the timing of the upgrade doesn’t work for you
+
+  We can offer you the alternative time slot of <%= alt_maintenance_window_date %> between <%= alt_maintenance_window_time_range %>.
+
+  Please reply to this email to request this alternative slot.
+
+  If you have any further questions, please contact GOV.UK PaaS support on gov-uk-paas-support@digital.cabinet-office.gov.uk.
+
+  Please note that if we do not hear from you, we will upgrade your Redis instances on <%= maintenance_window_date %> between <%= maintenance_window_time_range %>, to keep your services secure.
+
+  Kind regards,
+  The GOV.UK PaaS Team
+  MESSAGE
+
+  def generate_email_contents(
+    org_name:,
+    service_instances:,
+    maintenance_window_date:, maintenance_window_time_range:,
+    alt_maintenance_window_date:, alt_maintenance_window_time_range:
+  )
+    ERB.new(TEMPLATE).result_with_hash(
+      org_name: org_name,
+      service_instances: service_instances,
+      maintenance_window_date: maintenance_window_date,
+      maintenance_window_time_range: maintenance_window_time_range,
+      alt_maintenance_window_date: alt_maintenance_window_date,
+      alt_maintenance_window_time_range: alt_maintenance_window_time_range
+    )
+  end
+
+  def notify_tenant(
+    tenant_email_address:,
+    org_name:,
+    service_instances:,
+    maintenance_window_date:, maintenance_window_time_range:,
+    alt_maintenance_window_date:, alt_maintenance_window_time_range:
+  )
+    contents = generate_email_contents(
+      org_name: org_name,
+      service_instances: service_instances,
+      maintenance_window_date: maintenance_window_date,
+      maintenance_window_time_range: maintenance_window_time_range,
+      alt_maintenance_window_date: alt_maintenance_window_date,
+      alt_maintenance_window_time_range: alt_maintenance_window_time_range
+    )
+
+    resp = client.send_email(
+      email_address: tenant_email_address,
+
+      template_id: AWS_REDIS_UPGRADE_TEMPLATE_ID,
+      email_reply_to_id: PAAS_SUPPORT_REPLY_TO_ID,
+
+      personalisation: {
+        # Template the whole body because we have a list
+        contents: contents,
+
+        # Required for templating subject line
+        maintenance_window_date: maintenance_window_date,
+      }
+    )
+  end
+end
---
+++ tools/aws_redis_update_manager/spec/tenant_notifier_spec.rb
@@ -0,0 +1,129 @@
+require 'base64'
+require 'json'
+require 'ostruct'
+
+RSpec.describe TenantNotifier do
+  let(:notify_api_key) { 'we-are-mocking-this' }
+  let(:org_name) { 'example-org' }
+  let(:tenant_email_address) { 'org-manager@domain.tld' }
+
+  let(:service_instances) do
+    [
+      OpenStruct.new(
+        org_name: org_name,
+        space_name: 'a-space',
+        instance_name: 'an-instance',
+      ),
+      OpenStruct.new(
+        org_name: org_name,
+        space_name: 'another-space',
+        instance_name: 'another-instance',
+      ),
+    ]
+  end
+
+  let(:maintenance_window_date) { '2038/01/19' }
+  let(:maintenance_window_time_range) { '2000-2100' }
+
+  let(:alt_maintenance_window_date) { '2038/01/19' }
+  let(:alt_maintenance_window_time_range) { '2000-2100' }
+
+  context 'when generating an email' do
+    let(:contents) do
+      TenantNotifier.new(notify_api_key).generate_email_contents(
+        org_name: org_name,
+        service_instances: service_instances,
+        maintenance_window_date: maintenance_window_date,
+        maintenance_window_time_range: maintenance_window_time_range,
+        alt_maintenance_window_date: alt_maintenance_window_date,
+        alt_maintenance_window_time_range: alt_maintenance_window_time_range
+      )
+    end
+
+    it 'should list the service instances to be updated' do
+      expect(contents).to match('( Org / Space / Service )')
+
+      expect(contents).to match([
+        service_instances.first.org_name,
+        service_instances.first.space_name,
+        service_instances.first.instance_name,
+      ].join(' / '))
+
+      expect(contents).to match([
+        service_instances.last.org_name,
+        service_instances.last.space_name,
+        service_instances.last.instance_name,
+      ].join(' / '))
+    end
+
+    it 'should include the maintenance window' do
+      expect(contents).to match(
+        "on #{maintenance_window_date}, between #{maintenance_window_time_range}",
+      )
+    end
+
+    it 'should include the alternative maintenance window' do
+      expect(contents).to match(
+        "on #{alt_maintenance_window_date}, between #{alt_maintenance_window_time_range}",
+      )
+    end
+  end
+
+  context 'when notifying a tenant' do
+    let(:notifier) { TenantNotifier.new(notify_api_key) }
+
+    before(:each) do
+      notifier.instance_variable_set(:@client, double)
+      allow(notifier.client).to receive(:send_email)
+    end
+
+    let(:send_email) do
+      notifier.notify_tenant(
+        tenant_email_address: tenant_email_address,
+        org_name: org_name,
+        service_instances: service_instances,
+        maintenance_window_date: maintenance_window_date,
+        maintenance_window_time_range: maintenance_window_time_range,
+        alt_maintenance_window_date: alt_maintenance_window_date,
+        alt_maintenance_window_time_range: alt_maintenance_window_time_range
+      )
+    end
+
+    it 'should personalise the Notify "contents" field ' do
+      expect(notifier.client).to receive(:send_email).with(
+        hash_including(personalisation: hash_including(
+          contents: TenantNotifier.new(notify_api_key).generate_email_contents(
+            org_name: org_name,
+            service_instances: service_instances,
+            maintenance_window_date: maintenance_window_date,
+            maintenance_window_time_range: maintenance_window_time_range,
+            alt_maintenance_window_date: alt_maintenance_window_date,
+            alt_maintenance_window_time_range: alt_maintenance_window_time_range
+          )
+        ))
+      )
+
+      send_email
+    end
+
+    it 'should personalise the Notify "maintenance_window_date" field ' do
+      expect(notifier.client).to receive(:send_email).with(
+        hash_including(personalisation: hash_including(
+          maintenance_window_date: maintenance_window_date
+        ))
+      )
+
+      send_email
+    end
+
+    it 'should set the reply to adress' do
+      expect(notifier.client).to receive(:send_email).with(
+        hash_including(
+          email_reply_to_id: eq('76d63d5f-e140-6a37-92ae-fc0d0d136f6f')
+        )
+      )
+
+      send_email
+    end
+  end
+end
