d66734b6bed56865659f8eb77c4c48766fa6e2b5
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2020-08-11 14:43:45 +0100

    Enable YAML creation from both version 3 and version 4 tfstate files

--- concourse/scripts/extract_terraform_state_to_yaml_test.go
+++ concourse/scripts/extract_terraform_state_to_yaml_test.go
@@ -26,7 +26,45 @@ var _ = Describe("ExtractTerraformStateToYAML", func() {
 		Expect(err).ToNot(HaveOccurred())
 	})
 
-	Context("with a valid tfstate file", func() {
+	Context("with a version 4 tfstate file", func() {
+		BeforeEach(func() {
+			cmdInput = `
+{
+    "version": 4,
+    "terraform_version": "0.12.29",
+    "serial": 5,
+    "lineage": "bfa4e77c-4e4e-462e-92a9-dba07be0f409",
+	"outputs": {
+		"foo_dns_name": {
+			"sensitive": false,
+			"type": "string",
+			"value": "foo.example.com"
+		},
+		"foo_elastic_ip": {
+			"sensitive": false,
+			"type": "string",
+			"value": "10.210.221.248"
+		},
+		"foo_security_group_id": {
+			"sensitive": false,
+			"type": "string",
+			"value": "sg-12345678"
+		}
+	},
+	"resources": {}
+}
+			`
+		})
+
+		It("returns a YAML containing all the output values", func() {
+			Eventually(session).Should(gexec.Exit(0))
+			Expect(session.Out).To(gbytes.Say("terraform_outputs_foo_dns_name: foo.example.com"))
+			Expect(session.Out).To(gbytes.Say("terraform_outputs_foo_elastic_ip: 10.210.221.248"))
+			Expect(session.Out).To(gbytes.Say("terraform_outputs_foo_security_group_id: sg-12345678"))
+		})
+	})
+
+	Context("with a version 3 tfstate file", func() {
 		BeforeEach(func() {
 			cmdInput = `
 {
@@ -40,20 +78,20 @@ var _ = Describe("ExtractTerraformStateToYAML", func() {
                 "root"
             ],
             "outputs": {
-                "foo_dns_name": {
+                "bar_dns_name": {
                     "sensitive": false,
                     "type": "string",
-                    "value": "foo.example.com"
+                    "value": "bar.example.com"
                 },
-                "foo_elastic_ip": {
+                "bar_elastic_ip": {
                     "sensitive": false,
                     "type": "string",
-                    "value": "10.210.221.248"
+                    "value": "10.210.220.248"
                 },
-                "foo_security_group_id": {
+                "bar_security_group_id": {
                     "sensitive": false,
                     "type": "string",
-                    "value": "sg-12345678"
+                    "value": "sg-123456789"
                 }
             },
 			"resources": {},
@@ -64,42 +102,11 @@ var _ = Describe("ExtractTerraformStateToYAML", func() {
 			`
 		})
 
-		It("returns a YAML containing all the output values", func() {
-			Eventually(session).Should(gexec.Exit(0))
-			Expect(session.Out).To(gbytes.Say("terraform_outputs_foo_dns_name: foo.example.com"))
-			Expect(session.Out).To(gbytes.Say("terraform_outputs_foo_elastic_ip: 10.210.221.248"))
-			Expect(session.Out).To(gbytes.Say("terraform_outputs_foo_security_group_id: sg-12345678"))
-		})
-	})
-
-	Context("with an old-format tfstate file", func() {
-		BeforeEach(func() {
-			cmdInput = `
-{
-    "version": 1,
-    "serial": 5,
-    "modules": [
-        {
-            "path": [
-                "root"
-            ],
-            "outputs": {
-                "foo_dns_name": "foo.example.com",
-                "foo_elastic_ip": "10.210.221.248",
-                "foo_security_group_id": "sg-12345678"
-            },
-			"resources": {},
-			"depends_on": []
-		}
-	]
-}
-			`
-		})
-
 		It("Exits non-zero with an error on STDERR", func() {
-			Eventually(session).Should(gexec.Exit())
-			Expect(session.ExitCode()).NotTo(Equal(0))
-			Expect(session.Out.Contents()).To(BeEmpty())
+			Eventually(session).Should(gexec.Exit(0))
+			Expect(session.Out).To(gbytes.Say("terraform_outputs_bar_dns_name: bar.example.com"))
+			Expect(session.Out).To(gbytes.Say("terraform_outputs_bar_elastic_ip: 10.210.220.248"))
+			Expect(session.Out).To(gbytes.Say("terraform_outputs_bar_security_group_id: sg-123456789"))
 		})
 	})
 })
