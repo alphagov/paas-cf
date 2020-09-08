c85c6c3fd37f5b00a9f8d12587c26874454cd1c5
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2020-09-08 16:54:46 +0100

    sqs-broker: add deployment manifest
    deploy the sqs-broker boshrelease as part of the main deployment.
    
    the broker is deployed with two plans:
    
    * "standard" a regular AWS SQS queue
    * "fifo" a first-in-first-out AWS SQS queue
    
    the sqs-broker vm is given the "sqs-broker" IAM role (current setup
    externally from this config in the aws-account-wide-terraform), outputs
    from the terraform are consumed to set the permission boundary policy
    that will be used to guard against the broker having too wide a
    priviledge.
    
    as per similar brokers, passwords are generated as part of deployment
    
    Co-Authored-By: Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
    Co-Authored-By: Alex Monk <alex.monk@digital.cabinet-office.gov.uk>

---
+++ manifests/cf-manifest/spec/manifest/sqs_broker_spec.rb
@@ -0,0 +1,59 @@
+RSpec.describe "SQS broker properties" do
+  let(:manifest) { manifest_with_defaults }
+  let(:properties) { manifest.fetch("instance_groups.sqs_broker.jobs.sqs-broker.properties.sqs-broker") }
+
+  describe "service plans" do
+    let(:services) do
+      properties.fetch("catalog").fetch("services")
+    end
+    let(:all_plans) do
+      services.flat_map { |s| s["plans"] }
+    end
+
+    specify "all services have a unique id" do
+      all_ids = services.map { |s| s["id"] }
+      duplicated_ids = all_ids.select { |id| all_ids.count(id) > 1 }.uniq
+      expect(duplicated_ids).to be_empty,
+        "found duplicate service ids (#{duplicated_ids.join(',')})"
+    end
+
+    specify "all services have a unique name" do
+      all_names = services.map { |s| s["name"] }
+      duplicated_names = all_names.select { |name| all_names.count(name) > 1 }.uniq
+      expect(duplicated_names).to be_empty,
+        "found duplicate service names (#{duplicated_names.join(',')})"
+    end
+
+    specify "all plans have a unique id" do
+      all_ids = all_plans.map { |p| p["id"] }
+      duplicated_ids = all_ids.select { |id| all_ids.count(id) > 1 }.uniq
+      expect(duplicated_ids).to be_empty,
+        "found duplicate plan ids (#{duplicated_ids.join(',')})"
+    end
+
+    specify "all plans within each service have a unique name" do
+      services.each do |s|
+        all_names = s["plans"].map { |p| p["name"] }
+        duplicated_names = all_names.select { |name| all_names.count(name) > 1 }.uniq
+        expect(duplicated_names).to be_empty,
+          "found duplicate plan names (#{duplicated_names.join(',')})"
+      end
+    end
+  end
+
+  describe "service broker is set to be shareable" do
+    let(:services) do
+      properties.fetch("catalog").fetch("services")
+    end
+
+    it "each service of the aws sqs service broker is shareable" do
+      services.each do |service|
+        service_name = service["name"]
+        shareable = service.dig("metadata", "shareable")
+
+        expect(shareable).not_to be(nil), "Service '#{service_name}' has to be shareable, but the 'shareable' parameter is missing in catalog/services/metadata"
+        expect(shareable).to be(true), "Service '#{service_name}' has to be shareable, but the value of the parameter is #{shareable}"
+      end
+    end
+  end
+end
