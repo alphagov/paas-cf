f1d208c289fbfad87ba5ebffccd612efdd4ebcc1
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-09-16 17:26:09 +0100

    cf: test that prod(s) are highly available
    I noticed that nats in production only runs two instances
    
    This is _not good_ because if nats goes down (eg AZ failure) then route
    propagation will not work, which means routes will get stale, which
    means app traffic will start to 500
    
    Nats going down may also lead to other bad consequences.
    
    This commit adds some spec tests, which checks that all non-brokers in
    production have at least 3 instances.
    
    Prometheus is still experimental which is why I have exempted it.
    
    Brokers are less critical and so I have only mandated that they have 2
    instances. A broker going down is not a big deal during an outage,
    compared to app traffic. Adding an extra instance is a waste of money
    (IMO) and will increase deployment times.
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

---
+++ manifests/cf-manifest/spec/manifest/ha_spec.rb
@@ -0,0 +1,52 @@
+RSpec.describe "high availability" do
+  let(:all_groups) { manifest.fetch("instance_groups") }
+  let(:perm_groups) { all_groups.reject { |i| i["lifecycle"] == "errand" } }
+
+  let(:brokers) { perm_groups.select { |i| i["name"] =~ /broker/ } }
+
+  let(:non_brokers) do
+    perm_groups
+      .reject { |i| i["name"] =~ /broker/ }
+      .reject { |i| i["name"] == "prometheus" }
+  end
+
+  describe "london" do
+    let(:manifest) { manifest_for_env("prod-lon") }
+
+    describe "non-brokers" do
+      it "ensures all instance groups are highly available >= 3" do
+        small_groups = non_brokers.select { |i| i["instances"] < 3 }
+        names = small_groups.map { |i| i["name"] }
+        expect(small_groups).to be_empty, "#{names} should have >= 3 instances"
+      end
+    end
+
+    describe "brokers" do
+      it "ensures all instance groups are highly available >= 2" do
+        small_groups = brokers.select { |i| i["instances"] < 2 }
+        names = small_groups.map { |i| i["name"] }
+        expect(small_groups).to be_empty, "#{names} should have >= 2 instances"
+      end
+    end
+  end
+
+  describe "ireland" do
+    let(:manifest) { manifest_for_env("prod") }
+
+    describe "non-brokers" do
+      it "ensures all instance groups are highly available >= 3" do
+        small_groups = non_brokers.select { |i| i["instances"] < 3 }
+        names = small_groups.map { |i| i["name"] }
+        expect(small_groups).to be_empty, "#{names} should have >= 3 instances"
+      end
+    end
+
+    describe "brokers" do
+      it "ensures all instance groups are highly available >= 2" do
+        small_groups = brokers.select { |i| i["instances"] < 2 }
+        names = small_groups.map { |i| i["name"] }
+        expect(small_groups).to be_empty, "#{names} should have >= 2 instances"
+      end
+    end
+  end
+end
