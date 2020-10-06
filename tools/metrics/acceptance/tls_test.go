43d409dad45899b38650198018f5c04336135ecb
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-10-06 09:09:47 +0100

    metrics: tls acceptance uses Eventually
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- tools/metrics/acceptance/tls_test.go
+++ tools/metrics/acceptance/tls_test.go
@@ -7,7 +7,7 @@ import (
 
 var _ = Describe("TLS =", func() {
 	It("should return TLS cert metrics", func() {
-		Expect(metricFamilies).To(SatisfyAll(
+		Eventually(getMetrics).Should(SatisfyAll(
 			HaveKey("paas_tls_certificates_validity_days"),
 		))
 	})
