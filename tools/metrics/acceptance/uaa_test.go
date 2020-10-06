4dbfbfbe64703dbd47d2cb02e76fcf4dd4b2c81a
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-10-06 09:10:01 +0100

    metrics: uaa acceptance uses Eventually
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- tools/metrics/acceptance/uaa_test.go
+++ tools/metrics/acceptance/uaa_test.go
@@ -7,7 +7,7 @@ import (
 
 var _ = Describe("UAA", func() {
 	It("should return UAA user metrics", func() {
-		Expect(metricFamilies).To(SatisfyAll(
+		Eventually(getMetrics).Should(SatisfyAll(
 			HaveKey("paas_uaa_users_count"),
 			HaveKey("paas_uaa_active_users_count"),
 		))
