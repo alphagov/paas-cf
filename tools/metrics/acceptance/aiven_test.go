b971a816d8bc7e1bb8b86e12e64a44365142857c
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-10-06 09:02:54 +0100

    metrics: aiven acceptance uses Eventually/Should
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- tools/metrics/acceptance/aiven_test.go
+++ tools/metrics/acceptance/aiven_test.go
@@ -7,6 +7,8 @@ import (
 
 var _ = Describe("Aiven", func() {
 	It("should return estimated cost", func() {
-		Expect(metricFamilies).To(HaveKey("paas_aiven_estimated_cost_pounds"))
+		Eventually(getMetrics).Should(
+			HaveKey("paas_aiven_estimated_cost_pounds"),
+		)
 	})
 })
