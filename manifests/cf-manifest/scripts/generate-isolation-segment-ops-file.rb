66758ccc6d96972e7fcf73c12f6155d1a732e97a
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-10-08 09:41:02 +0100

    isoseg: add comments to the isoseg script
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- manifests/cf-manifest/scripts/generate-isolation-segment-ops-file.rb
+++ manifests/cf-manifest/scripts/generate-isolation-segment-ops-file.rb
@@ -60,6 +60,25 @@ unless seg_def["isolation_segment_size"].nil?
 end
 
 if seg_def["restricted_egress"]
+  # When we restrict egress in an isolation segment, we presumably want to
+  # prevent exfiltration of data via DNS requests.
+  #
+  # If we did not have a DNS masking service (in this case Coredns) then any
+  # DNS request would go to BOSH DNS and then to the AWS VPC DNS service and
+  # then to the internet
+  #
+  # An attacker could exfiltrade data by making a DNS lookup for
+  # 42424242424242424242.attacker-controlled-domain.com
+  # and logging DNS queries, (4242... is used as an example credit card number)
+  #
+  # We want apps to communicate with each other, so we need to allow
+  # apps.internal DNS requests
+  #
+  # Unfortunately there is presently no way to distinguish between DNS server
+  # during staging, and during runtime, as such we also need to allow DNS
+  # requests for buildpacks.cloudfoundry.org, so that certain dependencies
+  # provided by buildpacks (eg nginx package for staticfile buildpack) can be
+  # downloaded during staging
   isolation_segment["jobs"] << {
     "name" => "coredns",
     "release" => "observability",
@@ -86,12 +105,26 @@ if seg_def["restricted_egress"]
     },
   }
 
+  # Localhost in an app container is not the same as localhost on the cell
+  # In order for DNS requests made inside the container to reach a DNS server
+  # on the host, it must have a non-loopback (localhost) IP address
+  # 169.254/16 is defined in RFC 3927 as a range for link-local communication.
+  #
+  # The BOSH DNS agent listens on 169.254.0.2 for this reason.
+  # Coredns listening on 169.254.0.3 is an arbitrary choice
   isolation_segment
     .fetch("jobs")
     .find { |job| job["name"] == "silk-cni" }[
     "properties"][
     "dns_servers"] = ["169.254.0.3"]
 
+  # We are denying everything except 10/8
+  # Some of these CIDR blocks look a little strange.
+  # Look at the tests in spec/manifest/isolation_segment_spec.rb
+  #
+  # We are only denying egress during "running", not "always" or "staging",
+  # which means the staging process has arbitrary egress, which is what we
+  # want
   isolation_segment
     .fetch("jobs")
     .find { |job| job["name"] == "silk-cni" }[
