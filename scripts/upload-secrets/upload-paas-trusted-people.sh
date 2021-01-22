aa2c8b696c19b9e37bf70b45b5111ee16dac81db
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2021-01-22 14:42:27 +0000

    Update paas-trusted-people to `main`

--- scripts/upload-secrets/upload-paas-trusted-people.sh
+++ scripts/upload-secrets/upload-paas-trusted-people.sh
@@ -6,6 +6,6 @@ set -euo pipefail
 
 cd "$(mktemp -d -t paas-trusted-people-XXXXX)"
 
-git clone --depth 1 --branch "${PAAS_TRUSTED_PEOPLE_BRANCH:-master}" git@github.com:alphagov/paas-trusted-people.git
+git clone --depth 1 --branch "${PAAS_TRUSTED_PEOPLE_BRANCH:-main}" git@github.com:alphagov/paas-trusted-people.git
 aws s3 cp ./paas-trusted-people/users.yml "s3://gds-paas-${DEPLOY_ENV}-state/paas-trusted-people/users.yml"
 
