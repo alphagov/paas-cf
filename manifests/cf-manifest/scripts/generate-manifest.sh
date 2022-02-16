701012dc8a374ab2c7c986464d15eed54c9f42c8
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2022-02-16 15:09:27 +0000

    Further protect prod and staging from static-credentialled oauth client
    We absolutely never want the OAuth client with static credentials and a
    localhost redirect url to make it out to production. Making the condiiton
    surrounding the application of the ops file more robust will help with that.

--- manifests/cf-manifest/scripts/generate-manifest.sh
+++ manifests/cf-manifest/scripts/generate-manifest.sh
@@ -21,7 +21,7 @@ if [ "${SLIM_DEV_DEPLOYMENT-}" = "true" ]; then
   opsfile_args="$opsfile_args -o ${CF_MANIFEST_DIR}/operations/speed-up-deployment-dev.yml"
 fi
 
-if [ "${MAKEFILE_ENV_TARGET-}" = "dev" ]; then
+if [ "${MAKEFILE_ENV_TARGET-}" = "dev" ] && [ "${DEPLOY_ENV-}" != "prod" ] && [ "${DEPLOY_ENV-}" != "prod-lon" ] && [ "${DEPLOY_ENV-}" != "stg-lon" ]; then
   echo "Adding static OAuth credentials for PaaS admin local development." 1>&2
   echo "The callback URL for this client is localhost." 1>&2
   echo "This should only be available in development environments." 1>&2
