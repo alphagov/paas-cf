7ab7501fc1a913436cd3ae16116b9c6c94a312d4
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2022-05-27 09:19:23 +0100

    Add updated bucket configuration

--- terraform/cloudfoundry/buckets.tf
+++ terraform/cloudfoundry/buckets.tf
@@ -1,114 +1,170 @@
 resource "aws_s3_bucket" "droplets-s3" {
-  bucket        = "${var.env}-cf-droplets"
-  acl           = "private"
-  force_destroy = "true"
+  bucket = "${var.env}-cf-droplets"
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_acl" "droplets-s3" {
+  bucket = aws_s3_bucket.droplets-s3.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "droplets-s3" {
+  bucket = aws_s3_bucket.droplets-s3.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  lifecycle_rule {
+resource "aws_s3_bucket_lifecycle_configuration" "droplets-s3" {
+  bucket = aws_s3_bucket.droplets-s3.id
+
+  rule {
     id      = "Expire old previous versions"
-    enabled = true
+    status = "Enabled"
     prefix  = ""
+    aabort_incomplete_multipart_upload_days {
+      days_after_initiation ="3"
+    }
 
     noncurrent_version_expiration {
-      days = 36
+      noncurrent_days = 36
     }
 
     expiration {
       expired_object_delete_marker = true
     }
-
-    abort_incomplete_multipart_upload_days = "3"
   }
 }
 
 resource "aws_s3_bucket" "buildpacks-s3" {
-  bucket        = "${var.env}-cf-buildpacks"
-  acl           = "private"
-  force_destroy = "true"
+  bucket = "${var.env}-cf-buildpacks"
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_acl" "buildpacks-s3" {
+  bucket = aws_s3_bucket.buildpacks-s3.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "buildpacks-s3" {
+  bucket = aws_s3_bucket.buildpacks-s3.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  lifecycle_rule {
+resource "aws_s3_bucket_lifecycle_configuration" "buildpacks-s3" {
+  bucket = aws_s3_bucket.buildpacks-s3.id
+
+  rule {
     id      = "Expire old previous versions"
-    enabled = true
+    status = "Enabled"
     prefix  = ""
+    abort_incomplete_multipart_upload_days {
+      days_after_initiation ="3"
+    }
 
     noncurrent_version_expiration {
-      days = 36
+      noncurrent_days = 36
     }
 
     expiration {
       expired_object_delete_marker = true
     }
-
-    abort_incomplete_multipart_upload_days = "3"
   }
 }
-
 resource "aws_s3_bucket" "packages-s3" {
-  bucket        = "${var.env}-cf-packages"
-  acl           = "private"
-  force_destroy = "true"
+  bucket = "${var.env}-cf-packages"
+}
 
-  versioning {
-    enabled = true
-  }
+resource "aws_s3_bucket_acl" "packages-s3" {
+  bucket = aws_s3_bucket.packages-s3.id
+  acl    = "private"
+}
 
-  lifecycle_rule {
-    id      = "Expire old previous versions"
-    enabled = true
-    prefix  = ""
+resource "aws_s3_bucket_versioning" "packages-s3" {
+  bucket = aws_s3_bucket.packages-s3.id
 
-    noncurrent_version_expiration {
-      days = 36
-    }
-
-    expiration {
-      expired_object_delete_marker = true
-    }
-
-    abort_incomplete_multipart_upload_days = "3"
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket" "resources-s3" {
-  bucket        = "${var.env}-cf-resources"
-  acl           = "private"
-  force_destroy = "true"
+resource "aws_s3_bucket_lifecycle_configuration" "packages-s3" {
+  bucket = aws_s3_bucket.packages-s3.id
 
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
+  rule {
     id      = "Expire old previous versions"
-    enabled = true
+    status = "Enabled"
     prefix  = ""
+    abort_incomplete_multipart_upload_days {
+      days_after_initiation ="3"
+    }
 
     noncurrent_version_expiration {
-      days = 36
+      noncurrent_days = 36
     }
 
     expiration {
       expired_object_delete_marker = true
     }
+  }
+}
+resource "aws_s3_bucket" "resources-s3" {
+  bucket = "${var.env}-cf-resources"
+}
 
-    abort_incomplete_multipart_upload_days = "3"
+resource "aws_s3_bucket_acl" "resources-s3" {
+  bucket = aws_s3_bucket.resources-s3.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "resources-s3" {
+  bucket = aws_s3_bucket.resources-s3.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "resources-s3" {
+  bucket = aws_s3_bucket.resources-s3.id
+
+  rule {
+    id      = "Expire old previous versions"
+    status = "Enabled"
+    prefix  = ""
+    abort_incomplete_multipart_upload_days {
+      days_after_initiation ="3"
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 36
+    }
+
+    expiration {
+      expired_object_delete_marker = true
+    }
   }
 }
 
 resource "aws_s3_bucket" "test-artifacts" {
-  bucket        = "gds-paas-${var.env}-test-artifacts"
-  acl           = "private"
+  bucket = "gds-paas-${var.env}-test-artifacts"
   force_destroy = "true"
+}
 
-  lifecycle_rule {
-    enabled = true
+resource "aws_s3_bucket_acl" "test-artifacts" {
+  bucket = aws_s3_bucket.test-artifacts.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "test-artifacts" {
+  bucket        = aws_s3_bucket.test-artifacts.id
+
+
+  rule {
+    id     = "Expire old previous versions"
+    status = "Enabled"
     prefix  = ""
 
     expiration {
@@ -123,15 +179,28 @@ variable "elb_access_log_bucket_name" {
 
 resource "aws_s3_bucket" "elb_access_log" {
   bucket        = "${var.assets_prefix}-${var.env}-${var.elb_access_log_bucket_name}"
-  acl           = "private"
   force_destroy = "true"
+}
+
+resource "aws_s3_bucket_acl" "elb_access_log" {
+  bucket = aws_s3_bucket.elb_access_log.id
+  acl           = "private"
+}
+
+resource "aws_s3_bucket_policy" "elb_access_log" {
+  bucket = aws_s3_bucket.elb_access_log.id
   policy = templatefile("${path.module}/policies/elb_access_log_bucket.json.tpl", {
     bucket_name = "${var.assets_prefix}-${var.env}-${var.elb_access_log_bucket_name}",
     principal   = var.elb_account_ids[var.region]
   })
+}
 
-  lifecycle_rule {
-    enabled = true
+resource "aws_s3_bucket_lifecycle_configuration" "elb_access_log" {
+  bucket = aws_s3_bucket.elb_access_log.id
+
+  rule {
+    id     = "Expire old previous versions"
+    status = "Enabled"
     prefix  = ""
 
     expiration {
@@ -139,4 +208,3 @@ resource "aws_s3_bucket" "elb_access_log" {
     }
   }
 }
-
