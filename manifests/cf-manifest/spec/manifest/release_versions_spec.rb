bc34654a856e4cc80a3d6f4f4f4814bc5415b773
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2022-06-09 13:36:14 +0100

    Unpin the UAA release in the tests
    Since we're tracking the upstream release of UAA, we no longer need to
    test that we've a pinned version of UAA in place.

--- manifests/cf-manifest/spec/manifest/release_versions_spec.rb
+++ manifests/cf-manifest/spec/manifest/release_versions_spec.rb
@@ -27,12 +27,7 @@ RSpec.describe "release versions" do
       Gem::Version.new(v.gsub(/^v/, "").gsub(/^([0-9]+)$/, '0.0.\1'))
     end
 
-    pinned_releases = {
-      "uaa" => {
-        local: "0.1.35",
-        upstream: "75.19.0",
-      },
-    }
+    pinned_releases = {}
 
     manifest_releases = manifest_without_vars_store.fetch("releases").map { |release|
       [release["name"], release["version"]]
