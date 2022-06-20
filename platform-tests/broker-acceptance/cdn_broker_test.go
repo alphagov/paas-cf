57609199c31bb5ad9bff5cf5f2c5f7a0c8f33e29
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2022-06-20 10:23:15 +0100

    custom-broker-acceptance-tests: improve & expand cdn-broker tests
    add coverage of cf domain existence and ownership checking

--- platform-tests/broker-acceptance/cdn_broker_test.go
+++ platform-tests/broker-acceptance/cdn_broker_test.go
@@ -40,17 +40,67 @@ var _ = Describe("CDN broker", func() {
 
 			serviceInstanceName = generator.PrefixedRandomName(testConfig.GetNamePrefix(), "test-cdn")
 
+			// best effort tidyup - we don't really care if these pass or fail
+			defer pollForServiceDeletionCompletion(serviceInstanceName)
+			defer cf.Cf("delete-domain", domainName, "-f")
+			defer cf.Cf("delete-service", serviceInstanceName, "-f")
+
 			By("creating a CDN instance: "+serviceInstanceName, func() {
 				Expect(cf.Cf("create-domain", orgName, domainName).Wait(testConfig.DefaultTimeoutDuration())).To(Exit(0))
 				Expect(cf.Cf("create-service", serviceName, serviceName, serviceInstanceName, "-c", domainNameList).Wait(testConfig.DefaultTimeoutDuration())).To(Exit(0))
 				pollForCdnServiceCreationCompletion(serviceInstanceName)
 			})
 
-			defer By("deleting a standard CDN service", func() {
+			By("deleting a standard CDN service", func() {
 				Expect(cf.Cf("delete-service", serviceInstanceName, "-f").Wait(testConfig.DefaultTimeoutDuration())).To(Exit(0))
 				Expect(cf.Cf("delete-domain", domainName, "-f").Wait(testConfig.DefaultTimeoutDuration())).To(Exit(0))
 				pollForServiceDeletionCompletion(serviceInstanceName)
 			})
 		})
+
+		It("refuses to create a CDN for a domain without a cf create-domain", func() {
+			domainName := generator.PrefixedRandomName(testConfig.GetNamePrefix(), "cdn-broker") + ".net"
+			domainNameList := fmt.Sprintf(`{"domain": "%s"}`, domainName)
+
+			serviceInstanceName = generator.PrefixedRandomName(testConfig.GetNamePrefix(), "test-cdn")
+
+			// best effort tidyup - we don't really care if these pass or fail.
+			// currently this kind of failure doesn't actually stop the service
+			// being "created".
+			defer pollForServiceDeletionCompletion(serviceInstanceName)
+			defer cf.Cf("delete-service", serviceInstanceName, "-f")
+
+			By("attempting to create a CDN instance: "+serviceInstanceName, func() {
+				cf_create_service := cf.Cf("create-service", serviceName, serviceName, serviceInstanceName, "-c", domainNameList).Wait(testConfig.DefaultTimeoutDuration())
+				Expect(cf_create_service).To(Exit(1))
+				Expect(cf_create_service.Err.Contents()).To(ContainSubstring("cf create-domain"))
+			})
+		})
+
+		It("refuses to create a CDN for a domain with wrong ownership", func() {
+			domainName := generator.PrefixedRandomName(testConfig.GetNamePrefix(), "cdn-broker") + ".net"
+			domainNameList := fmt.Sprintf(`{"domain": "%s"}`, domainName)
+
+			serviceInstanceName = generator.PrefixedRandomName(testConfig.GetNamePrefix(), "test-cdn")
+
+			// best effort tidyup - we don't really care if these pass or fail.
+			// currently this kind of failure doesn't actually stop the service
+			// being "created".
+			defer pollForServiceDeletionCompletion(serviceInstanceName)
+			defer cf.Cf("delete-domain", domainName, "-f")
+			defer cf.Cf("delete-service", serviceInstanceName, "-f")
+
+			By("attempting to create a CDN instance: "+serviceInstanceName, func() {
+				Expect(cf.Cf("create-domain", altOrgName, domainName).Wait(testConfig.DefaultTimeoutDuration())).To(Exit(0))
+				cf_create_service := cf.Cf("create-service", serviceName, serviceName, serviceInstanceName, "-c", domainNameList).Wait(testConfig.DefaultTimeoutDuration())
+				Expect(cf_create_service).To(Exit(1))
+				Expect(cf_create_service.Err.Contents()).To(ContainSubstring("different organization"))
+			})
+		})
+
+		// tests of update-service functionality would be wonderful but quite hard to implement as to get a service
+		// into a state where it will accept updates we would have to first validate the domain which would require
+		// the tests having permissions to update dns records on route53.
+
 	})
 })
