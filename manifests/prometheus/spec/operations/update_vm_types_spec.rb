26516c387e47e20054c5a9b19fd8931a12338c3e
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2022-07-01 10:37:04 +0100

    Use xlarge instance type for platform Prometheus machines
    The previous instance type gave the machine 8gb RAM, which was proving not to
    be enough and caused Prometheus to dip in to its swap regularly. The xlarge
    instance type will give it 16gb RAM.

--- manifests/prometheus/spec/operations/update_vm_types_spec.rb
+++ manifests/prometheus/spec/operations/update_vm_types_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe "update-vm-types.yml" do
   {
     "alertmanager" => "nano",
     "grafana" => "nano",
-    "prometheus2" => "large",
+    "prometheus2" => "xlarge",
   }.each do |name, type|
     it "sets the vm_type for #{name} to #{type}" do
       expect(manifest_with_defaults.get("instance_groups.#{name}.vm_type")).to eq(type)
