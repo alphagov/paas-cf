2befa86831f8018629123d7a3cc7b64b089fe406
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2022-08-25 17:19:59 +0100

    app-autoscaler-cpu-usage example app: revamp for better reliability, simplicity
    it's much simpler to just check the evenness of the absolute time
    divided by the duration, and this also removes weirdness around the
    end of the hour that we got from secondOfHour-based behaviour.

--- platform-tests/example-apps/app-autoscaler-cpu-usage/main.go
+++ platform-tests/example-apps/app-autoscaler-cpu-usage/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"math"
 	"os"
 	"time"
 )
@@ -19,24 +18,26 @@ func main() {
 	}
 
 	log.Printf(
-		"My plan is to chill for %ds then party hard for %ds seconds, then repeat",
-		int(duration.Seconds()), int(duration.Seconds()),
+		"My plan is to chill for %s then party hard for %s, then repeat",
+		duration, duration,
 	)
 
 	for {
-		now := time.Now()
-		secondOfHour := now.Second() + (60 * now.Minute()) // between 0 and 3600
+		// using a fixed "now" makes it easier to reason about behaviour
+		// and removes an amount of nondeterminism
+		uniformNow := time.Now()
+		party, timeRemaining := shouldWePartyAndForHowLong(uniformNow, duration)
 
-		if weShouldChill(secondOfHour, duration) {
-			chillForSomeTime(time.Now().Sub(time.Now().Truncate(duration)))
+		if party {
+			partyHardForSomeTime(timeRemaining)
 		} else {
-			partyHardForSomeTime(time.Now().Sub(time.Now().Truncate(duration)))
+			chillForSomeTime(timeRemaining)
 		}
 	}
 }
 
 func chillForSomeTime(duration time.Duration) {
-	until := time.Now().Truncate(time.Minute).Add(duration)
+	until := time.Now().Truncate(time.Millisecond).Add(duration)
 
 	log.Printf("Going to chill until %s", until)
 
@@ -44,7 +45,7 @@ func chillForSomeTime(duration time.Duration) {
 }
 
 func partyHardForSomeTime(duration time.Duration) {
-	until := time.Now().Truncate(time.Minute).Add(duration)
+	until := time.Now().Truncate(time.Millisecond).Add(duration)
 	done := make(chan struct{})
 
 	log.Printf("Going to party hard until %s", until)
@@ -63,14 +64,18 @@ func partyHardForSomeTime(duration time.Duration) {
 	done <- struct{}{}
 }
 
-func weShouldChill(secondOfHour int, duration time.Duration) bool {
-	// y = sin(t * pi) is a wave with a period of 2
-	// where t is the current second of the hour
-	// y is positive between 0 and 1 and negative between 1 and 2
+func shouldWePartyAndForHowLong(tNow time.Time, period time.Duration) (bool, time.Duration) {
+	// this has to be calculated based on absolute time so that different
+	// instances are in sync with each other, meaning we can't use
+	// go's monotonic clock arithmetic, so in theory this isn't totally
+	// clock-slew-safe, but i can live with that.
 
-	// if sin(t * pi / seconds) is >= 0 then we should chill, else party hard
-	// this ensures that all instances of the app chill and in phase
+	// whether we should party is just whether the time tNow divided by the
+	// period is odd
+	shouldWe := (tNow.UnixMilli() / int64(period.Milliseconds())) % 2 != 0
 
-	durationSeconds := duration.Seconds()
-	return math.Sin(math.Pi*float64(secondOfHour)/durationSeconds) >= 0
+	// the time remaining is the period minus the remainder of that division
+	timeRemaining := period - time.Duration(float64(tNow.UnixMilli() % int64(period.Milliseconds())) * float64(time.Millisecond))
+
+	return shouldWe, timeRemaining
 }
