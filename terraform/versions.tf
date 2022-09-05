3a8271fd2cadb07ddf2709dad079e9bd9751cfe9
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2022-09-05 14:14:52 +0100

    Remove template proider and update version to match other usages

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,20 +2,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>4.15.0"
+      version = "~>4.25.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~>3.2.0"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "2.2.0"
+      version = "~>3.4.2"
     }
     pingdom = {
       source  = "russellcardullo/pingdom"
       version = "1.1.2"
     }
   }
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.2.8"
 }
