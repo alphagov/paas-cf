bb0c078f63c66c0711f1b1037297ac122bcce7c7
Ben Corlett <ben.corlett@digital.cabinet-office.gov.uk>
2023-03-08 08:45:21 +0000

    Allow vpc_peers network access to postgres databases

--- terraform/vpc-peering/vpc_peering.tf
+++ terraform/vpc-peering/vpc_peering.tf
@@ -18,6 +18,24 @@ data "aws_route_tables" "internet" {
   }
 }
 
+data "aws_route_tables" "main" {
+  vpc_id = var.vpc_id
+
+  filter {
+    name   = "association.main"
+    values = [true]
+  }
+}
+
+data "aws_network_acls" "default" {
+  vpc_id = var.vpc_id
+
+  filter {
+    name   = "default"
+    values = [true]
+  }
+}
+
 resource "aws_vpc_peering_connection" "vpc_peer" {
   for_each      = local.peer_name_to_details
   peer_owner_id = each.value.account_id
@@ -53,4 +71,36 @@ resource "aws_route" "vpc_peer_route_2" {
   timeouts {
     create = "5m"
   }
+}
+
+resource "aws_route" "default" {
+  for_each = local.peer_name_to_details
+
+  route_table_id         = data.aws_route_tables.main.ids[0]
+  destination_cidr_block = each.value.subnet_cidr
+  gateway_id             = local.vpc_to_peer_id[each.value.vpc_id][0]
+}
+
+resource "aws_network_acl_rule" "destination_postgres_rule" {
+  for_each = local.peer_name_to_details
+
+  network_acl_id = data.aws_network_acls.default.ids[0]
+  rule_number    = 10 + index(local.peer_name_to_details, each.value)
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = each.value.subnet_cidr
+  from_port      = 5432
+  to_port        = 5432
+}
+
+resource "aws_network_acl_rule" "deny_rule" {
+  for_each = local.peer_name_to_details
+
+  network_acl_id = data.aws_network_acls.default.ids[0]
+  rule_number    = 20 + index(local.peer_name_to_details, each.value)
+  egress         = false
+  protocol       = "-1"
+  rule_action    = "deny"
+  cidr_block     = each.value.subnet_cidr
 }
