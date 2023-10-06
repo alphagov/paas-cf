9c1003825f3e2b58cce753cb516f82d9027e7de3
Jamie Van Dyke <jamie.vandyke@digital.cabinet-office.gov.uk>
2023-10-06 16:58:20 +0100

    Change the ALB ports to 443 (fix s3 too)

--- terraform/cloudfoundry/broker_alb.tf
+++ terraform/cloudfoundry/broker_alb.tf
@@ -93,12 +93,12 @@ resource "aws_lb_listener_rule" "cf_s3_broker" {
 
 resource "aws_lb_target_group" "cf_s3_broker" {
   name     = "${var.env}-cf-s3-broker"
-  port     = 80
-  protocol = "HTTP"
+  port     = 443
+  protocol = "HTTPS"
   vpc_id   = var.vpc_id
 
   health_check {
-    port                = 80
+    port                = 443
     path                = "/healthcheck"
     protocol            = "HTTP"
     interval            = var.health_check_interval
@@ -137,12 +137,12 @@ resource "aws_lb_listener_rule" "cf_sqs_broker" {
 
 resource "aws_lb_target_group" "cf_sqs_broker" {
   name     = "${var.env}-cf-sqs-broker"
-  port     = 80
-  protocol = "HTTP"
+  port     = 443
+  protocol = "HTTPS"
   vpc_id   = var.vpc_id
 
   health_check {
-    port                = 80
+    port                = 443
     path                = "/healthcheck"
     protocol            = "HTTP"
     interval            = var.health_check_interval
